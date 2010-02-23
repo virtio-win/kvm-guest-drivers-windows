56399606863a0cc14202105223586a38df5b914a
Yan Vugenfirer <yvugenfi@redhat.com>
2010-02-23 12:14:12 +0200

    [virtio-serial] Initial commit. Project and build files.
    	Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>

---
+++ virtio-serial/clean.bat
@@ -0,0 +1,16 @@
+rmdir /S /Q Debug
+rmdir /S /Q Release
+rmdir /S /Q objfre_wxp_x86
+rmdir /S /Q objfre_wnet_x86
+rmdir /S /Q objfre_wnet_amd64
+rmdir /S /Q objfre_w2k_x86
+rmdir /S /Q objfre_wlh_x86
+rmdir /S /Q objfre_wlh_amd64
+rmdir /S /Q objfre_win7_x86
+rmdir /S /Q objfre_win7_amd64
+
+
+
+del /F *.log *.wrn *.err
+
+
