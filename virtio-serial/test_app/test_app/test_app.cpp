443f1152a16f0d7dd0e7fa908c378c9335d2927c
Yan Vugenfirer <yvugenfi@redhat.com>
2010-04-04 10:46:33 +0300

    [virtio-serial] Test app. Add async file operation tests.

--- virtio-serial/test_app/test_app/test_app.cpp
+++ virtio-serial/test_app/test_app/test_app.cpp
@@ -6,6 +6,20 @@
 
 #define PAGE_SIZE 4096
 
+VOID CALLBACK FileIOCompletionRoutine(
+  __in  DWORD dwErrorCode,
+  __in  DWORD dwNumberOfBytesTransfered,
+  __in  LPOVERLAPPED lpOverlapped
+)
+{
+	printf("Write complete with error %d, transfered %d\n", 
+		dwErrorCode, 
+		dwNumberOfBytesTransfered);
+
+	__asm int 3;
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
 	HANDLE hFile;
@@ -15,7 +29,7 @@ int _tmain(int argc, _TCHAR* argv[])
                         0,
                         NULL, // no SECURITY_ATTRIBUTES structure
                         OPEN_EXISTING, // No special create flags
-                        0, // No special attributes
+                        FILE_FLAG_OVERLAPPED,//0, // No special attributes
                         NULL);
 
 	if(INVALID_HANDLE_VALUE == hFile)
@@ -44,29 +58,73 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 	}
 */
+	HANDLE hEvent = CreateEvent(NULL, FALSE, FALSE, L"Just_event");
 	
+	printf ("Created event %x\n", hEvent);
+	BOOL bSomethingToRead = TRUE;
+	OVERLAPPED Overlapped;
+	OVERLAPPED OverlappedRead;
+
 	while(true)
 	{
 		printf("\n>");
 		gets(buffer);
-		if(!WriteFile(hFile,
+/*		if(!WriteFile(hFile,
 					  buffer,
 					  strlen(buffer) + 1,
 					  &size,
 					  NULL))
 		{
 			printf("Error writing to file %d\n", GetLastError());
-		}
-		if(ReadFile(hFile,
-					 read_buffer,
-					 PAGE_SIZE,
-					 &size,
-					 NULL))
-		{
-			read_buffer[size] = '\0';
+		}*/
 
-			//printf("Got from device %d bytes>>> \n%s\n", size, read_buffer);
-			printf("%s", read_buffer);
+		
+		memset(&Overlapped, 0, sizeof(OVERLAPPED));
+
+		if(!WriteFileEx(hFile,
+						buffer,
+						strlen(buffer) + 1,
+						&Overlapped,
+						FileIOCompletionRoutine))
+		{
+			printf("Error writing to file %d\n", GetLastError());
+		}
+
+		if(bSomethingToRead)
+		{
+			memset(&OverlappedRead, 0, sizeof(OVERLAPPED));
+			OverlappedRead.hEvent = hEvent;
+
+			bSomethingToRead = FALSE;
+
+			if(ReadFile(hFile,
+						 read_buffer,
+						 PAGE_SIZE,
+						 &size,
+						 &OverlappedRead))
+			{
+				read_buffer[size] = '\0';
+				bSomethingToRead = TRUE;
+				//printf("Got from device %d bytes>>> \n%s\n", size, read_buffer);
+				printf("%s", read_buffer);
+			}			
+		}
+
+		if(WAIT_TIMEOUT != WaitForSingleObject(hEvent, 1000))
+		{
+			bSomethingToRead = TRUE;
+
+			if (GetOverlappedResult(hFile,
+                               &OverlappedRead,
+                               &size,
+                               TRUE))
+			{
+				read_buffer[size] = '\0';
+				printf("Got from device %d bytes>>> \n%s\n", size, read_buffer);
+				
+
+				//				printf("%s", read_buffer);
+			}
 		}
 
 	}
