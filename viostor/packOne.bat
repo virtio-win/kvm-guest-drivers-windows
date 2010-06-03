49f5e7c2d3f931bc8994fe461e31e770fa0649ea
Vadim Rozenfeld <vrozenfe@redhat.com>
2010-06-03 14:19:37 +0300

    [viostor] replace INF with INX files.

--- viostor/packOne.bat
+++ viostor/packOne.bat
@@ -2,7 +2,6 @@
 : Param1 - Win7 | Wlh | Wnet | XP
 : Param2 - x86|x64
 : Param3 - sys name
-: Param3 - inf name
  
 if "%2"=="x64" set %%2=amd64
 
@@ -26,7 +25,6 @@ set BUILDROOT=C:\WINDDK\%DDKVER%
 set INST_OS=%1
 set INST_ARC=%2
 set SYS_NAME=%3
-set INF_NAME=%4
 rem set INST_EXT=INST_ARC
 
 if /i "%INST_ARC%"=="x64" goto :set_x64
@@ -41,9 +39,9 @@ set INST_EXT=amd64
 :startcopy
 set SYS_PATH_AND_NAME=objfre_%INST_OS%_%INST_ARC%\%INST_EXT%\%SYS_NAME%.sys
 set PDB_PATH_AND_NAME=objfre_%INST_OS%_%INST_ARC%\%INST_EXT%\%SYS_NAME%.pdb
-set INF_PATH_AND_NAME=%INF_NAME%.inf
+set INF_PATH_AND_NAME=objfre_%INST_OS%_%INST_ARC%\%INST_EXT%\%SYS_NAME%.inf
 
-rem echo makeinstall %1 %2 %3 %4
+rem echo makeinstall %1 %2 %3
 mkdir .\Install\%INST_OS%\%INST_ARC%
 del /Q .\Install\%INST_OS%\%INST_ARC%\%FILE_NAME%.*
 copy /Y %SYS_PATH_AND_NAME% .\Install\%INST_OS%\%INST_ARC%
@@ -81,7 +79,6 @@ inf2cat /driver:Install\%INST_OS%\%INST_ARC% /os:%_OSMASK_%
 set INST_OS=
 set INST_ARC=
 set SYS_NAME=
-set INF_NAME=
 set SYS_PATH_AND_NAME=
 set PDB_PATH_AND_NAME=
 set INF_PATH_AND_NAME=
