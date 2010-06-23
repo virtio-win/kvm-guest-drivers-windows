9aa57642279f9f55903576b8b63615b9af9765f6
Vadim Rozenfeld <vrozenfe@redhat.com>
2010-06-23 22:44:00 +0300

    [virtio-serial] remove virtio-serial project

--- virtio-serial/VIOSerialMemUtils.h
+++
@@ -1,8 +0,0 @@
-#ifndef VIOSERIAL_MEM_UTILS_H
-#define VIOSERIAL_MEM_UTILS_H
-
-PVOID AllocatePhysical(ULONG uSize);
-void FreePhysical(PVOID addr);
-PHYSICAL_ADDRESS GetPhysicalAddress(PVOID addr);
-
-#endif /* VIOSERIAL_MEM_UTILS_H */
