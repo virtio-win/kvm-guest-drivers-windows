d811b370d84d044bdbd9d6a58f9430df83095555
Yan Vugenfirer <yvugenfi@redhat.com>
2010-10-12 01:31:54 +0200

    [NetKVM] NetCfg utility - add exception class

---
+++ NetKVM/CoInstaller/Exception.cpp
@@ -0,0 +1,152 @@
+#include "stdafx.h"
+#include "Exception.h"
+
+neTKVMException::neTKVMException()
+{ 
+
+}
+
+neTKVMException::neTKVMException(LPCTSTR lpzMessage)
+{
+	if (lpzMessage)
+	{
+		SetMessage(tstring(lpzMessage));
+	}
+}
+
+neTKVMException::neTKVMException(const tstring &Message)
+{ 
+	SetMessage(Message);
+}
+
+neTKVMException::neTKVMException(const neTKVMException &Other)
+{
+	SetMessage(Other.m_Message);
+}
+
+neTKVMException::~neTKVMException(void)
+{
+
+}
+
+const char *neTKVMException::what() const
+{
+	return m_MBCSMessage.c_str();
+}
+
+LPCTSTR neTKVMException::twhat() const
+{
+	return m_Message.c_str();
+}
+
+void neTKVMException::SetMessage(const tstring &Message)
+{
+	m_Message     = Message;
+	m_MBCSMessage = tstring2string(m_Message); 
+}
+
+neTKVMNumErrorException::neTKVMNumErrorException(LPCTSTR lpzDescription, DWORD dwErrorCode)
+	: neTKVMException(lpzDescription), m_dwErrorCode(dwErrorCode)
+{
+
+}
+
+neTKVMNumErrorException::neTKVMNumErrorException(const tstring &Description, DWORD dwErrorCode)
+	: neTKVMException(Description), m_dwErrorCode(dwErrorCode)
+{
+
+}
+
+neTKVMNumErrorException::neTKVMNumErrorException(const neTKVMNumErrorException& Other)
+	: neTKVMException(Other)
+{
+	m_dwErrorCode = Other.m_dwErrorCode;
+}
+
+neTKVMCRTErrorException::neTKVMCRTErrorException(int nErrorCode)
+	: neTKVMNumErrorException(GetErrorString(nErrorCode), (DWORD)nErrorCode)
+{
+
+}
+
+neTKVMCRTErrorException::neTKVMCRTErrorException(LPCTSTR lpzDescription, int nErrorCode)
+	: neTKVMNumErrorException(tstring(lpzDescription) + GetErrorString((DWORD)nErrorCode), (DWORD)nErrorCode)
+{
+
+}
+
+neTKVMCRTErrorException::neTKVMCRTErrorException(const tstring &Description, int nErrorCode)
+: neTKVMNumErrorException(Description + GetErrorString((DWORD)nErrorCode), (DWORD)nErrorCode)
+{
+
+}
+
+neTKVMCRTErrorException::neTKVMCRTErrorException(const neTKVMCRTErrorException &Other)
+	: neTKVMNumErrorException(Other)
+{
+
+}
+
+tstring neTKVMCRTErrorException::GetErrorString(DWORD dwErrorCode)
+{
+#ifdef WIN32
+	TCHAR tcaBuff[256];
+	_tcserror_s(tcaBuff, TBUF_SIZEOF(tcaBuff), (int)dwErrorCode);
+	return tcaBuff;
+#else
+	return string2tstring(strerror((int)dwErrorCode));
+#endif
+}
+
+#ifdef WIN32
+neTKVMW32ErrorException::neTKVMW32ErrorException(DWORD dwErrorCode)
+	: neTKVMNumErrorException(GetErrorString(m_dwErrorCode), dwErrorCode)
+{
+
+}
+
+neTKVMW32ErrorException::neTKVMW32ErrorException(LPCTSTR lpzDescription, DWORD dwErrorCode)
+	: neTKVMNumErrorException(tstring(lpzDescription) + GetErrorString(dwErrorCode), dwErrorCode)
+{
+
+}
+
+neTKVMW32ErrorException::neTKVMW32ErrorException(const tstring &Description, DWORD dwErrorCode)
+	: neTKVMNumErrorException(Description + GetErrorString(dwErrorCode), dwErrorCode)
+{
+
+}
+
+neTKVMW32ErrorException::neTKVMW32ErrorException(const neTKVMW32ErrorException &Other)
+	: neTKVMNumErrorException(Other)
+{
+
+}
+
+tstring neTKVMW32ErrorException::GetErrorString(DWORD dwErrorCode)
+{
+	LPVOID lpMsgBuf;
+	DWORD  msgLen = ::FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+									FORMAT_MESSAGE_FROM_SYSTEM | 
+									FORMAT_MESSAGE_IGNORE_INSERTS|
+									FORMAT_MESSAGE_MAX_WIDTH_MASK,
+									NULL,
+									dwErrorCode,
+									MAKELANGID(LANG_ENGLISH, SUBLANG_ENGLISH_US),
+									(LPTSTR) &lpMsgBuf,
+									0,
+									NULL);
+	if(msgLen == 0)
+	{
+		tstringstream strm;
+		strm << TEXT("Failed to get error description for error code: 0x") << hex << dwErrorCode;
+		return strm.str();
+	}
+	else
+	{
+		tstring strResult((LPCTSTR)lpMsgBuf, msgLen);
+		::LocalFree( lpMsgBuf );
+		return strResult;
+	}
+}
+#endif // WIN32
---
+++ NetKVM/CoInstaller/Exception.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "tstrings.h"
+
+using std::exception;
+
+class neTKVMException : public exception
+{
+public:
+	neTKVMException();
+	neTKVMException(LPCTSTR lpzMessage);
+	neTKVMException(const tstring &Message);
+	neTKVMException(const neTKVMException &Other);
+	virtual ~neTKVMException();
+
+	virtual const char *what() const;
+	virtual LPCTSTR     twhat() const;
+
+protected:
+	void SetMessage(const tstring &Message);
+
+private:
+	tstring m_Message;
+	string  m_MBCSMessage;
+};
+
+class neTKVMNumErrorException : public neTKVMException
+{
+public:
+	neTKVMNumErrorException(LPCTSTR lpzDescription, DWORD dwErrorCode);
+	neTKVMNumErrorException(const tstring &Description, DWORD dwErrorCode);
+	neTKVMNumErrorException(const neTKVMNumErrorException& Other);
+
+	DWORD GetErrorCode(void) { return m_dwErrorCode; }
+
+protected:
+	DWORD m_dwErrorCode;
+};
+
+class neTKVMCRTErrorException : public neTKVMNumErrorException
+{
+public:
+	neTKVMCRTErrorException(int nErrorCode = errno);
+	neTKVMCRTErrorException(LPCTSTR lpzDescription, int nErrorCode = errno);
+	neTKVMCRTErrorException(const tstring &Description, int nErrorCode = errno);
+	neTKVMCRTErrorException(const neTKVMCRTErrorException &Other);
+
+protected:
+	static tstring GetErrorString(DWORD dwErrorCode);
+};
+
+#ifdef WIN32
+class neTKVMW32ErrorException : public neTKVMNumErrorException
+{
+public:
+	neTKVMW32ErrorException(DWORD dwErrorCode = GetLastError());
+	neTKVMW32ErrorException(LPCTSTR lpzDescription, DWORD dwErrorCode = GetLastError());
+	neTKVMW32ErrorException(const tstring &Description, DWORD dwErrorCode = GetLastError());
+	neTKVMW32ErrorException(const neTKVMW32ErrorException &Other);
+
+protected:
+	static tstring GetErrorString(DWORD dwErrorCode);
+};
+#endif
+
