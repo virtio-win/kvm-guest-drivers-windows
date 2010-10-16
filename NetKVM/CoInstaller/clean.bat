e4dfe4df923b9fdab3c303ab105df1e4122ed410
Yan Vugenfirer <yvugenfi@redhat.com>
2010-10-16 19:43:47 +0200

    [NetKVM] Network configuration utility - build with DDK in order to integrate with driver build

---
+++ NetKVM/CoInstaller/clean.bat
@@ -0,0 +1,12 @@
+del BuildLog.htm
+
+rmdir /S /Q objfre_wlh_x86
+rmdir /S /Q objfre_wnet_amd64
+rmdir /S /Q objfre_w2k_x86
+rmdir /S /Q objfre_wxp_x86
+rmdir /S /Q objfre_wlh_amd64
+rmdir /S /Q "Debug unicode"
+rmdir /S /Q "Release unicode"
+rmdir /S /Q x64
+
+del /F *.log *.wrn *.err
---
+++ NetKVM/CoInstaller/setbuildenv.bat
@@ -0,0 +1,2 @@
+set PSDK_INC_PATH="C:\Program Files\Microsoft SDKs\Windows\v6.0A\Include"
+set PSDK_LIB_PATH="C:\Program Files\Microsoft SDKs\Windows\v6.0A\Lib"
