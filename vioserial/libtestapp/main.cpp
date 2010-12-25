2de238cf6d463fa79c95e31db3110d22a1a00383
Vadim Rozenfeld <vrozenfe@redhat.com>
2010-12-25 18:42:34 +0200

    [vioserial] UM lib test. add port write test

--- vioserial/libtestapp/main.cpp
+++ vioserial/libtestapp/main.cpp
@@ -9,7 +9,20 @@ wmain(
     )
 {
     VIOSStartup();
-    printf ("we have %d port(s) in the system.\n", NumPorts());
+    UINT ports = NumPorts();
+    printf ("we have %d port(s) in the system.\n", ports);
+    if (ports)
+    {
+        PVOID port = OpenPortById(0);
+        if (port)
+        {
+           UINT test_size = 1024*1024*2;
+           PBYTE data = (PBYTE) new BYTE[test_size];
+           memset (data, '0', test_size);
+           WritePort(port, data, test_size);
+           delete[] data;
+        }
+    }
     VIOSCleanup();
     return 0;
 }
