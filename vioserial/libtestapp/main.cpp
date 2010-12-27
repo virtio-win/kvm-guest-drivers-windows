72260f1f13b97f8c454da628bb74af2d60280cff
Vadim Rozenfeld <vrozenfe@redhat.com>
2010-12-27 14:05:52 +0200

    [vioserial] UM lib test. Add port read test case.

--- vioserial/libtestapp/main.cpp
+++ vioserial/libtestapp/main.cpp
@@ -1,5 +1,73 @@
 #include "vioser.h"
 #include <stdio.h>
+#include "tchar.h"
+
+void ShowUsage()
+{
+    printf("\n");
+    printf("USAGE:\n");
+    printf("blnsvr -r\tRead Test\n");
+    printf("blnsvr -w\tWrite Test\n");
+    printf("blnsvr -i\tPort Status\n");
+    printf("\n");
+}
+
+
+VOID ReadTest( UINT id)
+{
+    PVOID port = OpenPortById(0);
+    if (port)
+    {
+        ULONG test_size = 1024*1024*2;
+        PBYTE data = (PBYTE) new BYTE[test_size];
+        memset (data, '\0', test_size);
+        if (!ReadPort(port, data, &test_size))
+        {
+           printf ("ReadTest Error.\n");
+        }
+        else if (test_size == 0)
+        {
+           printf ("No data!\n");
+        }
+        else
+        {
+           printf ("%s\n", data);
+        }
+        delete[] data;
+        return;
+    }
+    printf ("ReadTest Error. Invalid port index.\n");
+}
+
+VOID InfoTest( UINT id)
+{
+    PVOID port = OpenPortById(0);
+    if (port)
+    {
+        //
+        return;
+    }
+    printf ("InfoTest Error. Invalid port index.\n");
+}
+
+VOID WriteTest( UINT id)
+{
+    PVOID port = OpenPortById(0);
+    if (port)
+    {
+        ULONG test_size = 1024*1024*2;
+        PBYTE data = (PBYTE) new BYTE[test_size];
+        memset (data, '0', test_size);
+        if (!WritePort(port, data, test_size))
+        {
+           printf ("WriteTest Error.\n");
+        }
+        delete[] data;
+        return;
+    }
+    printf ("WriteTest Error. Invalid port index.\n");
+}
+
 
 ULONG
 _cdecl
@@ -10,19 +78,23 @@ wmain(
 {
     VIOSStartup();
     UINT ports = NumPorts();
-    printf ("we have %d port(s) in the system.\n", ports);
-    if (ports)
+    if(argc == 2 && ports > 0)
     {
-        PVOID port = OpenPortById(0);
-        if (port)
-        {
-           UINT test_size = 1024*1024*2;
-           PBYTE data = (PBYTE) new BYTE[test_size];
-           memset (data, '0', test_size);
-           WritePort(port, data, test_size);
-           delete[] data;
+        if (_tcsicmp(L"-r", argv[1]) == 0) {
+           ReadTest(0);
+        } else if (_tcsicmp(L"-w", argv[1]) == 0) {
+           WriteTest(0);
+        } else if (_tcsicmp(L"-i", argv[1]) == 0) {
+           InfoTest(0);
+        } else if (_tcsicmp(L"help", argv[1]) == 0) {
+           ShowUsage();
+        } else {
+           ShowUsage();
         }
+    } else {
+      printf ("we have %d port(s) in the system.\n", ports);
     }
+
     VIOSCleanup();
     return 0;
 }
