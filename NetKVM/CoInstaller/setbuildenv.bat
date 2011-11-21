b5bfe951c4d80e1eec18d4f3a0716fde2a8515bf
Yan Vugenfirer <yvugenfi@redhat.com>
2011-11-21 18:17:41 +0200

    [NetKVM] Enable to set external SDK path

--- NetKVM/CoInstaller/setbuildenv.bat
+++ NetKVM/CoInstaller/setbuildenv.bat
@@ -1,11 +1,19 @@
-::set PSDK_INC_PATH=C:\PROGRA~1\MI2578~1\Windows\v6.0A\Include
-::set PSDK_LIB_PATH=C:\PROGRA~1\MI2578~1\Windows\v6.0A\Lib
 @echo off
-set __sdk_path__="C:\Program Files\Microsoft SDKs\Windows\v6.0A"
+call :set_sdk_path
 call :setshortpath PSDK_INC_PATH %__sdk_path__%\Include
 call :setshortpath PSDK_LIB_PATH %__sdk_path__%\Lib
+call :clean_sdk_path
 goto :eof
 
 :setshortpath
 set %1=%~s2
+goto :eof
+
+:set_sdk_path
+set __sdk_path__="C:\Program Files\Microsoft SDKs\Windows\v6.0A"
+if NOT "%SDK_PATH%"=="" set __sdk_path__="%SDK_PATH%"
+goto :eof
+
+:clean_sdk_path
+set __sdk_path__=
 goto :eof
