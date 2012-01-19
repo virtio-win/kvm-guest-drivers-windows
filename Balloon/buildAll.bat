318fd25afe24440036060ceafd4a6cbe7c7272da
Yan Vugenfirer <yvugenfi@redhat.com>
2012-01-19 14:54:15 +0200

    [balloon] Cleanup environment variables after build for each platform.

--- Balloon/buildAll.bat
+++ Balloon/buildAll.bat
@@ -4,6 +4,8 @@ set SYS_FILE_NAME=balloon
 set APP_FILE_NAME=blnsvr
 
 for %%A in (Win7 Wnet Wlh WXp) do for %%B in (32 64) do call :%%A_%%B
+set SYS_FILE_NAME=
+set APP_FILE_NAME=
 goto :eof 
 
 :buildsys
@@ -40,45 +42,59 @@ set BUILD_ARC=
 goto :eof
 
 :WIN7_32
+setlocal
 set BUILD_OS=Win7
 set BUILD_ARC=x86
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WIN7_64
+setlocal
 set BUILD_OS=Win7
 set BUILD_ARC=x64
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WLH_32
+setlocal
 set BUILD_OS=Wlh
 set BUILD_ARC=x86
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WLH_64
+setlocal
 set BUILD_OS=Wlh
 set BUILD_ARC=x64
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WNET_32
+setlocal
 set BUILD_OS=Wnet
 set BUILD_ARC=x86
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WNET_64
+setlocal
 set BUILD_OS=Wnet
 set BUILD_ARC=x64
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WXP_32
+setlocal
 set BUILD_OS=WXp
 set BUILD_ARC=x86
 call :buildpack %BUILD_OS% %BUILD_ARC%
+endlocal
 goto :eof
 
 :WXP_64
