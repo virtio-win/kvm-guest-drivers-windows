66fc09ccddb59198ded8fd1cc1486649e77ee786
Yan Vugenfirer <yvugenfi@redhat.com>
2012-01-30 14:05:36 +0200

    [NetKVM] RSS: test for Toeplitz hash calculation

---
+++ NetKVM/DebugTools/RSS-Toeplitz/ReadMe.txt
@@ -0,0 +1,11 @@
+========================================================================
+    CONSOLE APPLICATION : RSS-Toeplitz 
+========================================================================
+
+See Toeplitz test vectors at
+http://msdn.microsoft.com/en-us/windows/hardware/ff571021
+
+Currently only little endian version.
+
+TODO: measurement and optimization
+TODO: big endian when it will be actual
---
+++ NetKVM/DebugTools/RSS-Toeplitz/WinToeplitz.h
@@ -0,0 +1,22 @@
+#ifndef win_toeplitz_h
+#define win_toeplitz_h
+
+#ifndef EXTERN_C
+#ifdef __cplusplus
+#define EXTERN_C extern "C"
+#else
+#define EXTERN_C 
+#endif
+#endif
+
+#define WTEP_MAX_KEY_SIZE	40
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
+
+EXTERN_C void toeplitzw_initialize(uint8_t *key, int keysize);
+EXTERN_C uint32_t toeplitzw_hash(const uint8_t *vector, int len);
+
+
+#endif
