a39d95d7135625e81d9e864d074ba80f16e37b9c
Vadim Rozenfeld <vrozenfe@redhat.com>
2012-07-26 08:59:35 +0300

    [NetKvm] adjust CoInstaller build script.

--- NetKVM/CoInstaller/setbuildenv.bat
+++ NetKVM/CoInstaller/setbuildenv.bat
@@ -11,7 +11,7 @@ goto :eof
 
 :set_sdk_path
 set __sdk_path__="C:\Program Files\Microsoft SDKs\Windows\v6.0A"
-if not exist %__sdk_path__% set __sdk_path__="C:\Program Files\Microsoft SDKs\Windows\v7.0A"
+if not exist %__sdk_path__% set __sdk_path__="C:\Program Files (x86)\Microsoft SDKs\Windows\v7.0A"
 if NOT "%SDK_PATH%"=="" set __sdk_path__="%SDK_PATH%"
 goto :eof
 
