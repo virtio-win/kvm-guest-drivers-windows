f340a460852364ba511e84a6c31121e2fce9ffb2
Vadim Rozenfeld <vrozenfe@redhat.com>
2012-08-14 06:09:38 +0300

    [viostor & vioscsi] fix win8 build

--- vioscsi/buildAll.bat
+++ vioscsi/buildAll.bat
@@ -20,21 +20,29 @@ set BUILD_OS=
 set BUILD_ARC=
 goto :eof
 
+:create2012H
+echo #define _NT_TARGET_MAJ %_NT_TARGET_MAJ%
+echo #define _NT_TARGET_MIN %_NT_TARGET_MIN%
+echo #define _MAJORVERSION_ %_MAJORVERSION_%
+echo #define _MINORVERSION_ %_MINORVERSION_%
+goto :eof
+
+
 :BuildUsing2012
-reg query "HKLM\Software\Wow6432Node\Microsoft\Windows Kits\WDK" /v WDKProductVersion > nul
-if %ERRORLEVEL% EQU 0 goto BuildUsing2012_WDKOK
-echo ERROR building Win8 drivers: Win8 WDK is not installed
-cd .
-goto :eof
-:BuildUsing2012_WDKOK
-reg query HKLM\Software\Wow6432Node\Microsoft\VisualStudio\11.0 /v InstallDir > nul
-if %ERRORLEVEL% EQU 0 goto BuildUsing2012_VS11OK
-echo ERROR building Win8 drivers: VS11 is not installed
-cd .
-goto :eof
-:BuildUsing2012_VS11OK
-cscript ..\tools\callVisuaStudio.vbs 11 vioscsi.vcxproj /Rebuild "%~1" /Out %2
-if %ERRORLEVEL% GEQ 1 echo VS2011 Build of "%~1" FAILED
+if "%_NT_TARGET_VERSION%"=="" set _NT_TARGET_VERSION=0x602
+if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
+if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
+if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
+set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_% 
+
+set _MAJORVERSION_=%_BUILD_MAJOR_VERSION_%
+set _MINORVERSION_=%_BUILD_MINOR_VERSION_%
+set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
+set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
+
+call :create2012H  > vioscsi-2012.h
+call ..\tools\callVisualStudio.bat 11 vioscsi.vcxproj /Rebuild "%~1" /Out %2
+del vioscsi-2012.h
 goto :eof
 
 :WIN8_32
@@ -42,6 +50,13 @@ setlocal
 set BUILD_OS=Win8
 set BUILD_ARC=x86
 set INF2CAT_PATH=
+
+if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
+if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
+if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
+set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
+set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_% 
+
 if exist Install\win8\x86 rmdir Install\win8\x86 /s /q
 call :BuildUsing2012 "Win8 Release|Win32" buildfre_win8_x86.log
 call packOne.bat %BUILD_OS% %BUILD_ARC% %SYS_FILE_NAME%
@@ -52,6 +67,8 @@ goto :eof
 setlocal
 set BUILD_OS=Win8
 set BUILD_ARC=x64
+set INF2CAT_PATH=
+
 if exist Install\win8\amd64 rmdir Install\win8\amd64 /s /q
 call :BuildUsing2012 "Win8 Release|x64" buildfre_win8_amd64.log
 call packOne.bat %BUILD_OS% %BUILD_ARC% %SYS_FILE_NAME%
@@ -82,14 +99,16 @@ set BUILD_ARC=x64
 call :buildpack %BUILD_OS% %BUILD_ARC%
 goto :eof
 
+:WNET_64
+set BUILD_OS=Wnet
+set BUILD_ARC=x64
+call :buildpack %BUILD_OS% %BUILD_ARC%
+goto :eof
+
+
 :WNET_32
 set BUILD_OS=Wnet
 set BUILD_ARC=x86
 call :buildpack %BUILD_OS% %BUILD_ARC%
 goto :eof
 
-:WNET_64
-set BUILD_OS=Wnet
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-goto :eof
