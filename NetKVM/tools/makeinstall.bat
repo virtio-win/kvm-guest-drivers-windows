fe61f6e634f41b8c9d3efb9b9eb79075c8912ab5
Yan Vugenfirer <yvugenfi@redhat.com>
2012-08-23 14:55:46 +0200

    [NetKVM] BZ #838008 added Port1FunctionNumber directive to the INF file for Win8/2012

--- NetKVM/tools/makeinstall.bat
+++ NetKVM/tools/makeinstall.bat
@@ -13,7 +13,7 @@ goto :eof
 
 :prepareinf
 ::original inf %1, copy to %2, define OS %3
-echo processing %1
+echo processing %1 for %3
 cl /nologo -DINCLUDE_TEST_PARAMS -D%3 /I. /EP %1 > %~nx1
 echo cleaning INF file...
 cscript /nologo tools\cleanemptystrings.vbs %~nx1 > %2\%~nx1
