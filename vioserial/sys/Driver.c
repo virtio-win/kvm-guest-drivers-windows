8af62381dcdb921e376f529cde36f3ca105ba18c
Vadim Rozenfeld <vrozenfe@redhat.com>
2012-09-28 05:06:40 +0200

    [vioserial] Add suport for Non-Execute NonPaged Pool on Win8

--- vioserial/sys/Driver.c
+++ vioserial/sys/Driver.c
@@ -38,6 +38,10 @@ NTSTATUS DriverEntry(IN PDRIVER_OBJECT  DriverObject,
     WDF_DRIVER_CONFIG      config;
     WDF_OBJECT_ATTRIBUTES  attributes;
 
+
+#if (NTDDI_VERSION > NTDDI_WIN7)
+    ExInitializeDriverRuntime(DrvRtPoolNxOptIn);
+#endif
     WPP_INIT_TRACING(DriverObject, RegistryPath);
 
     TraceEvents(TRACE_LEVEL_INFORMATION, DBG_INIT,
@@ -71,11 +75,10 @@ VIOSerialEvtDriverContextCleanup(
     IN WDFDRIVER Driver
     )
 {
-    PDRIVER_OBJECT  drvObj = WdfDriverWdmGetDriverObject( Driver );
     PAGED_CODE ();
 
     TraceEvents(TRACE_LEVEL_INFORMATION, DBG_PNP, "<--> %s\n", __FUNCTION__);
 
-    WPP_CLEANUP( drvObj);
+    WPP_CLEANUP( WdfDriverWdmGetDriverObject(Driver) );
 }
 
