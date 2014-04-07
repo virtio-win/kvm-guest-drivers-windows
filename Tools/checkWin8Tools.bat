0b83c731cb20ae8d5ac4b834347f04367d28c014
Yan Vugenfirer <yvugenfi@redhat.com>
2014-04-07 17:33:17 +0300

    Don't test specific DDK version

--- Tools/checkWin8Tools.bat
+++ Tools/checkWin8Tools.bat
@@ -1,12 +1,5 @@
 @echo off
 
-reg query "HKLM\Software\Microsoft\Windows Kits\WDK" /v WDKProductVersion >nul 2>nul
-if %ERRORLEVEL% EQU 0 goto checkVS11
-reg query "HKLM\Software\Wow6432Node\Microsoft\Windows Kits\WDK" /v WDKProductVersion > nul 2>nul
-if %ERRORLEVEL% EQU 0 goto checkVS11
-echo ERROR building Win8 drivers: Win8 WDK is not installed
-exit /b 1
-
 :checkVS11
 reg query HKLM\Software\Microsoft\VisualStudio\%1.0 /v InstallDir > nul 2>nul
 if %ERRORLEVEL% EQU 0 exit /b 0
