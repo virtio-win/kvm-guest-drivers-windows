81fc09fdc8acad4325833002e12c80639827a9b5
Joseph Hindin <jhindin@daynix.com>
2014-06-30 11:40:32 +0300

    NetKVM: BZ#1096505: Protecting RSS parameters access with the RW lock
    Signed-off-by: Joseph Hindin <jhindin@daynix.com>

--- NetKVM/wlh/ParaNdis6-Impl.cpp
+++ NetKVM/wlh/ParaNdis6-Impl.cpp
@@ -704,12 +704,16 @@ static __inline
 VOID NBLSetRSSInfo(PPARANDIS_ADAPTER pContext, PNET_BUFFER_LIST pNBL, PNET_PACKET_INFO PacketInfo)
 {
 #if PARANDIS_SUPPORT_RSS
+    CNdisRWLockState lockState;
+
+    pContext->RSSParameters.rwLock.acquireReadDpr(lockState);
     if(pContext->RSSParameters.RSSMode != PARANDIS_RSS_DISABLED)
     {
         NET_BUFFER_LIST_SET_HASH_TYPE    (pNBL, PacketInfo->RSSHash.Type);
         NET_BUFFER_LIST_SET_HASH_FUNCTION(pNBL, PacketInfo->RSSHash.Function);
         NET_BUFFER_LIST_SET_HASH_VALUE   (pNBL, PacketInfo->RSSHash.Value);
     }
+    pContext->RSSParameters.rwLock.releaseDpr(lockState);
 #else
     UNREFERENCED_PARAMETER(pContext);
     UNREFERENCED_PARAMETER(pNBL);
