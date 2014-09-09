167ee2e2b8849dcd02d723f56de05918d0933786
Gal Hammer <ghammer@redhat.com>
2014-09-09 15:35:42 +0300

    rng: bz#1091867: initial import of the viorng coinstaller.
    Add a coinstaller which handles the CNG provider registration.

---
+++ viorng/coinstaller/viorngci.c
@@ -0,0 +1,188 @@
+/*
+ * Copyright (C) 2014 Red Hat, Inc.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA.
+ *
+ * Refer to the LICENSE file for full details of the license.
+ *
+ * Written By: Gal Hammer <ghammer@redhat.com>
+ *
+ */
+
+#define STRICT
+#include <windows.h>
+#include <bcrypt.h>
+#include <setupapi.h>
+#include <tchar.h>
+
+#include <stdio.h>
+
+#include "viorngci.h"
+
+#define VIRTRNG_IMAGE_NAME L"viorngum.dll"
+
+PWSTR VirtRngAlgorithmNames[1] = {
+    BCRYPT_RNG_ALGORITHM
+};
+
+CRYPT_INTERFACE_REG VirtRngInterface = {
+    BCRYPT_RNG_INTERFACE, CRYPT_LOCAL, 1, VirtRngAlgorithmNames
+};
+
+PCRYPT_INTERFACE_REG VirtRngInterfaces[1] = {
+    &VirtRngInterface
+};
+
+CRYPT_IMAGE_REG VirtRngImage = {
+    VIRTRNG_IMAGE_NAME, 1, VirtRngInterfaces
+};
+
+CRYPT_PROVIDER_REG VirtRngProvider = {
+    0, NULL, &VirtRngImage, NULL
+};
+
+typedef ULONG (WINAPI* RtlNtStatusToDosErrorFunc)(IN NTSTATUS status);
+
+static void ReportError(const wchar_t *format, ...)
+{
+    WCHAR err[0x1000];
+    va_list list;
+
+    va_start(list, format);
+    vswprintf(err, sizeof(err), format, list);
+
+    MessageBox(HWND_DESKTOP, err, VIRTRNG_PROVIDER_NAME, MB_ICONERROR|MB_OK);
+}
+
+static DWORD ToDosError(NTSTATUS status)
+{
+    DWORD error = NO_ERROR;
+    HMODULE ntdll;
+    RtlNtStatusToDosErrorFunc RtlNtStatusToDosError;
+
+    ntdll = LoadLibrary(L"Ntdll.dll");
+    if (ntdll != NULL)
+    {
+        RtlNtStatusToDosError = (RtlNtStatusToDosErrorFunc)
+            GetProcAddress(ntdll, "RtlNtStatusToDosError");
+
+        if (RtlNtStatusToDosError != NULL)
+        {
+            error = RtlNtStatusToDosError(status);
+        }
+        else
+        {
+            error = GetLastError();
+            ReportError(L"Function not found.\nError code: 0x%08x.", error);
+        }
+    }
+    else
+    {
+        error = GetLastError();
+        ReportError(L"Failed to load ntdll.dll.\nError code: 0x%08x.", error);
+    }
+
+    return error;
+}
+
+NTSTATUS WINAPI RegisterProvider(BOOLEAN KernelMode)
+{
+    NTSTATUS status;
+
+    UNREFERENCED_PARAMETER(KernelMode);
+
+    status = BCryptRegisterProvider(VIRTRNG_PROVIDER_NAME, 0,
+        &VirtRngProvider);
+
+    if (!NT_SUCCESS(status))
+    {
+        ReportError(L"Failed to register as a CNG provider.\n"
+            L"Error code: 0x%08x.", status);
+        return status;
+    }
+
+    status = BCryptAddContextFunctionProvider(CRYPT_LOCAL, NULL,
+        BCRYPT_RNG_INTERFACE, BCRYPT_RNG_ALGORITHM, VIRTRNG_PROVIDER_NAME,
+        CRYPT_PRIORITY_BOTTOM);
+
+    if (!NT_SUCCESS(status))
+    {
+        ReportError(L"Failed to add cryptographic function.\n"
+            L"Error code: 0x%08x.", status);
+    }
+
+    return status;
+}
+
+NTSTATUS WINAPI UnregisterProvider()
+{
+    NTSTATUS status;
+
+    status = BCryptRemoveContextFunctionProvider(CRYPT_LOCAL, NULL,
+        BCRYPT_RNG_INTERFACE, BCRYPT_RNG_ALGORITHM, VIRTRNG_PROVIDER_NAME);
+
+    if (!NT_SUCCESS(status))
+    {
+        ReportError(L"Failed to remove cryptographic function.\n"
+            L"Error code: 0x%08x.", status);
+    }
+
+    status = BCryptUnregisterProvider(VIRTRNG_PROVIDER_NAME);
+    if (!NT_SUCCESS(status))
+    {
+        ReportError(L"Failed to unregister as a CNG provider.\n"
+            L"Error code: 0x%08x.", status);
+    }
+
+    return status;
+}
+
+DWORD CALLBACK VirtRngCoInstaller(IN DI_FUNCTION InstallFunction,
+                                  IN HDEVINFO DeviceInfoSet,
+                                  IN PSP_DEVINFO_DATA DeviceInfoData OPTIONAL,
+                                  IN OUT PCOINSTALLER_CONTEXT_DATA Context)
+{
+    NTSTATUS status;
+    DWORD error = NO_ERROR;
+
+    UNREFERENCED_PARAMETER(DeviceInfoSet);
+    UNREFERENCED_PARAMETER(DeviceInfoData);
+    UNREFERENCED_PARAMETER(Context);
+
+    switch (InstallFunction)
+    {
+        case DIF_INSTALLDEVICE:
+            status = RegisterProvider(FALSE);
+            if (!NT_SUCCESS(status))
+            {
+                error = ToDosError(status);
+            }
+            break;
+
+        case DIF_REMOVE:
+            status = UnregisterProvider();
+            if (!NT_SUCCESS(status))
+            {
+                error = ToDosError(status);
+            }
+            break;
+
+        default:
+            break;
+    }
+
+    return error;
+}
