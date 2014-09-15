d12d9a2259eb891a0a015caf703806ba47acecff
Joseph Hindin <yhindin@rehat.com>
2014-09-15 20:06:55 +0300

    NetKVM: BZ#1136606: testing MessageInfoTable for nullness in debug printout
    Signed-off-by: Joseph Hindin <yhindin@rehat.com>

--- NetKVM/wlh/ParaNdis6-Impl.cpp
+++ NetKVM/wlh/ParaNdis6-Impl.cpp
@@ -601,7 +601,17 @@ NDIS_STATUS ParaNdis_FinishSpecificInitialization(PARANDIS_ADAPTER *pContext)
         status = NdisMRegisterInterruptEx(pContext->MiniportHandle, pContext, &mic, &pContext->InterruptHandle);
     }
 
-    DPrintf(0, ("[%s] MSIX message count = %u\n", __FUNCTION__, mic.MessageInfoTable->MessageCount));
+#ifdef DBG
+    if (pContext->bUsingMSIX)
+    {
+        DPrintf(0, ("[%s] MSIX message table %savailable, count = %u\n", __FUNCTION__, (mic.MessageInfoTable == nullptr ? "not " : ""),
+            (mic.MessageInfoTable == nullptr ? 0 : mic.MessageInfoTable->MessageCount)));
+    }
+    else
+    {
+        DPrintf(0, ("[%s] Not using MSIX\n", __FUNCTION__));
+    }
+#endif
 
     if (status == NDIS_STATUS_SUCCESS)
     {
