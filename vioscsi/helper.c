b7e34a8a73f953fa6fc407384baba4acddf5e16a
Vadim Rozenfeld <vrozenfe@redhat.com>
2014-11-12 14:22:12 +1100

    [vioscsi] simplify virtio queues memory allocation and management
    (cherry picked from commit 6edd0311afe4162937b9c627b287a6e420f1a4ee)
    
    Conflicts:
    	vioscsi/helper.c

--- vioscsi/helper.c
+++ vioscsi/helper.c
@@ -37,19 +37,18 @@ SynchronizedSRBRoutine(
     BOOLEAN             ret = FALSE;
 ENTER_FN();
     SET_VA_PA();
-    if (virtqueue_add_buf(adaptExt->vq[2],
+    if (virtqueue_add_buf(adaptExt->vq[VIRTIO_SCSI_REQUEST_QUEUE_0],
                      &srbExt->sg[0],
                      srbExt->out, srbExt->in,
                      &srbExt->cmd, va, pa) >= 0){
-        ret = TRUE;
+        virtqueue_kick(adaptExt->vq[VIRTIO_SCSI_REQUEST_QUEUE_0]);
+        return TRUE;
     }
-    else
-    {
-        InsertTailList(&adaptExt->list_head, &srbExt->list_entry);
-    }
-    virtqueue_kick(adaptExt->vq[2]);
-EXIT_FN();
-    return ret;
+    Srb->SrbStatus = SRB_STATUS_BUSY;
+    StorPortBusy(DeviceExtension, 2);
+    virtqueue_kick(adaptExt->vq[VIRTIO_SCSI_REQUEST_QUEUE_0]);
+EXIT_ERR();
+    return FALSE;
 }
 
 VOID
@@ -95,11 +94,11 @@ SynchronizedTMFRoutine(
 
 ENTER_FN();
     SET_VA_PA();
-    if (virtqueue_add_buf(adaptExt->vq[0],
+    if (virtqueue_add_buf(adaptExt->vq[VIRTIO_SCSI_CONTROL_QUEUE],
                      &srbExt->sg[0],
                      srbExt->out, srbExt->in,
                      &srbExt->cmd, va, pa) >= 0){
-        virtqueue_kick(adaptExt->vq[0]);
+        virtqueue_kick(adaptExt->vq[VIRTIO_SCSI_CONTROL_QUEUE]);
         return TRUE;
     }
     Srb->SrbStatus = SRB_STATUS_BUSY;
@@ -169,7 +168,7 @@ ShutDown(
     IN PVOID DeviceExtension
     )
 {
-    USHORT cnt = 0;
+    ULONG index;
     PADAPTER_EXTENSION adaptExt = (PADAPTER_EXTENSION)DeviceExtension;
 ENTER_FN();
     while (!IsListEmpty(&adaptExt->list_head) && cnt < 100) {
@@ -178,20 +177,12 @@ ENTER_FN();
     }
     VirtIODeviceReset(&adaptExt->vdev);
     StorPortWritePortUshort(DeviceExtension, (PUSHORT)(adaptExt->device_base + VIRTIO_PCI_GUEST_FEATURES), 0);
-    if (adaptExt->vq[0]) {
-       virtqueue_shutdown(adaptExt->vq[0]);
-       VirtIODeviceDeleteQueue(adaptExt->vq[0], NULL);
-       adaptExt->vq[0] = NULL;
-    }
-    if (adaptExt->vq[1]) {
-       virtqueue_shutdown(adaptExt->vq[1]);
-       VirtIODeviceDeleteQueue(adaptExt->vq[1], NULL);
-       adaptExt->vq[1] = NULL;
-    }
-    if (adaptExt->vq[2]) {
-       virtqueue_shutdown(adaptExt->vq[2]);
-       VirtIODeviceDeleteQueue(adaptExt->vq[2], NULL);
-       adaptExt->vq[2] = NULL;
+    for (index = VIRTIO_SCSI_CONTROL_QUEUE; index < VIRTIO_SCSI_QUEUE_LAST; ++index) {
+        if (adaptExt->vq[index]) {
+            virtqueue_shutdown(adaptExt->vq[index]);
+            VirtIODeviceDeleteQueue(adaptExt->vq[index], NULL);
+            adaptExt->vq[index] = NULL;
+        }
     }
 EXIT_FN();
 }
@@ -294,11 +285,11 @@ SynchronizedKickEventRoutine(
     ULONGLONG           pa = 0;
 
 ENTER_FN();
-    if (virtqueue_add_buf(adaptExt->vq[1],
+    if (virtqueue_add_buf(adaptExt->vq[VIRTIO_SCSI_EVENTS_QUEUE],
                      &eventNode->sg,
                      0, 1,
                      eventNode, va, pa) >= 0){
-        virtqueue_kick(adaptExt->vq[1]);
+        virtqueue_kick(adaptExt->vq[VIRTIO_SCSI_EVENTS_QUEUE]);
         return TRUE;
     }
 EXIT_ERR();
