590267a404b33e2875268e535e869ce866e77ac8
Gal Hammer <ghammer@redhat.com>
2015-01-05 16:26:18 +0200

    balloon: bz#1177063: fixed bsod on s3/s4 when service was running.
    The driver didn't handle the EvtIoStop so the pending write request was
    not handled during sleep/hibernate.

--- Balloon/sys/queue.c
+++ Balloon/sys/queue.c
@@ -1,6 +1,7 @@
 #include "precomp.h"
 
 EVT_WDF_IO_QUEUE_IO_WRITE BalloonIoWrite;
+EVT_WDF_IO_QUEUE_IO_STOP BalloonIoStop;
 EVT_WDF_REQUEST_CANCEL BalloonEvtRequestCancel;
 
 #ifdef ALLOC_PRAGMA
@@ -17,11 +18,10 @@ BalloonQueueInitialize(
 
     PAGED_CODE();
 
-    WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(
-                 &queueConfig,
-                 WdfIoQueueDispatchSequential);
-
-    queueConfig.EvtIoWrite  = BalloonIoWrite;
+    WDF_IO_QUEUE_CONFIG_INIT_DEFAULT_QUEUE(&queueConfig,
+        WdfIoQueueDispatchSequential);
+    queueConfig.EvtIoWrite = BalloonIoWrite;
+    queueConfig.EvtIoStop = BalloonIoStop;
 
     status = WdfIoQueueCreate(
                  Device,
@@ -104,6 +104,30 @@ BalloonIoWrite(
 
         devCtx->PendingWriteRequest = Request;
     }
+}
+
+VOID BalloonIoStop(IN WDFQUEUE Queue,
+                   IN WDFREQUEST Request,
+                   IN ULONG ActionFlags)
+{
+    UNREFERENCED_PARAMETER(Queue);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_READ,
+        "--> %!FUNC! Request: %p", Request);
+
+    if (ActionFlags & WdfRequestStopActionSuspend)
+    {
+        WdfRequestStopAcknowledge(Request, FALSE);
+    }
+    else if (ActionFlags & WdfRequestStopActionPurge)
+    {
+        if (WdfRequestUnmarkCancelable(Request) != STATUS_CANCELLED)
+        {
+            WdfRequestComplete(Request, STATUS_CANCELLED);
+        }
+    }
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_READ, "<-- %!FUNC!");
 }
 
 VOID BalloonEvtRequestCancel(IN WDFREQUEST Request)
