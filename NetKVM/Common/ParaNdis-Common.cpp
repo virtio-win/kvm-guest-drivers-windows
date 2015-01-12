3860f7b7ffc8f78e87b0beb226954cdc9004f413
Joseph Hindin <yhindin@rehat.com>
2015-01-12 20:26:14 +0200

    NetKVM: BZ#1147239: Initializing CPathBundle instances and their flags before path creation

--- NetKVM/Common/ParaNdis-Common.cpp
+++ NetKVM/Common/ParaNdis-Common.cpp
@@ -1034,7 +1034,10 @@ static NDIS_STATUS ParaNdis_VirtIONetInit(PARANDIS_ADAPTER *pContext)
     for (i = 0; i < pContext->nPathBundles; i++)
     {
         new (pContext->pPathBundles + i, PLACEMENT_NEW) CPUPathesBundle();
+    }
 
+    for (i = 0; i < pContext->nPathBundles; i++)
+    {
         if (!pContext->pPathBundles[i].rxPath.Create(pContext, i * 2))
         {
             DPrintf(0, ("%s: CParaNdisRX creation failed\n", __FUNCTION__));
