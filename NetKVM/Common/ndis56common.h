virtio-win/kvm-guest-drivers-windows

commit 51375abb69069d9681856ecea149082d22802c3c

NetKVM: BZ#1190960: Global receive queue eliminated
Before the patch 'Routing unclassified packets through RX path's receive
queue', unclassified packets were routed through per-context receive queue,
placed into ReceiveQueues array at index 0. The patch 'Routing
unclassified packets through RX path's receive queue' changes
unclassified packets routing so that they are placed into a dedicated
receive queue, one per virtqueue/CParaNdisRX instance. This patch
completes the change by removing per-context receive queue; first
element in ReceiveQueues array has not special meaning and the
constant 'PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED'  was split
into 'PARANDIS_RECEIVE_UNCLASSIFIED_PACKET' and
'PARANDIS_RECEIVE_NO_QUEUE', depending on context.

Signed-off-by: Joseph Hindin <yhindin@rehat.com>

NetKVM/Common/ndis56common.h
@@ -469,10 +469,11 @@ typedef struct _tagPARANDIS_ADAPTER
     tOffloadSettings        Offload;
     NDIS_OFFLOAD_PARAMETERS InitialOffloadParameters;
 
-    PARANDIS_RECEIVE_QUEUE      ReceiveQueues[PARANDIS_RSS_MAX_RECEIVE_QUEUES + 1];
+    PARANDIS_RECEIVE_QUEUE      ReceiveQueues[PARANDIS_RSS_MAX_RECEIVE_QUEUES];
     BOOLEAN                     ReceiveQueuesInitialized;
-#define PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED (0)
-#define PARANDIS_FIRST_RSS_RECEIVE_QUEUE    (1)
+#define PARANDIS_RECEIVE_UNCLASSIFIED_PACKET (-1)
+#define PARANDIS_RECEIVE_NO_QUEUE  (-2)
+#define PARANDIS_FIRST_RSS_RECEIVE_QUEUE    (0)
 
     CParaNdisCX CXPath;
     BOOLEAN bCXPathAllocated;

NetKVM/Common/ParaNdis-Common.cpp
@@ -1451,7 +1451,7 @@ CCHAR ParaNdis_GetScalingDataForPacket(PARANDIS_ADAPTER *pContext, PNET_PACKET_I
     UNREFERENCED_PARAMETER(pPacketInfo);
     UNREFERENCED_PARAMETER(pTargetProcessor);
 
-    return PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+    return PARANDIS_RECEIVE_UNCLASSIFIED_PACKET;
 #endif
 }
 
@@ -1463,7 +1463,7 @@ CCHAR GetReceiveQueueForCurrentCPU(PARANDIS_ADAPTER *pContext)
 #else
     UNREFERENCED_PARAMETER(pContext);
 
-    return PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+    return PARANDIS_RECEIVE_NO_QUEUE;
 #endif
 }
 
@@ -1655,7 +1655,7 @@ BOOLEAN RxDPCWorkBody(PARANDIS_ADAPTER *pContext, CPUPathesBundle *pathBundle, U
                     &indicate, &indicateTail, &nIndicate);
             }
 
-            if(CurrCpuReceiveQueue != PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
+            if (CurrCpuReceiveQueue != PARANDIS_RECEIVE_NO_QUEUE)
             {
                 res |= ProcessReceiveQueue(pContext, &nPacketsToIndicate, &pContext->ReceiveQueues[CurrCpuReceiveQueue],
                     &indicate, &indicateTail, &nIndicate);

NetKVM/Common/ParaNdis-RX.cpp
@@ -222,7 +222,7 @@ VOID CParaNdisRX::ProcessRxRing(CCHAR nCurrCpuReceiveQueue)
             &pBufferDescriptor->PacketInfo,
             &TargetProcessor);
 
-        if (nTargetReceiveQueueNum == PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
+        if (nTargetReceiveQueueNum == PARANDIS_RECEIVE_UNCLASSIFIED_PACKET)
         {
             ParaNdis_ReceiveQueueAddBuffer(&m_UnclassifiedPacketsQueue, pBufferDescriptor);
         }

NetKVM/wlh/ParaNdis6-RSS.cpp
@@ -129,7 +129,7 @@ CCHAR FindReceiveQueueForCurrentCpu(PPARANDIS_SCALING_SETTINGS RSSScalingSetting
     ASSERT(CurrProcIdx != INVALID_PROCESSOR_INDEX);
 
     if(CurrProcIdx >= RSSScalingSettings->CPUIndexMappingSize)
-        return PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+        return PARANDIS_RECEIVE_NO_QUEUE;
 
     return RSSScalingSettings->CPUIndexMapping[CurrProcIdx];
 }
@@ -153,7 +153,7 @@ BOOLEAN AllocateCPUMappingArray(NDIS_HANDLE NdisHandle, PPARANDIS_SCALING_SETTIN
 
     for(i = 0; i < CPUNumber; i++)
     {
-        RSSScalingSettings->CPUIndexMapping[i] = PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+        RSSScalingSettings->CPUIndexMapping[i] = PARANDIS_RECEIVE_NO_QUEUE;
     }
 
     return TRUE;
@@ -177,7 +177,7 @@ VOID FillCPUMappingArray(
 
         if(CurrProcIdx != INVALID_PROCESSOR_INDEX)
         {
-            if (RSSScalingSettings->CPUIndexMapping[CurrProcIdx] == PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
+            if (RSSScalingSettings->CPUIndexMapping[CurrProcIdx] == PARANDIS_RECEIVE_NO_QUEUE)
             {
                 if (ReceiveQueue == PARANDIS_FIRST_RSS_RECEIVE_QUEUE)
                     RSSScalingSettings->FirstQueueIndirectionIndex = i;
@@ -193,7 +193,7 @@ VOID FillCPUMappingArray(
         }
         else
         {
-            RSSScalingSettings->QueueIndirectionTable[i] = PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+            RSSScalingSettings->QueueIndirectionTable[i] = PARANDIS_RECEIVE_NO_QUEUE;
         }
     }
 
@@ -205,7 +205,7 @@ VOID FillCPUMappingArray(
 
     for (i = 0; i < RSSScalingSettings->IndirectionTableSize / sizeof(PROCESSOR_NUMBER); i++)
     {
-        if (RSSScalingSettings->QueueIndirectionTable[i] == PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
+        if (RSSScalingSettings->QueueIndirectionTable[i] == PARANDIS_RECEIVE_NO_QUEUE)
         {
             /* If some hash values remains unassigned after the first pass, either because
             mapping processor number to index failed or there are not enough queues,
@@ -584,27 +584,25 @@ CCHAR ParaNdis6_RSSGetScalingDataForPacket(
     if (RSSParameters->RSSMode != PARANDIS_RSS_FULL || 
         RSSParameters->ActiveRSSScalingSettings.FirstQueueIndirectionIndex == INVALID_INDIRECTION_INDEX)
     {
-        targetQueue = PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+        targetQueue = PARANDIS_RECEIVE_UNCLASSIFIED_PACKET;
     }
     else if (packetInfo->RSSHash.Type == 0)
     {
-        /* In the RSS mode, unclassified packets are dispatched to the first queue, in order to preserve the order.
-           Hopefully, there are few unclassfied packets */
-        if (RSSParameters->ActiveRSSScalingSettings.IndirectionTableSize)
-        {
-            *targetProcessor = RSSParameters->ActiveRSSScalingSettings.IndirectionTable[RSSParameters->ActiveRSSScalingSettings.FirstQueueIndirectionIndex];
-            targetQueue = RSSParameters->ActiveRSSScalingSettings.QueueIndirectionTable[RSSParameters->ActiveRSSScalingSettings.FirstQueueIndirectionIndex];
-        }
-        else
-        {
-            targetQueue = PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
-        }
+        targetQueue = PARANDIS_RECEIVE_UNCLASSIFIED_PACKET;
     }
     else
     {
         ULONG indirectionIndex = packetInfo->RSSHash.Value & RSSParameters->ActiveRSSScalingSettings.RSSHashMask;
-        *targetProcessor = RSSParameters->ActiveRSSScalingSettings.IndirectionTable[indirectionIndex];
+
         targetQueue = RSSParameters->ActiveRSSScalingSettings.QueueIndirectionTable[indirectionIndex];
+        if (targetQueue == PARANDIS_RECEIVE_NO_QUEUE)
+        {
+            targetQueue = PARANDIS_RECEIVE_UNCLASSIFIED_PACKET;
+        }
+        else
+        {
+            *targetProcessor = RSSParameters->ActiveRSSScalingSettings.IndirectionTable[indirectionIndex];
+        }
     }
 
     return targetQueue;
@@ -617,7 +615,7 @@ CCHAR ParaNdis6_RSSGetCurrentCpuReceiveQueue(PARANDIS_RSS_PARAMS *RSSParameters)
 
     if(RSSParameters->RSSMode != PARANDIS_RSS_FULL)
     {
-        res = PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED;
+        res = PARANDIS_RECEIVE_NO_QUEUE;
     }
     else
     {

commit 7c86592acb27b0a339e9959c74c2b9519b885cd8

NetKVM: BZ#1190960: Vista compilation fix
The Vista compilation process was broken by commit 51375abb: there
is no global ReceiveQueue, number of received queues in the absance of RSS
became zero and Visual++ dosn't support non-standard zero-sized arrays.
The commit put ReceiveQueues field under #ifdef; rest of the commits
in this series place ReceiveQueues usage under #ifdef

Signed-off-by: Joseph Hindin <yhindin@rehat.com>

NetKVM/Common/ndis56common.h
@@ -469,11 +469,13 @@ typedef struct _tagPARANDIS_ADAPTER
     tOffloadSettings        Offload;
     NDIS_OFFLOAD_PARAMETERS InitialOffloadParameters;
 
+#ifdef PARANDIS_SUPPORT_RSS
     PARANDIS_RECEIVE_QUEUE      ReceiveQueues[PARANDIS_RSS_MAX_RECEIVE_QUEUES];
     BOOLEAN                     ReceiveQueuesInitialized;
+#define PARANDIS_FIRST_RSS_RECEIVE_QUEUE    (0)
+#endif
 #define PARANDIS_RECEIVE_UNCLASSIFIED_PACKET (-1)
 #define PARANDIS_RECEIVE_NO_QUEUE  (-2)
-#define PARANDIS_FIRST_RSS_RECEIVE_QUEUE    (0)
 
     CParaNdisCX CXPath;
     BOOLEAN bCXPathAllocated;