virtio-win/kvm-guest-drivers-windows

commit 1e4c959e3407bd4a8ac49d442bcec8ebde98da39

NetKVM: BZ#1147239: Indicating outside of ProcessReceiveQueue function
Signed-off-by: Joseph Hindin <yhindin@rehat.com>

NetKVM/Common/ParaNdis-Common.cpp
@@ -1273,6 +1273,8 @@ VOID ParaNdis_CleanupContext(PARANDIS_ADAPTER *pContext)
         }
     }
 
+    pContext->m_PauseLock.~CNdisRWLock();
+
 #if PARANDIS_SUPPORT_RSS
     if (pContext->bRSSInitialized)
     {
@@ -1523,13 +1525,14 @@ UpdateReceiveFailStatistics(PPARANDIS_ADAPTER pContext, UINT nCoalescedSegmentsC
 
 static BOOLEAN ProcessReceiveQueue(PARANDIS_ADAPTER *pContext,
                                     PULONG pnPacketsToIndicateLeft,
-                                    CCHAR nQueueIndex)
+                                    CCHAR nQueueIndex,
+                                    PNET_BUFFER_LIST *indicate,
+                                    PNET_BUFFER_LIST *indicateTail,
+                                    ULONG *nIndicate)
 {
     
     pRxNetDescriptor pBufferDescriptor;
     PPARANDIS_RECEIVE_QUEUE pTargetReceiveQueue = &pContext->ReceiveQueues[nQueueIndex];
-    PNET_BUFFER_LIST listsToIndicate = nullptr, indicateTail = nullptr;
-    ULONG nListsToIndicate = 0;
 
     if(NdisInterlockedIncrement(&pTargetReceiveQueue->ActiveProcessorsCount) == 1)
     {
@@ -1548,25 +1551,25 @@ static BOOLEAN ProcessReceiveQueue(PARANDIS_ADAPTER *pContext,
                 if(packet != NULL)
                 {
                     UpdateReceiveSuccessStatistics(pContext, pPacketInfo, nCoalescedSegmentsCount);
-                    if (listsToIndicate == nullptr)
+                    if (*indicate == nullptr)
                     {
-                        listsToIndicate = indicateTail = packet;
+                        *indicate = *indicateTail = packet;
                     }
                     else
                     {
-                        NET_BUFFER_LIST_NEXT_NBL(indicateTail) = packet;
-                        indicateTail = packet;
+                        NET_BUFFER_LIST_NEXT_NBL(*indicateTail) = packet;
+                        *indicateTail = packet;
                     }
 
-                    NET_BUFFER_LIST_NEXT_NBL(indicateTail) = NULL;
+                    NET_BUFFER_LIST_NEXT_NBL(*indicateTail) = NULL;
                     (*pnPacketsToIndicateLeft)--;
+                    (*nIndicate)++;
                 }
                 else
                 {
                     UpdateReceiveFailStatistics(pContext, nCoalescedSegmentsCount);
                     pBufferDescriptor->Queue->ReuseReceiveBuffer(pContext->ReuseBufferRegular, pBufferDescriptor);
                 }
-                nListsToIndicate++;
             }
             else
             {
@@ -1574,16 +1577,7 @@ static BOOLEAN ProcessReceiveQueue(PARANDIS_ADAPTER *pContext,
                 pBufferDescriptor->Queue->ReuseReceiveBuffer(pContext->ReuseBufferRegular, pBufferDescriptor);
             }
         }
-
-        if (listsToIndicate)
-        {
-            NdisMIndicateReceiveNetBufferLists(pContext->MiniportHandle,
-                listsToIndicate,
-                0,
-                nListsToIndicate,
-                0);
-        }
-    }
+     }
 
     NdisInterlockedDecrement(&pTargetReceiveQueue->ActiveProcessorsCount);
     return ReceiveQueueHasBuffers(pTargetReceiveQueue);
@@ -1595,21 +1589,43 @@ BOOLEAN RxDPCWorkBody(PARANDIS_ADAPTER *pContext, CPUPathesBundle *pathBundle, U
     BOOLEAN res = FALSE;
     BOOLEAN bMoreDataInRing;
 
+    PNET_BUFFER_LIST indicate, indicateTail;
+    ULONG nIndicate;
+
     CCHAR CurrCpuReceiveQueue = GetReceiveQueueForCurrentCPU(pContext);
 
     do
     {
-        pathBundle->rxPath.ProcessRxRing(CurrCpuReceiveQueue);
+        indicate = nullptr;
+        indicateTail = nullptr;
+        nIndicate = 0;
 
-        res |= ProcessReceiveQueue(pContext, &nPacketsToIndicate, PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED);
-
-        if(CurrCpuReceiveQueue != PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
         {
-            res |= ProcessReceiveQueue(pContext, &nPacketsToIndicate, CurrCpuReceiveQueue);
+            CNdisDispatchReadAutoLock tLock(pContext->m_PauseLock);
+
+            pathBundle->rxPath.ProcessRxRing(CurrCpuReceiveQueue);
+
+            res |= ProcessReceiveQueue(pContext, &nPacketsToIndicate, PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED,
+                &indicate, &indicateTail, &nIndicate);
+
+            if(CurrCpuReceiveQueue != PARANDIS_RECEIVE_QUEUE_UNCLASSIFIED)
+            {
+                res |= ProcessReceiveQueue(pContext, &nPacketsToIndicate, CurrCpuReceiveQueue,
+                    &indicate, &indicateTail, &nIndicate);
+            }
+
+            bMoreDataInRing = pathBundle->rxPath.RestartQueue();
         }
 
-        bMoreDataInRing = pathBundle->rxPath.RestartQueue();
-    } while(bMoreDataInRing);
+        if (nIndicate)
+        {
+            NdisMIndicateReceiveNetBufferLists(pContext->MiniportHandle,
+                indicate,
+                0,
+                nIndicate,
+                0);
+        }
+    } while (bMoreDataInRing);
 
     return res;
 }

NetKVM/Common/ParaNdis-TX.cpp
@@ -568,12 +568,15 @@ bool CParaNdisTX::DoPendingTasks(bool IsInterrupt)
                     m_VirtQueue.ProcessTXCompletions();
                     bDoKick = SendMapped(pNBLFailNow);
                     pNBLReturnNow = ProcessWaitingList();
-
-                    if (!m_VirtQueue.HasPacketsInHW() && m_Context->SendState == srsPausing)
                     {
-                        CallbackToCall = m_Context->SendPauseCompletionProc;
-                        m_Context->SendPauseCompletionProc = nullptr;
-                        m_Context->SendState = srsDisabled;
+                        CNdisPassiveWriteAutoLock tLock(m_Context->m_PauseLock);
+
+                        if (!m_VirtQueue.HasPacketsInHW() && m_Context->SendState == srsPausing)
+                        {
+                            CallbackToCall = m_Context->SendPauseCompletionProc;
+                            m_Context->SendPauseCompletionProc = nullptr;
+                            m_Context->SendState = srsDisabled;
+                        }
                     }
                  });
 

NetKVM/Common/ParaNdis-RX.cpp
@@ -159,17 +159,21 @@ void CParaNdisRX::ReuseReceiveBufferRegular(pRxNetDescriptor pBuffersDescriptor)
         }
 
         /* TODO -  the callback dispatch should be performed as a context method */
-        if (m_Context->m_upstreamPacketPending == 0)
+        ONPAUSECOMPLETEPROC callback = nullptr;
+
         {
-            if (m_Context->ReceiveState == srsPausing || m_Context->ReceivePauseCompletionProc)
+            CNdisPassiveWriteAutoLock tLock(m_Context->m_PauseLock);
+
+            if (m_Context->m_upstreamPacketPending == 0 && (m_Context->ReceiveState == srsPausing || m_Context->ReceivePauseCompletionProc))
             {
-                ONPAUSECOMPLETEPROC callback = m_Context->ReceivePauseCompletionProc;
+                callback = m_Context->ReceivePauseCompletionProc;
                 m_Context->ReceiveState = srsDisabled;
                 m_Context->ReceivePauseCompletionProc = NULL;
                 ParaNdis_DebugHistory(m_Context, hopInternalReceivePause, NULL, 0, 0, 0);
-                if (callback) callback(m_Context);
             }
         }
+
+        if (callback) callback(m_Context);
     }
     else
     {

NetKVM/Common/ndis56common.h
@@ -454,6 +454,7 @@ typedef struct _tagPARANDIS_ADAPTER
     ONPAUSECOMPLETEPROC     SendPauseCompletionProc;
     ONPAUSECOMPLETEPROC     ReceivePauseCompletionProc;
 
+    CNdisRWLock             m_PauseLock;
     CNdisRefCounter         m_upstreamPacketPending;
 
     LONG                    ReuseBufferRegular;

NetKVM/wlh/ParaNdis6-Driver.cpp
@@ -160,6 +160,15 @@ static NDIS_STATUS ParaNdis6_Initialize(
 
     if (status == NDIS_STATUS_SUCCESS)
     {
+        new (&pContext->m_PauseLock, PLACEMENT_NEW) CNdisRWLock();
+        if (!pContext->m_PauseLock.Create(pContext->MiniportHandle))
+        {
+            status = NDIS_STATUS_RESOURCES;
+        }
+    }
+
+    if (status == NDIS_STATUS_SUCCESS)
+    {
         NdisZeroMemory(&miniportAttributes, sizeof(miniportAttributes));
         miniportAttributes.RegistrationAttributes.Header.Type = NDIS_OBJECT_TYPE_MINIPORT_ADAPTER_REGISTRATION_ATTRIBUTES;
         miniportAttributes.RegistrationAttributes.Header.Revision = NDIS_MINIPORT_ADAPTER_REGISTRATION_ATTRIBUTES_REVISION_1;

NetKVM/wlh/ParaNdis6-Impl.cpp
@@ -954,9 +954,10 @@ NDIS_STATUS ParaNdis6_ReceivePauseRestart(
 {
     NDIS_STATUS status = NDIS_STATUS_SUCCESS;
 
-    /* TODO - access to ReceiveState and ReceivePauseCompletionProc have to be synchronized*/
     if (bPause)
     {
+        CNdisPassiveWriteAutoLock tLock(pContext->m_PauseLock);
+
         ParaNdis_DebugHistory(pContext, hopInternalReceivePause, NULL, 1, 0, 0);
         if (pContext->m_upstreamPacketPending != 0)
         {
@@ -1044,8 +1045,12 @@ NDIS_STATUS ParaNdis6_SendPauseRestart(
         ParaNdis_DebugHistory(pContext, hopInternalSendPause, NULL, 1, 0, 0);
         if (pContext->SendState == srsEnabled)
         {
-            pContext->SendState = srsPausing;
-            pContext->SendPauseCompletionProc = Callback;
+            {
+                CNdisPassiveWriteAutoLock tLock(pContext->m_PauseLock);
+
+                pContext->SendState = srsPausing;
+                pContext->SendPauseCompletionProc = Callback;
+            }
 
             for (UINT i = 0; i < pContext->nPathBundles; i++)
             {
@@ -1054,6 +1059,7 @@ NDIS_STATUS ParaNdis6_SendPauseRestart(
                     status = NDIS_STATUS_PENDING;
                 }
             }
+
             if (status == NDIS_STATUS_SUCCESS)
             {
                 pContext->SendState = srsDisabled;

commit 22d45cb02800d08de83c6ba43539fae38181b50b

NetKVM: BZ#1223426: Fix for performance degradation with multi-queue
This patch fixes driver's performance issue - caused by commit:

    1e4c959e3407bd4a8ac49d442bcec8ebde98da39
    Author: Joseph Hindin <yhindin@rehat.com>
    Date:   Mon Dec 1 17:13:28 2014 +0200
    NetKVM: BZ#1147239: Indicating outside of ProcessReceiveQueue function

The commit mentioned above acquired write lock of PARANDIS_ADAPTER.m_PauseLock in
CParaNdisRX::ReuseReceiveBufferRegular() method. This lock protects
critical section of driver's transition into Pause state and necessary for
correct functionality, but caused significant performance degradation.
m_PauseLock is global and acquired for all data queues by TX and RX code on DPC runlevel.

In order to improve performance, this commit add preliminary checks of
PARANDIS_ADAPTER.SendState and PARANDIS_ADAPTER.ReceiveState attributes of
miniport context before acquiring m_PauseLock and test these attributes under the lock
again. Hence this lock is acquired during transition to paused state only
when performance constraints are unimportant.

Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>
Signed-off-by: Igor Derzhavets <igord@daynix.com>

NetKVM/Common/ParaNdis-Common.cpp
@@ -1496,7 +1496,7 @@ VOID ParaNdis_TestPausing(PARANDIS_ADAPTER *pContext)
 {
     ONPAUSECOMPLETEPROC callback = nullptr;
 
-    if (pContext->m_rxPacketsOutsideRing == 0)
+    if (pContext->m_rxPacketsOutsideRing == 0 && pContext->ReceiveState == srsPausing)
     {
         CNdisPassiveWriteAutoLock tLock(pContext->m_PauseLock);
 

NetKVM/Common/ParaNdis-TX.cpp
@@ -583,15 +583,18 @@ bool CParaNdisTX::DoPendingTasks(bool IsInterrupt)
                     bDoKick = SendMapped(IsInterrupt, pNBLFailNow);
                     pNBLReturnNow = ProcessWaitingList();
                     {
-                        CNdisPassiveWriteAutoLock tLock(m_Context->m_PauseLock);
-
                         NdisDprAcquireSpinLock(&m_Context->m_CompletionLock);
 
-                        if (m_Context->SendState == srsPausing && !ParaNdis_HasPacketsInHW(m_Context))
+                        if (m_Context->SendState == srsPausing)
                         {
-                            CallbackToCall = m_Context->SendPauseCompletionProc;
-                            m_Context->SendPauseCompletionProc = nullptr;
-                            m_Context->SendState = srsDisabled;
+                            CNdisPassiveWriteAutoLock tLock(m_Context->m_PauseLock);
+
+                            if (m_Context->SendState == srsPausing && !ParaNdis_HasPacketsInHW(m_Context))
+                            {
+                                CallbackToCall = m_Context->SendPauseCompletionProc;
+                                m_Context->SendPauseCompletionProc = nullptr;
+                                m_Context->SendState = srsDisabled;
+                            }
                         }
                     }
                  });