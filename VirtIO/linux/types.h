4436412b2b6a96bb2d9a8679b3094d02e3eaa739
Yan Vugenfirer <yvugenfi@redhat.com>
2015-06-28 23:56:09 +0300

    virtio 1.0: Refactoring the library to be as close as possible to Linux
    Adding virtio_types.h and virtio_ids.h. Also adding types.h that includes
    basic types and substitutes part of osdep.h.
    
    Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>

---
+++ VirtIO/linux/types.h
@@ -0,0 +1,12 @@
+#ifndef _LINUX_TYPES_H
+#define _LINUX_TYPES_H
+
+#define __bitwise__
+#define __attribute__(x)
+
+#define __u8 unsigned char
+#define __u16 unsigned short
+#define __u32 unsigned long
+#define __u64 ULONGLONG
+
+#endif /* _LINUX_TYPES_H */
