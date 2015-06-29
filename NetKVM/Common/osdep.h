0454af5339c766fabfe49ab6bcde90cc621a2b4f
Yan Vugenfirer <yvugenfi@redhat.com>
2015-06-29 16:52:00 +0300

    virtio 1.0: NetKVM: removing from osdep.h types already defined in linux/types.h
    Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>

--- NetKVM/Common/osdep.h
+++ NetKVM/Common/osdep.h
@@ -32,12 +32,6 @@
 #define _Acquires_exclusive_lock_(lock)
 #endif
 
-#define u8 unsigned char
-#define u16 unsigned short
-#define u32 unsigned long
-#define u64 ULONGLONG
-
-
 #define mb()   KeMemoryBarrier()
 #define rmb()  KeMemoryBarrier()
 #define wmb()  KeMemoryBarrier()
