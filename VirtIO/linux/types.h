ca326dd5cb3b7435acb15b7e8be3c48b85b72768
Yan Vugenfirer <yvugenfi@redhat.com>
2015-06-29 16:38:06 +0300

    virtio 1.0: Refactoring the library - moving remaining types to linux/types.h
    Moving types definition from osdep.h to linux/types.h in virtio library
    
    Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>
    (cherry picked from commit 026a3a6e06e36e55fb9ef4bb01f618a2cb033edb)

--- VirtIO/linux/types.h
+++ VirtIO/linux/types.h
@@ -4,6 +4,11 @@
 #define __bitwise__
 #define __attribute__(x)
 
+#define u8 unsigned char
+#define u16 unsigned short
+#define u32 unsigned long
+#define u64 ULONGLONG
+
 #define __u8 unsigned char
 #define __u16 unsigned short
 #define __le16 unsigned short
