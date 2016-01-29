103f45f955a822768870a7b7975644c0295c7031
Yan Vugenfirer <yvugenfi@redhat.com>
2016-01-29 03:47:16 -0800

    NetKVM: Clean up the project for Windows 10 build
    * Rename the project and the solution as preparation to hold other
    configurations
    
    * Remove ARM plarform
    
    * Change batch file to compaile with new file names
    
    Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>

--- NetKVM/build-win10.bat
+++ NetKVM/build-win10.bat
@@ -1,13 +1,13 @@
 :: Build Windows 10
 setlocal
 call tools\set_version.bat
-call ..\tools\callVisualStudio.bat 14 netkvm-win10.sln /Rebuild "Release|x86" /Out buildfre_win10_x86.log
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win10 Release|x86" /Out buildfre_win10_x86.log
 endlocal
 if %ERRORLEVEL% NEQ 0 goto :eof
 
 setlocal
 call tools\set_version.bat
-call ..\tools\callVisualStudio.bat 14 netkvm-win10.sln /Rebuild "Release|x64" /Out buildfre_win10_amd64.log
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win10 Release|x64" /Out buildfre_win10_amd64.log
 endlocal
 if %ERRORLEVEL% NEQ 0 goto :eof
 
@@ -17,14 +17,14 @@ mkdir Install\Win10
 mkdir Install\Win10\x86
 mkdir Install\Win10\x64
 
-copy /y x86\Release\netkvm.pdb Install\Win10\x86\
-copy /y x86\Release\NetKVM-Win10\netkvm.inf Install\Win10\x86\
-copy /y x86\Release\NetKVM-Win10\netkvm.cat Install\Win10\x86\
-copy /y x86\Release\NetKVM-Win10\netkvm.sys Install\Win10\x86\
+copy /y x86\Win10Release\netkvm.pdb Install\Win10\x86\
+copy /y x86\Win10Release\NetKVM-VS2015\netkvm.inf Install\Win10\x86\
+copy /y x86\Win10Release\NetKVM-VS2015\netkvm.cat Install\Win10\x86\
+copy /y x86\Win10Release\NetKVM-VS2015\netkvm.sys Install\Win10\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win10\x86\
 
-copy /y x64\Release\netkvm.pdb Install\Win10\x64\
-copy /y x64\Release\NetKVM-Win10\netkvm.inf Install\Win10\x64\
-copy /y x64\Release\NetKVM-Win10\netkvm.cat Install\Win10\x64\
-copy /y x64\Release\NetKVM-Win10\netkvm.sys Install\Win10\x64\
+copy /y x64\Win10Release\netkvm.pdb Install\Win10\x64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.inf Install\Win10\x64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.cat Install\Win10\x64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.sys Install\Win10\x64\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win10\x64\
