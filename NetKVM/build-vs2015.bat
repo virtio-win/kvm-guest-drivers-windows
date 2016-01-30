f85cd6e4b38d852997821bc00a29231f3bd26c8a
Yan Vugenfirer <yvugenfi@redhat.com>
2016-01-30 06:24:28 -0800

    NetKVM: VS2015 build: Add Windows Vista and Windows 8.1 compilation
    Signed-off-by: Yan Vugenfirer <yvugenfi@redhat.com>

--- NetKVM/build-vs2015.bat
+++ NetKVM/build-vs2015.bat
@@ -1,5 +1,18 @@
 mkdir Install
 
+:: Build Windows Vista
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Vista Release|x86" /Out buildfre_win7_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Vista Release|x64" /Out buildfre_win7_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
 :: Build Windows 7
 setlocal
 call tools\set_version.bat
@@ -23,6 +36,19 @@ if %ERRORLEVEL% NEQ 0 goto :eof
 setlocal
 call tools\set_version.bat
 call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8 Release|x64" /Out buildfre_win8_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: Build Windows 8.1
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8.1 Release|x86" /Out buildfre_win8_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8.1 Release|x64" /Out buildfre_win8_amd64.log
 endlocal
 if %ERRORLEVEL% NEQ 0 goto :eof
 
@@ -56,19 +82,19 @@ xcopy /S /I /Y NDIS5\Install\XP\amd64 Install\XP\amd64
 mkdir Install
 mkdir Install\Vista
 mkdir Install\Vista\x86
-mkdir Install\Vista\x64
+mkdir Install\Vista\amd64
 mkdir Install\Win7
 mkdir Install\Win7\x86
-mkdir Install\Win7\x64
+mkdir Install\Win7\amd64
 mkdir Install\Win8
 mkdir Install\Win8\x86
-mkdir Install\Win8\x64
+mkdir Install\Win8\amd64
 mkdir Install\Win8.1
 mkdir Install\Win8.1\x86
-mkdir Install\Win8.1\x64
+mkdir Install\Win8.1\amd64
 mkdir Install\Win10
 mkdir Install\Win10\x86
-mkdir Install\Win10\x64
+mkdir Install\Win10\amd64
 
 :: Copy Windows Vista binaries
 copy /y x86\VistaRelease\netkvm.pdb Install\Vista\x86\
@@ -77,11 +103,11 @@ copy /y x86\VistaRelease\NetKVM-VS2015\netkvm.cat Install\Vista\x86\
 copy /y x86\VistaRelease\NetKVM-VS2015\netkvm.sys Install\Vista\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Vista\x86\
 
-copy /y x64\VistaRelease\netkvm.pdb Install\Vista\x64\
-copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.inf Install\Vista\x64\
-copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.cat Install\Vista\x64\
-copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.sys Install\Vista\x64\
-copy /y tools\NetKVMTemporaryCert.cer Install\Vista\x64\
+copy /y x64\VistaRelease\netkvm.pdb Install\Vista\amd64\
+copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.inf Install\Vista\amd64\
+copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.cat Install\Vista\amd64\
+copy /y x64\VistaRelease\NetKVM-VS2015\netkvm.sys Install\Vista\amd64\
+copy /y tools\NetKVMTemporaryCert.cer Install\Vista\amd64\
 
 :: Copy Windows 7 binaries
 copy /y x86\Win7Release\netkvm.pdb Install\Win7\x86\
@@ -90,11 +116,11 @@ copy /y x86\Win7Release\NetKVM-VS2015\netkvm.cat Install\Win7\x86\
 copy /y x86\Win7Release\NetKVM-VS2015\netkvm.sys Install\Win7\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win7\x86\
 
-copy /y x64\Win7Release\netkvm.pdb Install\Win7\x64\
-copy /y x64\Win7Release\NetKVM-VS2015\netkvm.inf Install\Win7\x64\
-copy /y x64\Win7Release\NetKVM-VS2015\netkvm.cat Install\Win7\x64\
-copy /y x64\Win7Release\NetKVM-VS2015\netkvm.sys Install\Win7\x64\
-copy /y tools\NetKVMTemporaryCert.cer Install\Win7\x64\
+copy /y x64\Win7Release\netkvm.pdb Install\Win7\amd64\
+copy /y x64\Win7Release\NetKVM-VS2015\netkvm.inf Install\Win7\amd64\
+copy /y x64\Win7Release\NetKVM-VS2015\netkvm.cat Install\Win7\amd64\
+copy /y x64\Win7Release\NetKVM-VS2015\netkvm.sys Install\Win7\amd64\
+copy /y tools\NetKVMTemporaryCert.cer Install\Win7\amd64\
 
 :: Copy Windows 8 binaries
 copy /y x86\Win8Release\netkvm.pdb Install\Win8\x86\
@@ -103,11 +129,11 @@ copy /y x86\Win8Release\NetKVM-VS2015\netkvm.cat Install\Win8\x86\
 copy /y x86\Win8Release\NetKVM-VS2015\netkvm.sys Install\Win8\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win8\x86\
 
-copy /y x64\Win8Release\netkvm.pdb Install\Win8\x64\
-copy /y x64\Win8Release\NetKVM-VS2015\netkvm.inf Install\Win8\x64\
-copy /y x64\Win8Release\NetKVM-VS2015\netkvm.cat Install\Win8\x64\
-copy /y x64\Win8Release\NetKVM-VS2015\netkvm.sys Install\Win8\x64\
-copy /y tools\NetKVMTemporaryCert.cer Install\Win8\x64\
+copy /y x64\Win8Release\netkvm.pdb Install\Win8\amd64\
+copy /y x64\Win8Release\NetKVM-VS2015\netkvm.inf Install\Win8\amd64\
+copy /y x64\Win8Release\NetKVM-VS2015\netkvm.cat Install\Win8\amd64\
+copy /y x64\Win8Release\NetKVM-VS2015\netkvm.sys Install\Win8\amd64\
+copy /y tools\NetKVMTemporaryCert.cer Install\Win8\amd64\
 
 :: Copy Windows 8.1 binaries
 copy /y x86\Win8.1Release\netkvm.pdb Install\Win8.1\x86\
@@ -116,11 +142,11 @@ copy /y x86\Win8.1Release\NetKVM-VS2015\netkvm.cat Install\Win8.1\x86\
 copy /y x86\Win8.1Release\NetKVM-VS2015\netkvm.sys Install\Win8.1\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win8.1\x86\
 
-copy /y x64\Win8.1Release\netkvm.pdb Install\Win8.1\x64\
-copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.inf Install\Win8.1\x64\
-copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.cat Install\Win8.1\x64\
-copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.sys Install\Win8.1\x64\
-copy /y tools\NetKVMTemporaryCert.cer Install\Win8.1\x64\
+copy /y x64\Win8.1Release\netkvm.pdb Install\Win8.1\amd64\
+copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.inf Install\Win8.1\amd64\
+copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.cat Install\Win8.1\amd64\
+copy /y x64\Win8.1Release\NetKVM-VS2015\netkvm.sys Install\Win8.1\amd64\
+copy /y tools\NetKVMTemporaryCert.cer Install\Win8.1\amd64\
 
 :: Copy Windows 10 binaries
 copy /y x86\Win10Release\netkvm.pdb Install\Win10\x86\
@@ -129,9 +155,9 @@ copy /y x86\Win10Release\NetKVM-VS2015\netkvm.cat Install\Win10\x86\
 copy /y x86\Win10Release\NetKVM-VS2015\netkvm.sys Install\Win10\x86\
 copy /y tools\NetKVMTemporaryCert.cer Install\Win10\x86\
 
-copy /y x64\Win10Release\netkvm.pdb Install\Win10\x64\
-copy /y x64\Win10Release\NetKVM-VS2015\netkvm.inf Install\Win10\x64\
-copy /y x64\Win10Release\NetKVM-VS2015\netkvm.cat Install\Win10\x64\
-copy /y x64\Win10Release\NetKVM-VS2015\netkvm.sys Install\Win10\x64\
-copy /y tools\NetKVMTemporaryCert.cer Install\Win10\x64\
+copy /y x64\Win10Release\netkvm.pdb Install\Win10\amd64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.inf Install\Win10\amd64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.cat Install\Win10\amd64\
+copy /y x64\Win10Release\NetKVM-VS2015\netkvm.sys Install\Win10\amd64\
+copy /y tools\NetKVMTemporaryCert.cer Install\Win10\amd64\
 
