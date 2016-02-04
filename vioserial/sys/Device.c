e9bc70fb8799ef0f8a495db2511357ef54d406d7
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-02-04 12:35:40 +1100

    Bug 1303510 - Change the copyright on all the Windows drivers to "2016" in virtio-win-prewhql build

--- vioserial/sys/Device.c
+++ vioserial/sys/Device.c
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2010-2015  Red Hat, Inc.
+ * Copyright (c) 2010-2016  Red Hat, Inc.
  *
  * File: VIOSerialDevice.c
  *
