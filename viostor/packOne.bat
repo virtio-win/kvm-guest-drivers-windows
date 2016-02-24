0d7ccdf39c314c2ce835b4d1afc388b23da4521e
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-02-23 16:16:21 -0800

    adjust build settings

--- viostor/packOne.bat
+++ viostor/packOne.bat
@@ -66,12 +66,14 @@ goto error_inf2cat
 setlocal
 if /i "%2"=="x86" set _OSMASK_=XP_X86,Server2003_X86
 if /i "%2"=="x64" set _OSMASK_=XP_X64,Server2003_X64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %INST_ARC%
 goto run_inf2cat
 
 :create_vista
 setlocal
 if /i "%2"=="x86" set _OSMASK_=Vista_X86,Server2008_X86
 if /i "%2"=="x64" set _OSMASK_=Vista_X64,Server2008_X64
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %INST_ARC%
 goto run_inf2cat
 
 :create_win7
