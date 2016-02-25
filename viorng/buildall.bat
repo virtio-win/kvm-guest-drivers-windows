82c3c4d15eb376282f3398cc630231004f6f883a
Ladi Prosek <lprosek@redhat.com>
2016-02-25 13:57:50 +0100

    Build viorng with VS for all targets
    This commit makes viorng.sys build using VS (currently 2013) for
    all target OSes and removes the legacy build files. Since VS is
    unable to build drivers for <Win7 out of the box, all settings
    (include directories, libs and lib directories, the .sys file
    extension, linker flags, .inf processing) are specified manually
    in viorng.vcxproj.

--- viorng/buildall.bat
+++ viorng/buildall.bat
@@ -10,8 +10,7 @@ set _MAJORVERSION_=%_BUILD_MAJOR_VERSION_%
 set _MINORVERSION_=%_BUILD_MINOR_VERSION_%
 set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
 
-for %%O in (Win7 Win8 Win10) do for %%P in (Win32 x64) do call :build_driver %%O %%P
-for %%O in (Vista) do for %%P in (Win32 x64) do call :build_vista_driver %%O %%P
+for %%O in (Vista Win7 Win8 Win10) do for %%P in (Win32 x64) do call :build_driver %%O %%P
 
 endlocal
 
@@ -51,36 +50,6 @@ call :set_windows_version %1
 call :create_version_file "2012-defines.h"
 call :set_out_filename %1 %2
 set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_%
-goto :eof
-
-:build_vista_driver
-call :prebuild_driver %1 %2
-if "%DDKVER%"=="" set DDKVER=7600.16385.1
-set DDKBUILDENV=
-set BUILDROOT=C:\WINDDK\%DDKVER%
-pushd %BUILDROOT%
-set DDKENV=%2
-if "%DDKENV%"=="Win32" set DDKENV=x86
-call bin\setenv.bat %BUILDROOT% %DDKENV% fre %OS% no_oacr
-popd
-pushd ..\VirtIO
-if /i "%2"=="Win32" call buildAll.bat WLH_32
-if /i "%2"=="x64" call buildAll.bat WLH_64
-popd
-build -cZg
-set ARCH=amd64
-if "%2"=="Win32" set ARCH=i386
-set SRC_DIR=.\viorng\objfre_%OS%_%PLAT%\%ARCH%
-set DST_DIR=.\Install\%OS%\%PLAT%
-mkdir %DST_DIR%
-copy %BUILDROOT%\redist\wdf\%PLAT%\WdfCoInstaller01009.dll %DST_DIR%
-call :build_um_provider Vista %2
-call :build_co_installer Vista %2
-for %%E in (inf pdb sys) do copy %SRC_DIR%\viorng.%%E %DST_DIR%
-if /i "%2"=="Win32" set OS_SYS=Vista_X86,Server2008_X86
-if /i "%2"=="x64" set OS_SYS=Vista_X64,Server2008_X64
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-inf2cat /driver:%DST_DIR% /os:%OS_SYS%
 goto :eof
 
 :build_driver
