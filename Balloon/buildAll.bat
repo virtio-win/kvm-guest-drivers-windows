d125fba9e1837897c0ccc0699c3e5b54dfbf139e
Ladi Prosek <lprosek@redhat.com>
2016-02-29 14:41:42 +0100

    Build balloon with VS for all targets
    This commit makes balloon.sys build using VS (currently 2013) for
    all target OSes and removes the legacy build files. Since VS is
    unable to build drivers for <Win7 out of the box, all settings
    (include directories, libs and lib directories, the .sys file
    extension, linker flags, .inf processing) are specified manually
    in balloon.vcxproj.

--- Balloon/buildAll.bat
+++ Balloon/buildAll.bat
@@ -1,146 +1,27 @@
 @echo off
 
-set SYS_FILE_NAME=balloon
-
-for %%A in (Win7 Wnet Wlh WXp Win8 Win10) do for %%B in (32 64) do call :%%A_%%B
-set SYS_FILE_NAME=
+for %%A in (WXp Wnet Wlh Win7 Win8 Win10) do for %%B in (32 64) do call :buildpack %%A_%%B
 goto :eof 
 
 :buildsys
 cd sys
-call buildOne.bat %1 %2
+call buildOne.bat %1 balloon
 cd ..
 goto :eof
 
 :packsys
 cd sys
-call packOne.bat %1 %2 %SYS_FILE_NAME%
+call packOne.bat %1 %2 balloon
 cd ..
 goto :eof
 
 :buildpack
-call :buildsys %1 %2
-call :packsys %1 %2
+if /i "%1"=="WXp" if "%2"=="64" goto :eof
+set BUILD_ARC=%2
+set BUILD_ARC=x%BUILD_ARC:32=86%
+call :buildsys %1 %BUILD_ARC%
+call :packsys %1 %BUILD_ARC%
 set BUILD_OS=
 set BUILD_ARC=
 goto :eof
 
-:WXP_32
-setlocal
-set BUILD_OS=WXp
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WXP_64
-goto :eof
-
-:WNET_32
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WNET_64
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_32
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_64
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WIN7_32
-setlocal
-set BUILD_OS=Win7
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WIN7_64
-setlocal
-set BUILD_OS=Win7
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_32
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_64
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WNET_32
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WNET_64
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WXP_32
-setlocal
-set BUILD_OS=WXp
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WXP_64
-goto :eof
-
-:Win8_32
-setlocal
-set BUILD_OS=Win8
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-rem pause
-goto :eof
-
-:Win8_64
-setlocal
-set BUILD_OS=Win8
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-rem pause
-goto :eof
