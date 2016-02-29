0fd0db43a05f5611b719f6ea8a6a73632c234ae4
Ladi Prosek <lprosek@redhat.com>
2016-02-29 14:41:42 +0100

    Build balloon with VS for all targets
    This commit makes balloon.sys build using VS (currently 2013) for
    all target OSes and removes the legacy build files. Since VS is
    unable to build drivers for <Win7 out of the box, all settings
    (include directories, libs and lib directories, the .sys file
    extension, linker flags, .inf processing) are specified manually
    in balloon.vcxproj.

--- Balloon/buildAll.bat
+++ Balloon/buildAll.bat
@@ -1,9 +1,7 @@
 @echo off
 
-set SYS_FILE_NAME=balloon
+for %%A in (Win7 Wnet Wlh WXp Win8) do for %%B in (32 64) do call :buildpack %%A %%B
 
-for %%A in (Win7 Wnet Wlh WXp Win8) do for %%B in (32 64) do call :%%A_%%B
-set SYS_FILE_NAME=
 goto :eof 
 
 :buildsys
@@ -14,90 +12,16 @@ goto :eof
 
 :packsys
 cd sys
-call packOne.bat %1 %2 %SYS_FILE_NAME%
+call packOne.bat %1 %2 balloon
 cd ..
 goto :eof
 
 :buildpack
-call :buildsys %1 %2
-call :packsys %1 %2
-set BUILD_OS=
-set BUILD_ARC=
-goto :eof
-
-:WIN7_32
+if /i "%1"=="WXp" if "%2"=="64" goto :eof
 setlocal
-set BUILD_OS=Win7
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
+set BUILD_ARC=%2
+set BUILD_ARC=x%BUILD_ARC:32=86%
+call :buildsys %1 %BUILD_ARC%
+call :packsys %1 %BUILD_ARC%
 endlocal
-goto :eof
-
-:WIN7_64
-setlocal
-set BUILD_OS=Win7
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_32
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WLH_64
-setlocal
-set BUILD_OS=Wlh
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WNET_32
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WNET_64
-setlocal
-set BUILD_OS=Wnet
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WXP_32
-setlocal
-set BUILD_OS=WXp
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-goto :eof
-
-:WXP_64
-goto :eof
-
-:Win8_32
-setlocal
-set BUILD_OS=Win8
-set BUILD_ARC=x86
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-rem pause
-goto :eof
-
-:Win8_64
-setlocal
-set BUILD_OS=Win8
-set BUILD_ARC=x64
-call :buildpack %BUILD_OS% %BUILD_ARC%
-endlocal
-rem pause
 goto :eof
--- Balloon/sys/buildOne.bat
+++ Balloon/sys/buildOne.bat
@@ -1,41 +1,55 @@
 @echo off
+setlocal
 
+if "%DDKINSTALLROOT%"=="" set DDKINSTALLROOT=C:\WINDDK\
 if "%DDKVER%"=="" set DDKVER=7600.16385.1
-set BUILDROOT=C:\WINDDK\%DDKVER%
 
 if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
 if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
 if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
 
 if /i "%1"=="prepare" goto %1
-if /i "%1"=="finalize" goto %1
-if /i "%1"=="Win8" goto %1_%2
-set DDKBUILDENV=
-pushd %BUILDROOT%
-call %BUILDROOT%\bin\setenv.bat %BUILDROOT% %2 fre %1 no_oacr
-popd
 
-call :prepare_version
-set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_% 
-if exist ..\..\Tools\xdate.exe ..\..\Tools\xdate.exe -u > timestamp.txt
-if exist timestamp.txt set /p STAMPINF_DATE= < timestamp.txt
-del timestamp.txt    
-
-build -cZg
-
-set DDKVER=
-set BUILDROOT=
+call :%1_%2
 goto :eof
 
 :Win8_x86
-call :BuildWin8 "Win8 Release|Win32" buildfre_win8_x86.log
+call :BuildProject "Win8 Release|Win32" buildfre_win8_x86.log
 goto :eof
 
 :Win8_x64
-call :BuildWin8 "Win8 Release|x64" buildfre_win8_amd64.log
+call :BuildProject "Win8 Release|x64" buildfre_win8_amd64.log
 goto :eof
 
-:BuildWin8
+:Win7_x86
+call :BuildProject "Win7 Release|Win32" buildfre_win7_x86.log
+goto :eof
+
+:Win7_x64
+call :BuildProject "Win7 Release|x64" buildfre_win7_amd64.log
+goto :eof
+
+:Wlh_x86
+call :BuildProject "Vista Release|Win32" buildfre_wlh_x86.log
+goto :eof
+
+:Wlh_x64
+call :BuildProject "Vista Release|x64" buildfre_wlh_amd64.log
+goto :eof
+
+:Wnet_x86
+call :BuildProject "Win2k3 Release|Win32" buildfre_wnet_x86.log
+goto :eof
+
+:Wnet_x64
+call :BuildProject "Win2k3 Release|x64" buildfre_wnet_amd64.log
+goto :eof
+
+:WXp_x86
+call :BuildProject "WinXP Release|Win32" buildfre_wxp_x86.log
+goto :eof
+
+:BuildProject
 setlocal
 if "%_NT_TARGET_VERSION%"=="" set _NT_TARGET_VERSION=0x602
 if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
@@ -54,6 +68,7 @@ goto :eof
 :prepare_version
 set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
 goto :eof
+
 :create2012H
 echo #ifndef __DATE__ 
 echo #define __DATE__ "%DATE%"
@@ -66,15 +81,9 @@ echo #define _NT_TARGET_MIN %_RHEL_RELEASE_VERSION_%
 echo #define _MAJORVERSION_ %_BUILD_MAJOR_VERSION_%
 echo #define _MINORVERSION_ %_BUILD_MINOR_VERSION_%
 goto :eof
+
 :prepare
 set _NT_TARGET_VERSION=0x0602
 call :prepare_version
 call :create2012H  > 2012-defines.h
-goto :eof
-:finalize
-echo finalizing build (%2 %3)
-rem del 2012-defines.h 
-pushd ..
-rem call packOne.bat %2 %3 balloon
-popd
 goto :eof
