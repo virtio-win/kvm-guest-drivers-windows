50a1fb792fa6786c4283ecb91e6d1c025271b03b
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-04-10 23:18:30 -0700

    fix compilation problems

--- viorng/buildall.bat
+++ viorng/buildall.bat
@@ -11,7 +11,7 @@ set _MINORVERSION_=%_BUILD_MINOR_VERSION_%
 set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
 
 for %%O in (Win7 Win8 Win10) do for %%P in (Win32 x64) do call :build_driver %%O %%P
-for %%P in (Win32 x64) do call :build_vista_driver Vista %%P
+for %%O in (Vista) do for %%P in (Win32 x64) do call :build_vista_driver %%O %%P
 
 endlocal
 
@@ -89,17 +89,23 @@ call :fix_wdfcoinstaller_name %1 %2
 goto :eof
 
 :build_um_provider
+set BUILDPLAT=%2
+if "%2"=="Win32" set BUILDPLAT=x86
 call :set_windows_version %1
 call :set_out_filename %1 %2
-call ..\tools\callVisualStudio.bat 14 cng\um\viorngum.vcxproj /Rebuild "Release|%2" /Out %OUT_FILENAME%
+call ..\tools\callVisualStudio.bat 14 cng\um\viorngum.vcxproj /Rebuild "Release|%BUILDPLAT%" /Out %OUT_FILENAME%
 copy "cng\um\%2\Release\viorngum.dll" "Install\%OS%\%PLAT%\"
+set BUILDPLAT=
 goto :eof
 
 :build_co_installer
+set BUILDPLAT=%2
+if "%2"=="Win32" set BUILDPLAT=x86
 call :set_windows_version %1
 call :set_out_filename %1 %2
-call ..\tools\callVisualStudio.bat 14 coinstaller\viorngci.vcxproj /Rebuild "Release|%2" /Out %OUT_FILENAME%
+call ..\tools\callVisualStudio.bat 14 coinstaller\viorngci.vcxproj /Rebuild "Release|%BUILDPLAT%" /Out %OUT_FILENAME%
 copy "coinstaller\%2\Release\viorngci.dll" "Install\%OS%\%PLAT%\"
+set BUILDPLAT=
 goto :eof
 
 :create_version_file
