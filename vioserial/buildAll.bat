3cf7a3d49806155219366ef8637fa1f1bd070651
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-05-01 22:37:28 -0700

    fix driver compilation problems

--- vioserial/buildAll.bat
+++ vioserial/buildAll.bat
@@ -1,6 +1,6 @@
 @echo off
 
-for %%A in (Wnet Wlh WXp Win7 Win8 Win10) do for %%B in (32 64) do call :buildpack %%A_%%B
+for %%A in (Wnet Wlh WXp Win7 Win8 Win10) do for %%B in (32 64) do call :buildpack %%A %%B
 
 goto :eof 
 
@@ -30,13 +30,13 @@ goto :eof
 
 :buildpack
 if /i "%1"=="WXp" if "%2"=="64" goto :eof
+setlocal
 set BUILD_ARC=%2
 set BUILD_ARC=x%BUILD_ARC:32=86%
 call :buildsys %1 %BUILD_ARC%
-call :buildapp %1 %BUILD_ARC%
+rem call :buildapp %1 %BUILD_ARC%
 call :packsys %1 %BUILD_ARC%
-call :packapp %1 %BUILD_ARC%
-set BUILD_OS=
-set BUILD_ARC=
+rem call :packapp %1 %BUILD_ARC%
+endlocal
 goto :eof
 
--- vioserial/sys/buildOne.bat
+++ vioserial/sys/buildOne.bat
@@ -38,7 +38,7 @@ call :BuildProject "Win10 Release|x64" buildfre_win10_amd64.log
 goto :eof
 
 :Wlh_x86
-call :BuildProject "Vista Release|Win32" buildfre_wlh_x86.log
+call :BuildProject "Vista Release|x86" buildfre_wlh_x86.log
 goto :eof
 
 :Wlh_x64
@@ -46,7 +46,7 @@ call :BuildProject "Vista Release|x64" buildfre_wlh_amd64.log
 goto :eof
 
 :Wnet_x86
-call :BuildProject "Win2k3 Release|Win32" buildfre_wnet_x86.log
+call :BuildProject "Win2k3 Release|x86" buildfre_wnet_x86.log
 goto :eof
 
 :Wnet_x64
@@ -54,7 +54,7 @@ call :BuildProject "Win2k3 Release|x64" buildfre_wnet_amd64.log
 goto :eof
 
 :WXp_x86
-call :BuildProject "WinXP Release|Win32" buildfre_wxp_x86.log
+call :BuildProject "WinXP Release|x86" buildfre_wxp_x86.log
 goto :eof
 
 :BuildProject
@@ -69,7 +69,8 @@ set _MINORVERSION_=%_BUILD_MINOR_VERSION_%
 set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
 set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
 set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_%
-call ..\..\tools\callVisualStudio.bat 12 vioser.vcxproj /Rebuild "%~1" /Out %2
+call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call ..\..\tools\callVisualStudio.bat 14 vioser.vcxproj /Rebuild "%~1" /Out %2
 endlocal
 goto :eof
 
