8374eb50961d4cc7c1960ffe056bcb06a68e0b22
Dmitry Fleytman <dmitry@daynix.com>
2016-06-21 11:22:48 +0300

    NetKVM: Beautify CNdisRefCounter default constructor
    Signed-off-by: Dmitry Fleytman <dmitry@daynix.com>

--- NetKVM/Common/ParaNdis-Util.h
+++ NetKVM/Common/ParaNdis-Util.h
@@ -104,7 +104,7 @@ typedef CLockedContext<CNdisSpinLock> TSpinLocker;
 class CNdisRefCounter
 {
 public:
-    CNdisRefCounter() {}
+    CNdisRefCounter() = default;
 
     void AddRef() { NdisInterlockedIncrement(&m_Counter); }
     void AddRef(ULONG RefCnt);
