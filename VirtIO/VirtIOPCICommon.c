f5ac08e24fed9db7e18b8570ca5b5a0662ae57a3
Vadim Rozenfeld <vrozenfe@redhat.com>
2016-07-07 10:12:18 +1000

    [vioscsi] bug-fix for bz#1352432

--- VirtIO/VirtIOPCICommon.c
+++ VirtIO/VirtIOPCICommon.c
@@ -302,6 +302,9 @@ void virtio_delete_queues(VirtIODevice *vdev)
     struct virtqueue *vq;
     unsigned i;
 
+    if (vdev->info == NULL)
+        return;
+
     for (i = 0; i < vdev->maxQueues; i++) {
         vq = vdev->info[i].vq;
         if (vq != NULL) {
