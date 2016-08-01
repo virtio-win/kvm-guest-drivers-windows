efd8a035f96e83bc1ca27889f6cffb1f7d66b919
Ladi Prosek <lprosek@redhat.com>
2016-08-01 11:07:34 +0200

    balloon: Fix driver versioning
    We used to build only the Win8 driver with Visual Studio so the
    hardcoded _NT_TARGET_VERSION=0x0602 in buildOne.bat worked fine.
    Now that all drivers are build with VS, the variable needs to be
    set based on the particular target.

--- Balloon/sys/buildOne.bat
+++ Balloon/sys/buildOne.bat
@@ -13,52 +13,52 @@ call :%1_%2
 goto :eof
 
 :Win7_x86
-call :BuildProject "Win7 Release|x86" buildfre_win7_x86.log
+call :BuildProject "Win7 Release|x86" 0x601 buildfre_win7_x86.log
 goto :eof
 
 :Win7_x64
-call :BuildProject "Win7 Release|x64" buildfre_win7_amd64.log
+call :BuildProject "Win7 Release|x64" 0x601 buildfre_win7_amd64.log
 goto :eof
 
 :Win8_x86
-call :BuildProject "Win8 Release|x86" buildfre_win8_x86.log
+call :BuildProject "Win8 Release|x86" 0x602 buildfre_win8_x86.log
 goto :eof
 
 :Win8_x64
-call :BuildProject "Win8 Release|x64" buildfre_win8_amd64.log
+call :BuildProject "Win8 Release|x64" 0x602 buildfre_win8_amd64.log
 goto :eof
 
 :Win10_x86
-call :BuildProject "Win10 Release|x86" buildfre_win10_x86.log
+call :BuildProject "Win10 Release|x86" 0xA00 buildfre_win10_x86.log
 goto :eof
 
 :Win10_x64
-call :BuildProject "Win10 Release|x64" buildfre_win10_amd64.log
+call :BuildProject "Win10 Release|x64" 0xA00 buildfre_win10_amd64.log
 goto :eof
 
 :Wlh_x86
-call :BuildProject "Vista Release|x86" buildfre_wlh_x86.log
+call :BuildProject "Vista Release|x86" 0x600 buildfre_wlh_x86.log
 goto :eof
 
 :Wlh_x64
-call :BuildProject "Vista Release|x64" buildfre_wlh_amd64.log
+call :BuildProject "Vista Release|x64" 0x600 buildfre_wlh_amd64.log
 goto :eof
 
 :Wnet_x86
-call :BuildProject "Win2k3 Release|x86" buildfre_wnet_x86.log
+call :BuildProject "Win2k3 Release|x86" 0x502 buildfre_wnet_x86.log
 goto :eof
 
 :Wnet_x64
-call :BuildProject "Win2k3 Release|x64" buildfre_wnet_amd64.log
+call :BuildProject "Win2k3 Release|x64" 0x502 buildfre_wnet_amd64.log
 goto :eof
 
 :WXp_x86
-call :BuildProject "WinXP Release|x86" buildfre_wxp_x86.log
+call :BuildProject "WinXP Release|x86" 0x501 buildfre_wxp_x86.log
 goto :eof
 
 :BuildProject
 setlocal
-if "%_NT_TARGET_VERSION%"=="" set _NT_TARGET_VERSION=0x602
+if "%_NT_TARGET_VERSION%"=="" set _NT_TARGET_VERSION=%2
 if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
 if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
 if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
@@ -69,7 +69,7 @@ set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% 
 set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
 set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_%
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-call ..\..\tools\callVisualStudio.bat 14 balloon.vcxproj /Rebuild "%~1" /Out %2
+call ..\..\tools\callVisualStudio.bat 14 balloon.vcxproj /Rebuild "%~1" /Out %3
 endlocal
 goto :eof
 
@@ -91,7 +91,6 @@ echo #define _MINORVERSION_ %_BUILD_MINOR_VERSION_%
 goto :eof
 
 :prepare
-set _NT_TARGET_VERSION=0x0602
 call :prepare_version
 call :create2012H  > 2012-defines.h
 goto :eof
