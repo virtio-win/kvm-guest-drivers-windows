fd3f5152b26895edd2f98ae6d0a5cbcfd2f50af7
Bishara AbuHattoum <bishara@daynix.com>
2016-08-10 15:50:35 +0300

    Additions and modifications to NetKVM:
    1. Added a buildAll_Debug script for NetKVM and changed
       the VS project and solution accordingly.
    2. Fixed the bug where the VS compilation wasnt copying
       the install driver's files to the right path, and
       updated the build_All script accordingly, also fixed
       the bug of copying the non stamped inf file.
    
    Signed-off-by: Bishara AbuHattoum <bishara@daynix.com>

---
+++ NetKVM/buildAll_Debug.bat
@@ -0,0 +1,81 @@
+
+:: Build Windows Vista
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Vista Debug|x86" /Out buildchk_win7_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Vista Debug|x64" /Out buildchk_win7_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: Build Windows 7
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win7 Debug|x86" /Out buildchk_win7_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win7 Debug|x64" /Out buildchk_win7_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: Build Windows 8
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8 Debug|x86" /Out buildchk_win8_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8 Debug|x64" /Out buildchk_win8_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: Build Windows 8.1
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8.1 Debug|x86" /Out buildchk_win8_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win8.1 Debug|x64" /Out buildchk_win8_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: Build Windows 10
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win10 Debug|x86" /Out buildchk_win10_x86.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+setlocal
+call tools\set_version.bat
+call ..\tools\callVisualStudio.bat 14 netkvm-vs2015.sln /Rebuild "Win10 Debug|x64" /Out buildchk_win10_amd64.log
+endlocal
+if %ERRORLEVEL% NEQ 0 goto :eof
+
+:: XP
+pushd NDIS5
+call buildAll.bat XPCHK
+popd
+if %ERRORLEVEL% NEQ 0 goto :eof
+xcopy /S /I /Y NDIS5\Install_Checked\XP\x86 Install_Debug\XP\x86
+
+:: XP64
+pushd NDIS5
+call buildAll.bat XP64CHK
+popd
+if %ERRORLEVEL% NEQ 0 goto :eof
+xcopy /S /I /Y NDIS5\Install_Checked\XP\amd64 Install_Debug\XP\amd64
+
+xcopy /y /q tools\NetKVMTemporaryCert.cer Install_Debug\NetKVM.cer*
