553bc2cc977b0dda053029f870251a0c144aaff1
Ladi Prosek <lprosek@redhat.com>
2016-08-10 15:28:44 +0200

    vioinput: Eliminate 2012-defines.h
    This commit eliminates 2012-defines.h which was being generated
    during build. The required version macros are now defined in
    Driver.Common.props included from the project.

--- vioinput/sys/buildOne.bat
+++ vioinput/sys/buildOne.bat
@@ -4,12 +4,6 @@ setlocal
 if "%DDKINSTALLROOT%"=="" set DDKINSTALLROOT=C:\WINDDK\
 if "%DDKVER%"=="" set DDKVER=7600.16385.1
 
-if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
-if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
-if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
-
-if /i "%1"=="prepare" goto %1
-
 call :%1_%2
 goto :eof
 
@@ -39,39 +33,11 @@ goto :eof
 
 :BuildProject
 setlocal
-if "%_NT_TARGET_VERSION%"=="" set _NT_TARGET_VERSION=0x602
-if "%_BUILD_MAJOR_VERSION_%"=="" set _BUILD_MAJOR_VERSION_=101
-if "%_BUILD_MINOR_VERSION_%"=="" set _BUILD_MINOR_VERSION_=58000
-if "%_RHEL_RELEASE_VERSION_%"=="" set _RHEL_RELEASE_VERSION_=61
 
 set _MAJORVERSION_=%_BUILD_MAJOR_VERSION_%
 set _MINORVERSION_=%_BUILD_MINOR_VERSION_%
-set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
 set _NT_TARGET_MIN=%_RHEL_RELEASE_VERSION_%
-set STAMPINF_VERSION=%_NT_TARGET_MAJ%.%_RHEL_RELEASE_VERSION_%.%_BUILD_MAJOR_VERSION_%.%_BUILD_MINOR_VERSION_%
+
 call ..\..\tools\callVisualStudio.bat 14 vioinput.vcxproj /Rebuild "%~1" /Out %2
 endlocal
-goto :eof
-
-:prepare_version
-set /a _NT_TARGET_MAJ="(%_NT_TARGET_VERSION% >> 8) * 10 + (%_NT_TARGET_VERSION% & 255)"
-goto :eof
-
-:create2012H
-echo #ifndef __DATE__
-echo #define __DATE__ "%DATE%"
-echo #endif
-echo #ifndef __TIME__
-echo #define __TIME__ "%TIME%"
-echo #endif
-echo #define _NT_TARGET_MAJ %_NT_TARGET_MAJ%
-echo #define _NT_TARGET_MIN %_RHEL_RELEASE_VERSION_%
-echo #define _MAJORVERSION_ %_BUILD_MAJOR_VERSION_%
-echo #define _MINORVERSION_ %_BUILD_MINOR_VERSION_%
-goto :eof
-
-:prepare
-set _NT_TARGET_VERSION=0x0602
-call :prepare_version
-call :create2012H  > 2012-defines.h
 goto :eof
