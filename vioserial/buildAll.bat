748fa255e0b3e80737e7426d98b287e813d9b7b9
Ladi Prosek <lprosek@redhat.com>
2016-10-11 12:43:41 +0200

    vioser: Refactor sys/packOne.bat into a post-build step
    This commit reworks the packOne.bat script into a post-build step
    invoked from VS/MSBuild as opposed to something that needs to be
    run after the build finishes. It is part of a longer-term plan to
    have VS/MSBuild drive the build for better error propagation,
    incremental build support, and more.
    
    A couple of hard-coded paths are also removed from packOne.bat.

--- vioserial/buildAll.bat
+++ vioserial/buildAll.bat
@@ -20,12 +20,6 @@ call buildOne.bat %1 %2
 cd ..
 goto :eof
 
-:packsys
-cd sys
-call packOne.bat %1 %2 vioser
-cd ..
-goto :eof
-
 :packapp
 cd app
 call packOne.bat %1 %2 vioser-test
@@ -39,8 +33,6 @@ set BUILD_ARC=%2
 set BUILD_ARC=x%BUILD_ARC:32=86%
 call :buildsys %1 %BUILD_ARC%
 call :buildapp %1 %BUILD_ARC%
-call :packsys %1 %BUILD_ARC%
 call :packapp %1 %BUILD_ARC%
 endlocal
 goto :eof
-
