4d408040c720d037c65ea5360f80d7bd5429a9d2
Ladi Prosek <lprosek@redhat.com>
2016-10-11 13:22:18 +0200

    vioinput: Refactor sys/packOne.bat into a post-build step
    This commit reworks the packOne.bat script into a post-build step
    invoked from VS/MSBuild as opposed to something that needs to be
    run after the build finishes. It is part of a longer-term plan to
    have VS/MSBuild drive the build for better error propagation,
    incremental build support, and more.
    
    A couple of hard-coded paths are also removed from packOne.bat.

--- vioinput/buildAll.bat
+++ vioinput/buildAll.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%1_%2" neq "_" (
-  call :buildpack %1 %2
+  call :build %1 %2
 ) else (
-  for %%A in (Win7 Win8 Win10) do for %%B in (32 64) do call :buildpack %%A %%B
+  for %%A in (Win7 Win8 Win10) do for %%B in (32 64) do call :build %%A %%B
 )
 
 goto :eof
@@ -14,17 +14,10 @@ call buildOne.bat %1 %2
 cd ..
 goto :eof
 
-:packsys
-cd sys
-call packOne.bat %1 %2 vioinput
-cd ..
-goto :eof
-
-:buildpack
+:build
 setlocal
 set BUILD_ARC=%2
 set BUILD_ARC=x%BUILD_ARC:32=86%
 call :buildsys %1 %BUILD_ARC%
-call :packsys %1 %BUILD_ARC%
 endlocal
 goto :eof
