904dc7c6c9c1c6d149a3d2ec5ce2391f0eb770e0
Ladi Prosek <lprosek@redhat.com>
2016-10-11 12:10:53 +0200

    balloon: Refactor packOne.bat into a post-build step
    This commit reworks the packOne.bat script into a post-build step
    invoked from VS/MSBuild as opposed to something that needs to be
    run after the build finishes. It is part of a longer-term plan to
    have VS/MSBuild drive the build for better error propagation,
    incremental build support, and more.
    
    A couple of hard-coded paths are also removed from packOne.bat.

--- Balloon/buildAll.bat
+++ Balloon/buildAll.bat
@@ -1,9 +1,9 @@
 @echo off
 
 if "%1_%2" neq "_" (
-  call :buildpack %1 %2
+  call :build %1 %2
 ) else (
-  for %%A in (WXp Wnet Wlh Win7 Win8 Win10) do for %%B in (32 64) do call :buildpack %%A %%B
+  for %%A in (WXp Wnet Wlh Win7 Win8 Win10) do for %%B in (32 64) do call :build %%A %%B
 )
 
 goto :eof 
@@ -14,19 +14,11 @@ call buildOne.bat %1 %2
 cd ..
 goto :eof
 
-:packsys
-cd sys
-call packOne.bat %1 %2 balloon
-cd ..
-goto :eof
-
-:buildpack
+:build
 if /i "%1"=="WXp" if "%2"=="64" goto :eof
 set BUILD_ARC=%2
 set BUILD_ARC=x%BUILD_ARC:32=86%
 call :buildsys %1 %BUILD_ARC%
-call :packsys %1 %BUILD_ARC%
 set BUILD_OS=
 set BUILD_ARC=
 goto :eof
-
