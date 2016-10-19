ec8903469a09aae80110717522553f3e0aef7a16
Ladi Prosek <lprosek@redhat.com>
2016-10-19 16:05:31 +0200

    virtio: Switch over to Tools/build.bat
    This commit switches over to the one shared build script.
    buildAll.bat now by default performs an incremental build.

--- VirtIO/buildAll.bat
+++ VirtIO/buildAll.bat
@@ -1,64 +1,6 @@
 @echo off
-
-if "%1_%2" neq "_" (
-  call :%1_%2
-) else (
-  call clean.bat
-  for %%A in (Wxp Wnet Wlh Win7 Win8 Win10) do for %%B in (32 64) do call :%%A_%%B
-)
+call ..\tools\build.bat VirtioLib.vcxproj "Wxp Wnet Wlh Win7 Win8 Win10" %*
 
 pushd WDF
 call buildAll.bat %*
 popd
-goto :eof
-
-:Wlh_32
-call :BuildProject "Vista Release|x86" buildfre_wlh_x86.log
-goto :eof
-
-:Wlh_64
-call :BuildProject "Vista Release|x64" buildfre_wlh_amd64.log
-goto :eof
-
-:Wnet_32
-call :BuildProject "Win2k3 Release|x86" buildfre_wnet_x86.log
-goto :eof
-
-:Wnet_64
-call :BuildProject "Win2k3 Release|x64" buildfre_wnet_amd64.log
-goto :eof
-
-:Wxp_32
-call :BuildProject "WinXP Release|x86" buildfre_wxp_x86.log
-goto :eof
-
-:Wxp_64
-goto :eof
-
-:Win7_32
-call :BuildProject "Win7 Release|x86" buildfre_win7_x86.log
-goto :eof
-
-:Win7_64
-call :BuildProject "Win7 Release|x64" buildfre_win7_amd64.log
-goto :eof
-
-:Win8_32
-call :BuildProject "Win8 Release|x86" buildfre_win8_x86.log
-goto :eof
-
-:Win8_64
-call :BuildProject "Win8 Release|x64" buildfre_win8_amd64.log
-goto :eof
-
-:Win10_32
-call :BuildProject "Win10 Release|x86" buildfre_win10_x86.log
-goto :eof
-
-:Win10_64
-call :BuildProject "Win10 Release|x64" buildfre_win10_amd64.log
-goto :eof
-
-:BuildProject
-call ..\tools\callVisualStudio.bat 14 VirtioLib.vcxproj /Rebuild "%~1" /Out %2
-goto :eof
