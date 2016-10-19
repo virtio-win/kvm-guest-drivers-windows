33721e419aec97ba1ff781e301cde633445bd6f4
Ladi Prosek <lprosek@redhat.com>
2016-10-19 14:21:45 +0200

    netkvm: Switch over to Tools/build.bat
    This commit switches over to the one shared build script.
    buildAll.bat now by default performs an incremental build.

--- NetKVM/create_dvl_log_vs2015.cmd
+++
@@ -1,8 +0,0 @@
-setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
-msbuild.exe NetKVM-VS2015.vcxproj /t:clean /p:Configuration="Win10 Release" /P:Platform=x64
-msbuild.exe NetKVM-VS2015.vcxproj /t:sdv /p:inputs="/clean" /p:Configuration="Win10 Release" /P:platform=x64
-msbuild.exe NetKVM-VS2015.vcxproj /p:Configuration="Win10 Release" /P:Platform=x64 /P:RunCodeAnalysisOnce=True
-msbuild.exe NetKVM-VS2015.vcxproj /t:sdv /p:inputs="/check" /p:Configuration="Win10 Release" /P:platform=x64
-msbuild.exe NetKVM-VS2015.vcxproj /t:dvl /p:Configuration="Win10 Release" /P:platform=x64
-endlocal
