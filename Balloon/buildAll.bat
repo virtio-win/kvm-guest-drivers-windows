aa9775b961c50109fd18a319d2f0182083cbc9a1
Ladi Prosek <lprosek@redhat.com>
2016-10-20 12:01:23 +0200

    balloon: Add balloon.sln with all dependencies
    The new solution file references all projects required to build
    the balloon driver. Dependencies are properly configured so
    balloon/buildAll.bat will succeed on a clean source tree.

--- Balloon/buildAll.bat
+++ Balloon/buildAll.bat
@@ -1,2 +1,1 @@
-@call ..\tools\build.bat sys\balloon.vcxproj "WXp Wnet Wlh Win7 Win8 Win10" %*
-@call ..\tools\build.bat app\blnsvr.vcxproj "WXp Wnet Wlh Win7 Win8 Win10" %*
+@call ..\tools\build.bat balloon.sln "WXp Wnet Wlh Win7 Win8 Win10" %*
