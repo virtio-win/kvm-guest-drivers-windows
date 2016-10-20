2b9704a2a6186846c04a5164c4b5619ed90c042c
Ladi Prosek <lprosek@redhat.com>
2016-10-20 09:50:23 +0200

    vioser: Add vioser.sln with all dependencies
    The new solution file references all projects required to build
    the vioserial driver. Dependencies are properly configured so
    vioserial/buildAll.bat will succeed on a clean source tree.

--- vioserial/buildAll.bat
+++ vioserial/buildAll.bat
@@ -1,2 +1,1 @@
-@call ..\tools\build.bat sys\vioser.vcxproj "WXp Wnet Wlh Win7 Win8 Win10" %*
-@call ..\tools\build.bat app\vioser-test.vcxproj "WXp Wnet Wlh Win7 Win8 Win10" %*
+@call ..\tools\build.bat vioser.sln "WXp Wnet Wlh Win7 Win8 Win10" %*
