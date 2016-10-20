c56b1311e6dc327addee250d194657bc490e59ab
Ladi Prosek <lprosek@redhat.com>
2016-10-20 11:24:02 +0200

    viostor: Add viostor.sln with all dependencies
    The new solution file references all projects required to build
    the viostor driver. Dependencies are properly configured so
    viostor/buildAll.bat will succeed on a clean source tree.

--- viostor/buildAll.bat
+++ viostor/buildAll.bat
@@ -1,1 +1,4 @@
-@call ..\tools\build.bat viostor.vcxproj "Wxp Wnet Wlh Win7 Win8 Win10 Win8_SDV Win10_SDV" %*
+@echo off
+call ..\tools\build.bat viostor.sln "Wxp Wnet Wlh Win7 Win8 Win10" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat viostor.vcxproj "Win8_SDV Win10_SDV" %*
