f011f0ac7b5541c498fb896b45193012bbdf472c
Ladi Prosek <lprosek@redhat.com>
2016-10-20 11:52:48 +0200

    netkvm: Update NetKVM-VS2015.sln and NDIS5/NetKVM-NDIS5.sln with dependencies
    Dependencies are properly configured so NetKVM/buildAll.bat will
    succeed on a clean source tree.

--- NetKVM/buildAll.bat
+++ NetKVM/buildAll.bat
@@ -1,3 +1,6 @@
-@call ..\tools\build.bat netkvm-vs2015.sln "Wlh Win7 Win8 Win8.1 Win10" %*
-@call ..\tools\build.bat NetKVM-VS2015.vcxproj "Win10_SDV" %*
-@call ..\tools\build.bat NDIS5\NetKVM-NDIS5.sln "WXp Wnet" %*
+@echo off
+call ..\tools\build.bat netkvm-vs2015.sln "Wlh Win7 Win8 Win8.1 Win10" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat NetKVM-VS2015.vcxproj "Win10_SDV" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat NDIS5\NetKVM-NDIS5.sln "WXp Wnet" %*
