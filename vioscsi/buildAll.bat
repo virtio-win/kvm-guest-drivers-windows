6437c3b3c2e9e42894b996ab3a7002610ae37c5c
Ladi Prosek <lprosek@redhat.com>
2016-10-20 11:15:28 +0200

    vioscsi: Add vioscsi.sln with all dependencies
    The new solution file references all projects required to build
    the vioscsi driver. Dependencies are properly configured so
    vioscsi/buildAll.bat will succeed on a clean source tree.

--- vioscsi/buildAll.bat
+++ vioscsi/buildAll.bat
@@ -1,1 +1,4 @@
-@call ..\tools\build.bat vioscsi.vcxproj "Wlh Win7 Win8 Win10 Win8_SDV Win10_SDV" %*
+@echo off
+call ..\tools\build.bat vioscsi.sln "Wlh Win7 Win8 Win10" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat vioscsi.vcxproj "Win8_SDV Win10_SDV" %*
