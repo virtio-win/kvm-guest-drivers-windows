d127f6686cb0b6c4a2b4f17d953f54596904335a
Angel Pagan <apagan@stratus.com>
2017-01-09 16:36:19 -0500

    Fix bounds checking for 4K sector drives

--- viostor/virtio_stor.c
+++ viostor/virtio_stor.c
@@ -1282,7 +1282,7 @@ RhelScsiGetCapacity(
 
     blocksize = adaptExt->info.blk_size;
     lastLBA = adaptExt->info.capacity / (blocksize / SECTOR_SIZE) - 1;
-    adaptExt->lastLBA = lastLBA;
+    adaptExt->lastLBA = adaptExt->info.capacity;
 
     if (Srb->DataTransferLength == sizeof(READ_CAPACITY_DATA)) {
         if (lastLBA > 0xFFFFFFFF) {
