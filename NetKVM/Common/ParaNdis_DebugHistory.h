a97911e4946724541f6a5adde01af79b79e6ce26
Yuri Benditovich <yuri.benditovich@daynix.com>
2017-01-26 01:08:58 +0200

    netkvm: Add ability to keep array of pending TX NBLs
    This debug instrumentation is resolved to nothing in
    regular release compilation. To enable it, uncomment
    KEEP_PENDING_NBL define in ParaNdis_DebugHistory.h
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/Common/ParaNdis_DebugHistory.h
+++ NetKVM/Common/ParaNdis_DebugHistory.h
@@ -1,6 +1,29 @@
 #pragma once
 
 //#define ENABLE_HISTORY_LOG
+//#define KEEP_PENDING_NBL
+
+#if !defined(KEEP_PENDING_NBL)
+
+void FORCEINLINE ParaNdis_DebugNBLIn(PNET_BUFFER_LIST nbl, ULONG& index)
+{
+    UNREFERENCED_PARAMETER(nbl);
+    UNREFERENCED_PARAMETER(index);
+}
+
+void FORCEINLINE ParaNdis_DebugNBLOut(ULONG index, PNET_BUFFER_LIST nbl)
+{
+    UNREFERENCED_PARAMETER(index);
+    UNREFERENCED_PARAMETER(nbl);
+}
+
+#else
+
+void ParaNdis_DebugNBLIn(PNET_BUFFER_LIST nbl, ULONG& index);
+void ParaNdis_DebugNBLOut(ULONG index, PNET_BUFFER_LIST nbl);
+
+#endif
+
 #if !defined(ENABLE_HISTORY_LOG)
 
 void FORCEINLINE ParaNdis_DebugHistory(
