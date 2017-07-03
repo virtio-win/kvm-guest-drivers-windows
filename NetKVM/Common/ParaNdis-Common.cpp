virtio-win/kvm-guest-drivers-windows

commit 976828bd46efde5d57a54d7a825ef3ad443e0870

NetKVM: Verification of EtherType before RX checksum calculation
Now, before starting the process of calculating the checksum of an RX
packet, the value of EtherType is checked to correspond either to IPv4
or to IPv6. If this value is different, calculating the checksum of the
packet is skipped, and it is classified as "not an IP packet".

Signed-off-by: Leonid Bloch <lbloch@redhat.com>

NetKVM/Common/ParaNdis-Common.cpp
@@ -2142,13 +2142,14 @@ tChecksumCheckResult ParaNdis_CheckRxChecksum(
                                             PARANDIS_ADAPTER *pContext,
                                             ULONG virtioFlags,
                                             tCompletePhysicalAddress *pPacketPages,
-                                            ULONG ulPacketLength,
+                                            PNET_PACKET_INFO pPacketInfo,
                                             ULONG ulDataOffset,
                                             BOOLEAN verifyLength)
 {
     tOffloadSettingsFlags f = pContext->Offload.flags;
     tChecksumCheckResult res;
     tTcpIpPacketParsingResult ppr;
+    ULONG ulPacketLength = pPacketInfo->dataLength;
     ULONG flagsToCalculate = 0;
     res.value = 0;
 
@@ -2172,8 +2173,16 @@ tChecksumCheckResult ParaNdis_CheckRxChecksum(
         }
     }
 
-    ppr = ParaNdis_CheckSumVerify(pPacketPages, ulPacketLength - ETH_HEADER_SIZE, ulDataOffset + ETH_HEADER_SIZE, flagsToCalculate,
-        verifyLength, __FUNCTION__);
+    if (pPacketInfo->isIP4 || pPacketInfo->isIP6)
+    {
+        ppr = ParaNdis_CheckSumVerify(pPacketPages, ulPacketLength - ETH_HEADER_SIZE,
+                                      ulDataOffset + ETH_HEADER_SIZE, flagsToCalculate,
+                                      verifyLength, __FUNCTION__);
+    }
+    else
+    {
+        ppr.ipStatus = ppresNotIP;
+    }
 
     if (ppr.ipCheckSum == ppresIPTooShort || ppr.xxpStatus == ppresXxpIncomplete)
     {

NetKVM/Common/ndis56common.h
@@ -643,7 +643,7 @@ tChecksumCheckResult ParaNdis_CheckRxChecksum(
                                             PARANDIS_ADAPTER *pContext,
                                             ULONG virtioFlags,
                                             tCompletePhysicalAddress *pPacketPages,
-                                            ULONG ulPacketLength,
+                                            PNET_PACKET_INFO pPacketInfo,
                                             ULONG ulDataOffset,
                                             BOOLEAN verifyLength);
 

NetKVM/wlh/ParaNdis6-Impl.cpp
@@ -866,7 +866,7 @@ tPacketIndicationType ParaNdis_PrepareReceivedPacket(
                     pContext,
                     pHeader->flags,
                     &pBuffersDesc->PhysicalPages[PARANDIS_FIRST_RX_DATA_PAGE],
-                    pPacketInfo->dataLength,
+                    pPacketInfo,
                     nBytesStripped, TRUE);
                 if (csRes.value)
                 {

commit 01ec4748c781858cadb069ee1a78d0245ca6305e

netkvm: Avoid using uninitialized variable
Regression from commit 976828bd46efde5d57a54d7a825ef3ad443e0870,
earlier each received packet was tested with recheck of packet
protocol after stripping VLAN tags.
When the result of checksum check has random value, the driver
can erroneously report packet as one with invalid checksum.
If due to some reason the packet was not recognized by the driver
as IP one, it shall report the checksum as not tested.

Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

NetKVM/Common/ParaNdis-Common.cpp
@@ -2159,6 +2159,7 @@ tChecksumCheckResult ParaNdis_CheckRxChecksum(
     }
     else
     {
+        ppr.value = 0;
         ppr.ipStatus = ppresNotIP;
     }
 