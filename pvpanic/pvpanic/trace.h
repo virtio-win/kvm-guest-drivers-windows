80d71f5ce2d03c80f3c570598c610fc4bc84c573
Ladi Prosek <lprosek@redhat.com>
2017-08-01 13:06:41 +0200

    pvpanic: Switch to BSD license
    The project is switching from GPL to BSD.
    
    Signed-off-by: Ladi Prosek <lprosek@redhat.com>

--- pvpanic/pvpanic/trace.h
+++ pvpanic/pvpanic/trace.h
@@ -1,26 +1,30 @@
 /*
  * Copyright (C) 2015-2017 Red Hat, Inc.
  *
- * This software is licensed under the GNU General Public License,
- * version 2 (GPLv2) (see COPYING for details), subject to the following
- * clarification.
- *
- * With respect to binaries built using the Microsoft(R) Windows Driver
- * Kit (WDK), GPLv2 does not extend to any code contained in or derived
- * from the WDK ("WDK Code"). As to WDK Code, by using or distributing
- * such binaries you agree to be bound by the Microsoft Software License
- * Terms for the WDK. All WDK Code is considered by the GPLv2 licensors
- * to qualify for the special exception stated in section 3 of GPLv2
- * (commonly known as the system library exception).
- *
- * There is NO WARRANTY for this software, express or implied,
- * including the implied warranties of NON-INFRINGEMENT, TITLE,
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Refer to the LICENSE file for full details of the license.
- *
  * Written By: Gal Hammer <ghammer@redhat.com>
  *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met :
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and / or other materials provided with the distribution.
+ * 3. Neither the names of the copyright holders nor the names of their contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
 
 //
