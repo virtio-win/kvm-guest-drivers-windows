2f55b0718eccca46150cb691e97a97f3b80b35b3
Ladi Prosek <lprosek@redhat.com>
2017-11-02 08:25:52 +0100

    ivshmem: Fully integrate with virtio-win build
    * Adds buildAll/cleanAll scripts and a per-driver solution.
    * Makes all binaries drop to ivshmem\Install.
    * Renames test.exe to ivshmem-test.exe.
    * Fixes SDV build (it didn't like the <OutDir> and <IntDir> settings
      for some reason).
    
    Signed-off-by: Ladi Prosek <lprosek@redhat.com>

---
+++ ivshmem/buildAll.bat
@@ -0,0 +1,1 @@
+@call ..\tools\build.bat ivshmem.sln "Win10" %*
---
+++ ivshmem/cleanAll.bat
@@ -0,0 +1,19 @@
+@echo on
+
+rmdir /S /Q Install
+
+for /d %%x in (objfre_*) do rmdir /S /Q %%x
+for /d %%x in (objchk_*) do rmdir /S /Q %%x
+
+rmdir /S /Q .\sdv
+rmdir /S /Q .\sdv.temp
+
+del /F *.log *.wrn *.err
+del ivshmem.dvl.xml
+del sdv-map.h
+del sdv-user.sdv
+del SDV-default.xml
+
+cd test
+call cleanAll.bat
+cd ..
