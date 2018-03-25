634052314afb950d433307685db13c2f5aca2aeb
Yuri Benditovich <yuri.benditovich@daynix.com>
2018-03-25 17:44:09 +0300

    viocrypt: initial addition of projects and files
    Added driver, provider, test and package projects
    Currently they are for build purpose only.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

---
+++ viocrypt/sys/WppTrace.h
@@ -0,0 +1,72 @@
+/*
+* Copyright (C) 2018 Red Hat, Inc.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions
+* are met :
+* 1. Redistributions of source code must retain the above copyright
+*    notice, this list of conditions and the following disclaimer.
+* 2. Redistributions in binary form must reproduce the above copyright
+*    notice, this list of conditions and the following disclaimer in the
+*    documentation and / or other materials provided with the distribution.
+* 3. Neither the names of the copyright holders nor the names of their contributors
+*    may be used to endorse or promote products derived from this software
+*    without specific prior written permission.
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+* ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+* ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+* OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+* HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+* LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+* OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+* SUCH DAMAGE.
+*/
+//
+// Define the tracing flags.
+//
+// Tracing GUID is the same as device interface GUID
+// {0F0EA958-378D-4FE5-A0C5-EB1D02C3063C}
+//
+
+#pragma once
+
+#define WPP_CONTROL_GUIDS \
+    WPP_DEFINE_CONTROL_GUID( \
+        VioCryptoTraceGuid, (0F0EA958,378D,4FE5,A0C5,EB1D02C3063C), \
+                                             \
+        WPP_DEFINE_BIT(TRACE_DRIVER)         \
+    )
+
+#define WPP_FLAG_LEVEL_LOGGER(flag, level) \
+    WPP_LEVEL_LOGGER(flag)
+
+#define WPP_FLAG_LEVEL_ENABLED(flag, level) \
+    (WPP_LEVEL_ENABLED(flag) && WPP_CONTROL(WPP_BIT_ ## flag).Level >= level)
+
+#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags) \
+    WPP_LEVEL_LOGGER(flags)
+
+#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) \
+    (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
+
+
+//
+// This comment block is scanned by the trace preprocessor to define our
+// Trace function.
+//
+// begin_wpp config
+// FUNC Trace{FLAG=TRACE_DRIVER}(LEVEL,MSG, ...);
+// FUNC TraceEvents(LEVEL, FLAGS, MSG, ...);
+// FUNC DbgPrint{FLAG=TRACE_DRIVER}(LEVEL, (MSG, ...));
+// end_wpp
+//
+
+#if (NTDDI_VERSION < NTDDI_WIN7)
+#define NO_WPP
+#define Trace(...) {}
+#define WPP_INIT_TRACING(a,b)
+#define WPP_CLEANUP(a)
+#endif
