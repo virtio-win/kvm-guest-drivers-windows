fad97a5c0fd05d6568ee75a8f94b502ae6d8eed2
Sameeh Jubran <sameeh@daynix.com>
2018-05-20 04:48:38 +0300

    NetKVM: Log interrupt vector to CPU mapping
    Signed-off-by: Sameeh Jubran <sameeh@daynix.com>

--- NetKVM/wlh/ParaNdis6-Driver.cpp
+++ NetKVM/wlh/ParaNdis6-Driver.cpp
@@ -733,6 +733,8 @@ static void SetupInterrruptAffinity(PIO_RESOURCE_REQUIREMENTS_LIST prrl)
                     desc->u.Interrupt.Group = procNumber.Group;
                     desc->u.Interrupt.TargetedProcessors = 1i64 << procNumber.Number;
                     desc->u.Interrupt.AffinityPolicy = IrqPolicySpecifiedProcessors;
+                    DPrintf(0, "[%s]: Assigning CmResourceTypeInterrupt, min/max = %lx/%lx Option = 0x%lx, ShareDisposition = %u to #CPU = %d\n", __FUNCTION__,
+                        desc->u.Interrupt.MinimumVector, desc->u.Interrupt.MaximumVector, desc->Option, desc->ShareDisposition, procNumber.Number);
                 }
                 else
                 {
