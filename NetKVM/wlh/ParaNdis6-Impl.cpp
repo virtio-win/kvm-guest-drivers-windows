7b4cc2852d06398ae75d0ff50049a560f9789211
zhengrong.lzr <zhengrong.lzr@alibaba-inc.com>
2018-09-03 11:47:15 +0800

    netkvm need to do with enable msi that not used.
    netkvm when qemu enable 8 queue (msi count is 17), nPathBundles is 2, qemu send msi large than 6, will result os crash.
    eg: pContext->pPathBundles[i],  i will match the index that had not init.

--- NetKVM/wlh/ParaNdis6-Impl.cpp
+++ NetKVM/wlh/ParaNdis6-Impl.cpp
@@ -262,7 +262,11 @@ static CParaNdisAbstractPath *GetPathByMessageId(PARANDIS_ADAPTER *pContext, ULO
     CParaNdisAbstractPath *path = NULL;
 
     UINT bundleId = MessageId / 2;
-    if (pContext->CXPath.getMessageIndex() == MessageId)
+	if (pContext->CXPath.getMessageIndex() < MessageId)
+	{
+		path = NULL;
+	}
+	else if (pContext->CXPath.getMessageIndex() == MessageId)
     {
         path = &pContext->CXPath;
     }
@@ -307,6 +311,10 @@ static BOOLEAN MiniportMSIInterrupt(
     }
 
     CParaNdisAbstractPath *path = GetPathByMessageId(pContext, MessageId);
+	if (NULL == path) {
+		*QueueDefaultInterruptDpc = FALSE;
+		return TRUE;
+	}
 
     path->SetLastInterruptTimestamp(pContext->LastInterruptTimeStamp);
 
