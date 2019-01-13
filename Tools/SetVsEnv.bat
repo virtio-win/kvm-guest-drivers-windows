39f0d4ad80527fe1e4045d5ad9fb7ae52a02d366
Yuri Benditovich <yuri.benditovich@daynix.com>
2019-01-13 10:22:22 +0200

    build: support Community and Professional VS 2017
    For Brew build and other configurations which use
    Professional VS.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- Tools/SetVsEnv.bat
+++ Tools/SetVsEnv.bat
@@ -1,9 +1,14 @@
 @echo off
-echo %0: Setting NATIVE ENV for %1.
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" %1
+if not "%VSFLAVOR%"=="" goto :knownVS
+call :checkvs
+echo USING %VSFLAVOR% Visual Studio
 
+:knownVS
+echo %0: Setting NATIVE ENV for %1 (VS %VSFLAVOR%)...
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\%VSFLAVOR%\VC\Auxiliary\Build\vcvarsall.bat" %1
+goto :eof
 
-
-
-
-
+:checkvs
+set VSFLAVOR=Professional
+if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" set VSFLAVOR=Community
+goto :eof
