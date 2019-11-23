c46f38f6243623c35f44f795a96c52b5102721e1
Yuri Benditovich <yuri.benditovich@daynix.com>
2019-11-23 10:11:09 +0200

    netkvm: avoid rebuilding the driver each time
    Do not update netkvmmof.h without need, check whether
    generated header file is different from existing one,
    only then update it.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/Mof/build.cmd
+++ NetKVM/Mof/build.cmd
@@ -1,2 +1,8 @@
 mofcomp.exe -B:.\netkvm.bmf ..\Common\netkvm.mof
-wmimofck.exe -h..\Common\netkvmmof.h -m -u .\netkvm.bmf
+wmimofck.exe -h..\Common\tmpmof.h -m -u .\netkvm.bmf
+fc ..\Common\netkvmmof.h ..\Common\tmpmof.h > nul
+if not errorlevel 1 goto thesame
+echo Updating netkvmmof.h
+copy /Y ..\Common\tmpmof.h ..\Common\netkvmmof.h
+:thesame
+del ..\Common\tmpmof.h
