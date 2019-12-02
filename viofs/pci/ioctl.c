c657b9761a4b31a18c10250db9f81710a650265a
Gal Hammer <ghammer@redhat.com>
2019-12-02 11:34:30 +0200

    viofs: initial import

---
+++ viofs/pci/ioctl.c
@@ -0,0 +1,364 @@
+/*
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * Written By: Gal Hammer <ghammer@redhat.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met :
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and / or other materials provided with the distribution.
+ * 3. Neither the names of the copyright holders nor the names of their contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "viofs.h"
+#include "ioctl.tmh"
+
+EVT_WDF_REQUEST_CANCEL VirtFsEvtRequestCancel;
+
+static int GetVirtQueueIndex(IN PDEVICE_CONTEXT Context)
+{
+    int index = 0;
+
+    UNREFERENCED_PARAMETER(Context);
+    
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "VirtQueueIndex: %d", index);
+    
+    return index;
+}
+
+static SIZE_T GetRequiredScatterGatterSize(IN PVIRTIO_FS_REQUEST Request)
+{
+    SIZE_T n;
+    
+    n = ((Request->InputBufferLength / PAGE_SIZE) + 1) + 
+        ((Request->OutputBufferLength / PAGE_SIZE) + 1);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "Required SG Size: %Iu", n);
+
+    return n;
+}
+
+static PMDL VirtFsAllocatePages(IN SIZE_T TotalBytes)
+{
+    PHYSICAL_ADDRESS low_addr;
+    PHYSICAL_ADDRESS high_addr;
+    PHYSICAL_ADDRESS skip_bytes;
+
+    low_addr.QuadPart = 0;
+    high_addr.QuadPart = -1;
+    skip_bytes.QuadPart = 0;
+
+    return MmAllocatePagesForMdlEx(low_addr, high_addr, skip_bytes,
+        TotalBytes, MmNonCached,
+        MM_DONT_ZERO_ALLOCATION | MM_ALLOCATE_FULLY_REQUIRED);
+}
+
+static int FillScatterGatterFromMdl(OUT struct scatterlist sg[],
+                                    IN PMDL Mdl,
+                                    IN size_t Length)
+{
+    PPFN_NUMBER pfn;
+    ULONG total_pages;
+    ULONG len;
+    ULONG j;
+    int i = 0;
+
+    while (Mdl != NULL)
+    {
+        total_pages = MmGetMdlByteCount(Mdl) / PAGE_SIZE;
+        pfn = MmGetMdlPfnArray(Mdl);
+        for (j = 0; j < total_pages; j++)
+        {
+            len = (ULONG)(min(Length, PAGE_SIZE));
+            Length -= len;
+            sg[i].physAddr.QuadPart = (ULONGLONG)(*(pfn + j)) << PAGE_SHIFT;
+            sg[i].length = len;
+            i += 1;
+        }
+        Mdl = Mdl->Next;
+    }
+
+    return i;
+}
+
+static NTSTATUS VirtFsEnqueueRequest(IN PDEVICE_CONTEXT Context,
+                                     IN PVIRTIO_FS_REQUEST Request)
+{
+    WDFSPINLOCK vq_lock;
+    struct virtqueue *vq;
+    struct scatterlist *sg;
+    size_t sg_size;
+    int vq_index;
+    int ret;
+    int out_num, in_num;
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "--> %!FUNC!");
+
+    vq_index = GetVirtQueueIndex(Context);
+    vq = Context->VirtQueues[vq_index];
+    vq_lock = Context->VirtQueueLocks[vq_index];
+
+    sg_size = GetRequiredScatterGatterSize(Request);
+    sg = ExAllocatePoolWithTag(NonPagedPool,
+        sg_size * sizeof(struct scatterlist), VIRT_FS_MEMORY_TAG);
+
+    if (sg == NULL)
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL,
+            "Failed to allocate a %Iu items scatter-gatter list", sg_size);
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    out_num = FillScatterGatterFromMdl(sg, Request->InputBuffer,
+        Request->InputBufferLength);
+    in_num = FillScatterGatterFromMdl(sg + out_num, Request->OutputBuffer,
+        Request->OutputBufferLength);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "Push %p Request: %p",
+        Request, Request->Request);
+
+    WdfSpinLockAcquire(Context->RequestsLock);
+    PushEntryList(&Context->RequestsList, &Request->ListEntry);
+    WdfSpinLockRelease(Context->RequestsLock);
+
+    WdfSpinLockAcquire(vq_lock);
+    ret = virtqueue_add_buf(vq, sg, out_num, in_num, Request, NULL, 0);
+    if (ret < 0)
+    {
+        PSINGLE_LIST_ENTRY iter;
+
+        WdfSpinLockRelease(vq_lock);
+
+        WdfSpinLockAcquire(Context->RequestsLock);
+        iter = &Context->RequestsList;
+        while (iter->Next != NULL)
+        {
+            PVIRTIO_FS_REQUEST removed = CONTAINING_RECORD(iter->Next,
+                VIRTIO_FS_REQUEST, ListEntry);
+
+            if (Request == removed)
+            {
+                TraceEvents(TRACE_LEVEL_VERBOSE, DBG_DPC,
+                    "Delete %p Request: %p", removed, removed->Request);
+                iter->Next = removed->ListEntry.Next;
+                break;
+            }
+
+            iter = iter->Next;
+        };
+        WdfSpinLockRelease(Context->RequestsLock);
+        
+        ExFreePoolWithTag(sg, VIRT_FS_MEMORY_TAG);
+
+        return STATUS_UNSUCCESSFUL;
+    }
+
+    WdfSpinLockRelease(vq_lock);
+    ExFreePoolWithTag(sg, VIRT_FS_MEMORY_TAG);
+
+    virtqueue_kick(vq);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "<-- %!FUNC!");
+
+    return STATUS_SUCCESS;
+}
+
+VOID VirtFsEvtIoDeviceControl(IN WDFQUEUE Queue,
+                              IN WDFREQUEST Request,
+                              IN size_t OutputBufferLength,
+                              IN size_t InputBufferLength,
+                              IN ULONG IoControlCode)
+{
+    PDEVICE_CONTEXT context = GetDeviceContext(WdfIoQueueGetDevice(Queue));
+    WDFMEMORY handle;
+    NTSTATUS status;
+    PVIRTIO_FS_REQUEST fs_req;
+    PVOID in_buf_va;
+    PUCHAR in_buf, out_buf;
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL,
+        "--> %!FUNC! Queue: %p Request: %p IoCtrl: %x InLen: %Iu OutLen: %Iu",
+        Queue, Request, IoControlCode, InputBufferLength, OutputBufferLength);
+
+    if (InputBufferLength < sizeof(struct fuse_in_header))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL, "Insufficient in buffer");
+        status = STATUS_INVALID_PARAMETER;
+        goto complete_wdf_req_no_fs_req;
+    }
+
+    if ((OutputBufferLength > 0) &&
+        (OutputBufferLength < sizeof(struct fuse_out_header)))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL, "Insufficient out buffer");
+        status = STATUS_INVALID_PARAMETER;
+        goto complete_wdf_req_no_fs_req;
+    }
+
+    status = WdfRequestRetrieveInputBuffer(Request, InputBufferLength,
+        &in_buf, NULL);
+
+    if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL,
+            "WdfRequestRetrieveInputBuffer failed");
+        goto complete_wdf_req_no_fs_req;
+    }
+
+    status = WdfRequestRetrieveOutputBuffer(Request, OutputBufferLength,
+        &out_buf, NULL);
+ 
+    if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL,
+            "WdfRequestRetrieveOutputBuffer failed");
+        goto complete_wdf_req_no_fs_req;
+    }
+
+    status = WdfMemoryCreateFromLookaside(context->RequestsLookaside,
+        &handle);
+
+    if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL,
+            "WdfMemoryCreateFromLookaside failed");
+        goto complete_wdf_req_no_fs_req;
+    }
+
+    fs_req = WdfMemoryGetBuffer(handle, NULL);
+    fs_req->Handle = handle;
+    fs_req->Request = Request;
+    fs_req->InputBuffer = VirtFsAllocatePages(InputBufferLength);
+    fs_req->InputBufferLength = InputBufferLength;
+
+    if (OutputBufferLength > 0)
+    {
+        fs_req->OutputBuffer = VirtFsAllocatePages(OutputBufferLength);
+        fs_req->OutputBufferLength = OutputBufferLength;
+    }
+
+    if ((fs_req->InputBuffer == NULL) ||
+        ((OutputBufferLength > 0) && (fs_req->OutputBuffer == NULL)))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL, "Data allocation failed");
+        status = STATUS_INSUFFICIENT_RESOURCES;
+        goto complete_wdf_req;
+    }
+
+    in_buf_va = MmMapLockedPagesSpecifyCache(fs_req->InputBuffer, KernelMode,
+        MmNonCached, NULL, FALSE, NormalPagePriority);
+
+    if (in_buf_va == NULL)
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL, "MmMapLockedPages failed");
+        status = STATUS_INSUFFICIENT_RESOURCES;
+        goto complete_wdf_req;
+    }
+
+    RtlCopyMemory(in_buf_va, in_buf, InputBufferLength);
+    MmUnmapLockedPages(in_buf_va, fs_req->InputBuffer);
+    
+    status = WdfRequestMarkCancelableEx(Request, VirtFsEvtRequestCancel);
+    if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_IOCTL,
+            "WdfRequestMarkCancelableEx failed: %!STATUS!", status);
+        goto complete_wdf_req;
+    }
+
+    status = VirtFsEnqueueRequest(context, fs_req);
+    if (!NT_SUCCESS(status))
+    {
+        if (WdfRequestUnmarkCancelable(Request) != STATUS_CANCELLED)
+        {
+            goto complete_wdf_req;
+        }
+    }
+
+    goto end;
+
+complete_wdf_req:
+    FreeVirtFsRequest(fs_req);
+
+complete_wdf_req_no_fs_req:
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL,
+        "Complete Request: %p Status: %!STATUS!", Request, status);
+    WdfRequestComplete(Request, status);
+
+end:
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "<-- %!FUNC!");
+}
+
+VOID VirtFsEvtIoStop(IN WDFQUEUE Queue,
+                     IN WDFREQUEST Request,
+                     IN ULONG ActionFlags)
+{
+    UNREFERENCED_PARAMETER(Queue);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL,
+        "--> %!FUNC! Request: %p", Request);
+
+    if (ActionFlags & WdfRequestStopActionSuspend)
+    {
+        WdfRequestStopAcknowledge(Request, FALSE);
+    }
+    else if (ActionFlags & WdfRequestStopActionPurge)
+    {
+        if (WdfRequestUnmarkCancelable(Request) != STATUS_CANCELLED)
+        {
+            WdfRequestComplete(Request , STATUS_CANCELLED);
+        }
+    }
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "<-- %!FUNC!");
+}
+
+VOID VirtFsEvtRequestCancel(IN WDFREQUEST Request)
+{
+    PDEVICE_CONTEXT context = GetDeviceContext(WdfIoQueueGetDevice(
+        WdfRequestGetIoQueue(Request)));
+    PSINGLE_LIST_ENTRY iter;
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL,
+        "--> %!FUNC! Cancelled Request: %p", Request);
+    
+    WdfRequestComplete(Request, STATUS_CANCELLED);
+
+    WdfSpinLockAcquire(context->RequestsLock);
+    iter = &context->RequestsList;
+    while (iter->Next != NULL)
+    {
+        PVIRTIO_FS_REQUEST entry = CONTAINING_RECORD(iter->Next,
+            VIRTIO_FS_REQUEST, ListEntry);
+
+        if (Request == entry->Request)
+        {
+            TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL,
+                "Clear virtio fs request %p", entry);
+            entry->Request = NULL;
+            break;
+        }
+
+        iter = iter->Next;
+    };
+    WdfSpinLockRelease(context->RequestsLock);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_IOCTL, "<-- %!FUNC!");
+}
---
+++ viofs/pci/power.c
@@ -0,0 +1,248 @@
+/*
+ * Copyright (C) 2019 Red Hat, Inc.
+ *
+ * Written By: Gal Hammer <ghammer@redhat.com>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met :
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and / or other materials provided with the distribution.
+ * 3. Neither the names of the copyright holders nor the names of their contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#include "viofs.h"
+
+#include "power.tmh"
+
+#ifdef ALLOC_PRAGMA
+#pragma alloc_text(PAGE, VirtFsEvtDevicePrepareHardware)
+#pragma alloc_text(PAGE, VirtFsEvtDeviceReleaseHardware)
+#pragma alloc_text(PAGE, VirtFsEvtDeviceD0Entry)
+#pragma alloc_text(PAGE, VirtFsEvtDeviceD0Exit)
+#endif
+
+NTSTATUS VirtFsEvtDevicePrepareHardware(IN WDFDEVICE Device,
+                                        IN WDFCMRESLIST Resources,
+                                        IN WDFCMRESLIST ResourcesTranslated)
+{
+    PDEVICE_CONTEXT context = GetDeviceContext(Device);
+    NTSTATUS status = STATUS_SUCCESS;
+	u64 HostFeatures, GuestFeatures = 0;
+
+    UNREFERENCED_PARAMETER(Resources);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "--> %!FUNC! Device: %p",
+        Device);
+
+    PAGED_CODE();
+
+    status = VirtIOWdfInitialize(&context->VDevice, Device,
+		ResourcesTranslated, NULL, VIRT_FS_MEMORY_TAG);
+
+	if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_POWER,
+			"VirtIOWdfInitialize failed with %!STATUS!", status);
+    }
+
+	HostFeatures = VirtIOWdfGetDeviceFeatures(&context->VDevice);
+
+	if (virtio_is_feature_enabled(HostFeatures, VIRTIO_F_VERSION_1))
+	{
+		virtio_feature_enable(GuestFeatures, VIRTIO_F_VERSION_1);
+	}
+	if (virtio_is_feature_enabled(HostFeatures, VIRTIO_F_ANY_LAYOUT))
+	{
+		virtio_feature_enable(GuestFeatures, VIRTIO_F_ANY_LAYOUT);
+	}
+
+	VirtIOWdfSetDriverFeatures(&context->VDevice, GuestFeatures);
+
+    VirtIOWdfDeviceGet(&context->VDevice,
+        FIELD_OFFSET(VIRTIO_FS_CONFIG, Tag),
+        &context->Config.Tag,
+        sizeof(context->Config.Tag));
+
+    status = RtlUTF8ToUnicodeN(context->Tag, sizeof(context->Tag), NULL,
+        context->Config.Tag, sizeof(context->Config.Tag));
+
+    if (!NT_SUCCESS(status))
+    {
+        TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER,
+            "Failed to convert config tag: %!STATUS!", status);
+        status = STATUS_SUCCESS;
+    }
+    else
+    {
+        TraceEvents(TRACE_LEVEL_INFORMATION, DBG_POWER,
+            "Config tag: %s Tag: %S", context->Config.Tag, context->Tag);
+    }
+
+	VirtIOWdfDeviceGet(&context->VDevice,
+		FIELD_OFFSET(VIRTIO_FS_CONFIG, RequestQueues),
+		&context->Config.RequestQueues,
+		sizeof(context->Config.RequestQueues));
+
+	TraceEvents(TRACE_LEVEL_INFORMATION, DBG_POWER,
+		"Request queues: %d", context->Config.RequestQueues);
+
+	context->VirtQueues = ExAllocatePoolWithTag(NonPagedPool,
+		context->Config.RequestQueues * sizeof(struct virtqueue*),
+		VIRT_FS_MEMORY_TAG);
+
+	if (context->VirtQueues != NULL)
+	{
+		RtlZeroMemory(context->VirtQueues,
+			context->Config.RequestQueues * sizeof(struct virtqueue*));
+	}
+	else
+	{
+		TraceEvents(TRACE_LEVEL_ERROR, DBG_POWER,
+			"Failed to allocate request queues");
+
+		status = STATUS_INSUFFICIENT_RESOURCES;
+	}
+
+    if (NT_SUCCESS(status))
+    {
+        context->VirtQueueLocks = ExAllocatePoolWithTag(NonPagedPool,
+            context->Config.RequestQueues * sizeof(WDFSPINLOCK),
+            VIRT_FS_MEMORY_TAG);
+    }
+
+    if (context->VirtQueueLocks != NULL)
+    {
+        WDF_OBJECT_ATTRIBUTES attributes;
+        WDFSPINLOCK *lock;
+        ULONG i;
+
+        for (i = 0; i < context->Config.RequestQueues; i++)
+        {
+            lock = &context->VirtQueueLocks[i];
+
+            WDF_OBJECT_ATTRIBUTES_INIT(&attributes);
+            attributes.ParentObject = Device;
+
+            status = WdfSpinLockCreate(&attributes, lock);
+            if (!NT_SUCCESS(status))
+            {
+                TraceEvents(TRACE_LEVEL_ERROR, DBG_INIT,
+                    "WdfSpinLockCreate failed");
+                break;
+            }
+        }
+    }
+    else
+    {
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_POWER,
+            "Failed to allocate queue locks");
+
+        status = STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER,
+        "<-- %!FUNC! Status: %!STATUS!", status);
+
+    return status;
+}
+
+NTSTATUS VirtFsEvtDeviceReleaseHardware(IN WDFDEVICE Device,
+                                        IN WDFCMRESLIST ResourcesTranslated)
+{
+    PDEVICE_CONTEXT context = GetDeviceContext(Device);
+
+    UNREFERENCED_PARAMETER(ResourcesTranslated);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "--> %!FUNC!");
+
+    PAGED_CODE();
+
+    VirtIOWdfShutdown(&context->VDevice);
+
+	if (context->VirtQueues != NULL)
+	{
+		ExFreePoolWithTag(context->VirtQueues, VIRT_FS_MEMORY_TAG);
+		context->VirtQueues = NULL;
+	}
+
+    if (context->VirtQueueLocks != NULL)
+    {
+        ExFreePoolWithTag(context->VirtQueueLocks, VIRT_FS_MEMORY_TAG);
+        context->VirtQueueLocks = NULL;
+    }
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "<-- %!FUNC!");
+
+    return STATUS_SUCCESS;
+}
+
+NTSTATUS VirtFsEvtDeviceD0Entry(IN WDFDEVICE Device,
+                                IN WDF_POWER_DEVICE_STATE PreviousState)
+{
+    NTSTATUS status = STATUS_SUCCESS;
+    PDEVICE_CONTEXT context = GetDeviceContext(Device);
+    VIRTIO_WDF_QUEUE_PARAM param;
+
+    UNREFERENCED_PARAMETER(PreviousState);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "--> %!FUNC! Device: %p",
+        Device);
+
+    PAGED_CODE();
+
+	param.Interrupt = context->WdfInterrupt;
+
+	status = VirtIOWdfInitQueues(&context->VDevice,
+		context->Config.RequestQueues, context->VirtQueues, &param);
+
+    if (NT_SUCCESS(status))
+    {
+        VirtIOWdfSetDriverOK(&context->VDevice);
+    }
+    else
+    {
+        VirtIOWdfSetDriverFailed(&context->VDevice);
+        TraceEvents(TRACE_LEVEL_ERROR, DBG_POWER,
+            "VirtIOWdfInitQueues failed with %x", status);
+    }
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "<-- %!FUNC!");
+
+    return status;
+}
+
+NTSTATUS VirtFsEvtDeviceD0Exit(IN WDFDEVICE Device,
+                               IN WDF_POWER_DEVICE_STATE TargetState)
+{
+    PDEVICE_CONTEXT context = GetDeviceContext(Device);
+
+    UNREFERENCED_PARAMETER(TargetState);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "--> %!FUNC! Device: %p",
+        Device);
+
+    PAGED_CODE();
+
+    VirtIOWdfDestroyQueues(&context->VDevice);
+
+    TraceEvents(TRACE_LEVEL_VERBOSE, DBG_POWER, "<-- %!FUNC!");
+
+    return STATUS_SUCCESS;
+}
