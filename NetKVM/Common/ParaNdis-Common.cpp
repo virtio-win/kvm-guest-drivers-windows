8ef8a178fdf15e5f75d62f451cfd5470804379c3
Yuri Benditovich <yuri.benditovich@daynix.com>
2019-12-19 08:50:00 +0200

    netkvm: make ParaNdis_GetScalingDataForPacket inline
    The procedure was in global scope, but used only with RX/RSS.
    Making it static inline.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/Common/ParaNdis-Common.cpp
+++ NetKVM/Common/ParaNdis-Common.cpp
@@ -1454,20 +1454,6 @@ VOID ParaNdis_ProcessorNumberToGroupAffinity(PGROUP_AFFINITY Affinity, const PPR
     Affinity->Mask <<= Processor->Number;
 }
 
-
-CCHAR ParaNdis_GetScalingDataForPacket(PARANDIS_ADAPTER *pContext, PNET_PACKET_INFO pPacketInfo, PPROCESSOR_NUMBER pTargetProcessor)
-{
-#if PARANDIS_SUPPORT_RSS
-    return ParaNdis6_RSSGetScalingDataForPacket(&pContext->RSSParameters, pPacketInfo, pTargetProcessor);
-#else
-    UNREFERENCED_PARAMETER(pContext);
-    UNREFERENCED_PARAMETER(pPacketInfo);
-    UNREFERENCED_PARAMETER(pTargetProcessor);
-
-    return PARANDIS_RECEIVE_UNCLASSIFIED_PACKET;
-#endif
-}
-
 static __inline
 CCHAR GetReceiveQueueForCurrentCPU(PARANDIS_ADAPTER *pContext)
 {
