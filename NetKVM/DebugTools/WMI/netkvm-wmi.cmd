ea6c2df3c33e44194ab945dfb3c3692772eb5b26
Yuri Benditovich <yuri.benditovich@daynix.com>
2020-01-16 19:35:58 +0200

    netkvm: add custom OID to get RSS statistics
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/DebugTools/WMI/netkvm-wmi.cmd
+++ NetKVM/DebugTools/WMI/netkvm-wmi.cmd
@@ -4,6 +4,7 @@ if "%1"=="" goto help
 if /i "%1"=="debug" goto debug
 if /i "%1"=="stat" goto stat
 if /i "%1"=="reset" goto reset
+if /i "%1"=="rss" goto rss_set
 goto help
 :debug
 call :dowmic netkvm_logging set level=%2
@@ -17,6 +18,15 @@ goto :eof
 call :dowmic netkvm_statistics set rxChecksumOK=0
 goto :eof
 
+:rss
+call :dowmic NetKvm_RssDiagnostics get
+goto :eof
+
+:rss_set
+if "%2"=="" goto rss
+call :dowmic NetKvm_RssDiagnostics set DeviceSupport=%2
+goto :eof
+
 :dowmic
 wmic /namespace:\\root\wmi path %*
 goto :eof
@@ -27,5 +37,7 @@ echo %~nx0 command parameter
 echo debug level            Controls debug level (use level 0..5)
 echo stat                   Retrieves internal statistics
 echo reset                  Resets internal statistics
+echo rss                    Query RSS statistics
+echo rss 0/1                Disable/enable RSS device support
 goto :eof
 
