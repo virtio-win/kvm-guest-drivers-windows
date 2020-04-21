8f2faca7a9327600d65e19f72f2a86850752646a
Gal Hammer <ghammer@redhat.com>
2020-04-21 10:07:11 +0300

    viofs-svc: SetFileSize now handle the SetAllocationSize parameter

--- viofs/svc/fusereq.h
+++ viofs/svc/fusereq.h
@@ -227,3 +227,16 @@ typedef struct
     struct fuse_entry_out   entry;
 
 } FUSE_MKDIR_OUT;
+
+typedef struct
+{
+    struct fuse_in_header       hdr;
+    struct fuse_fallocate_in    falloc;
+
+} FUSE_FALLOCATE_IN;
+
+typedef struct
+{
+    struct fuse_out_header  hdr;
+
+} FUSE_FALLOCATE_OUT;
