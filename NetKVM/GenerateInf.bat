3bb1f37508d452e301e224b6bb682d796239d1e6
Yuri Benditovich <yuri.benditovich@daynix.com>
2020-08-16 11:28:23 +0300

    netkvm: generate INF files
    Avoiding changing 3 different files in order to add/change
    content in all of final INF files.
    Provided 3 source text files:
    1. Basic INF (-RSS -RSC)
    2. Added parameters for RSS
    3. Added parameters for RSC
    Resulting INF is generated by GenerateInf.bat according to
    passed parameter (INF name).
    Names of source INF files for customization are the same as before.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

---
+++ NetKVM/GenerateInf.bat
@@ -0,0 +1,24 @@
+@echo off
+pushd "%~dp0"
+if /i "%~n1"=="netkvm_no_RSS" call :netkvm_no_RSS
+if /i "%~n1"=="netkvm_no_RSC" call :netkvm_no_RSC
+if /i "%~n1"=="netkvm" call :netkvm
+popd
+goto :eof
+:netkvm_no_RSS
+copy /y netkvm-base.txt netkvm_no_RSS.inx.tmp > nul
+call :update netkvm_no_RSS.inx
+goto :eof
+:netkvm_no_RSC
+copy /y netkvm-base.txt + netkvm-add-rss.txt netkvm_no_RSC.inx.tmp > nul
+call :update netkvm_no_RSC.inx
+goto :eof
+:netkvm
+copy /y netkvm-base.txt + netkvm-add-rss.txt + netkvm-add-rsc.txt netkvm.inx.tmp > nul
+call :update netkvm.inx
+goto :eof
+:update
+fc /b %1 %1.tmp >nul 2>&1
+if errorlevel 1 copy /y %1.tmp %1 > nul
+del %1.tmp
+goto :eof
