fa095c9b10825b42a30b2d4b6cee2da2b0f177fc
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-01-25 11:30:05 +0200

    build: separate build of legacy drivers
    Add an ability to disable build of legacy drivers
    (XP to Win7 and respective servers) by environment
    variable VIRTIO_WIN_NO_LEGACY.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- buildAll.bat
+++ buildAll.bat
@@ -1,7 +1,9 @@
 @echo off
 call tools\build.bat virtio-win.sln Win10 ARM64
 if errorlevel 1 goto :fail
-call tools\build.bat virtio-win.sln "Wxp Wnet Wlh Win7 Win8 Win8.1 Win10" %*
+if "%VIRTIO_WIN_NO_LEGACY%"=="" call tools\build.bat virtio-win.sln "Wxp Wnet Wlh Win7" %*
+if errorlevel 1 goto :fail
+call tools\build.bat virtio-win.sln "Win8 Win8.1 Win10" %*
 if errorlevel 1 goto :fail
 call tools\build.bat NetKVM\NetKVM-VS2015.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
