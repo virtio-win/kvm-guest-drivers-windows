bf4b747a0bd62a39eb67baf02ca68905cfc0ee3d
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-04-08 21:56:52 -0700

    build: fix NetKVM clean procedure for EWDK
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/clean_dvl_log.cmd
+++ NetKVM/clean_dvl_log.cmd
@@ -1,5 +1,6 @@
 setlocal
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x64
+call ..\Tools\SetVsEnv.bat
+rmdir /q /s sdv
 msbuild.exe NetKVM-VS2015.vcxproj /t:clean /p:Configuration="Win10 Release" /P:Platform=x64
 msbuild.exe NetKVM-VS2015.vcxproj /t:sdv /p:inputs="/clean" /p:Configuration="Win10 Release" /P:platform=x64
 endlocal
