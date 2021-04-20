6d2832571138f2f1d4a2a175661ba0212ca8a67a
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-04-20 08:52:12 +0300

    netkvm: exclude XP/2003/Vista from the buildall.bat
    This does not affect the common build, only engineering build
    of the specific driver.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/buildAll.bat
+++ NetKVM/buildAll.bat
@@ -1,6 +1,6 @@
 @echo off
-call ..\tools\build.bat netkvm-vs2015.sln "Wlh Win7" %*
+call ..\tools\build.bat netkvm-vs2015.sln "Win7" %*
 if errorlevel 1 goto :eof
 call build_NoLegacy.bat
 if errorlevel 1 goto :eof
-call ..\tools\build.bat NDIS5\NetKVM-NDIS5.sln "WXp Wnet" %*
+::call ..\tools\build.bat NDIS5\NetKVM-NDIS5.sln "WXp Wnet Wlh" %*
