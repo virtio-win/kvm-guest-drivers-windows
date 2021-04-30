d67bcd66522df0c6a19a7f3d6566e404a8b41e57
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-04-30 07:47:37 +0000

    signAll: Switch to EWDK env
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- Tools/signAll.bat
+++ Tools/signAll.bat
@@ -1,5 +1,4 @@
 @echo off
-SET "SignToolDir=C:\Program Files (x86)\Windows Kits\8.1\bin\x64"
-for /r "%~dp0\..\" %%i in (*.sys) do "%SignToolDir%\signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
-for /r "%~dp0\..\" %%i in (*.cat) do "%SignToolDir%\signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
-
+call "%~dp0\SetVsEnv.bat" x86
+for /r "%~dp0\..\" %%i in (*.sys) do "signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
+for /r "%~dp0\..\" %%i in (*.cat) do "signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
