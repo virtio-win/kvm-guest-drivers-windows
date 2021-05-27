a9ee63bcb5dc5177b9ae0d5cf2633971c3314376
Vadim Rozenfeld <vrozenfe@redhat.com>
2021-05-28 09:54:11 +1000

    [viogpudo] add logging and error messaging code
    Signed-off-by: Vadim Rozenfeld <vrozenfe@redhat.com>

---
+++ viogpu/shared/log.cpp
@@ -0,0 +1,49 @@
+#include <windows.h>
+#include <string>
+#include "log.h"
+#include "strsafe.h"
+
+void ErrorHandler(const char *s, int err)
+{
+    wprintf(L"Failed. Error %d\n", err );
+
+    LPWSTR lpMsgBuf = NULL;
+    if (FormatMessage(
+        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+        NULL,
+        err,
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+        (LPWSTR)(&lpMsgBuf),
+        0, NULL) > 0) {
+        wprintf(L"%s\n", lpMsgBuf);
+        PrintMessage(lpMsgBuf);
+        LocalFree(lpMsgBuf);
+    } else {
+        wprintf(L"unknown error\n");
+    }
+
+    FILE* pLog;
+    if (fopen_s(&pLog, "vgpusvp.log", "a") == 0 && pLog) {
+        fprintf(pLog, "%s failed, error code = %d\n", s, err);
+        fclose(pLog);
+    }
+
+    ExitProcess(err);
+}
+
+void PrintMessage(LPCWSTR pFormat, ...)
+{
+    va_list args;
+    WCHAR debugOutputBuffer[1024] = {'0'};
+    va_start(args, pFormat);
+    StringCbVPrintf(debugOutputBuffer, sizeof(debugOutputBuffer), pFormat, args);
+    va_end(args);
+
+    OutputDebugString(debugOutputBuffer);
+
+    FILE* pLog;
+    if (fopen_s(&pLog, "vgpusvp.log", "a") == 0 && pLog) {
+        fprintf(pLog, "%ws\n", debugOutputBuffer);
+        fclose(pLog);
+    }
+}
---
+++ viogpu/shared/log.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <windows.h>
+
+__declspec(noreturn) void ErrorHandler(const char *s, int err);
+void PrintMessage(LPCWSTR pFormat, ...);
