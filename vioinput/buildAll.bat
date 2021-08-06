f21a6d3281a0eba58d9edc9646bb4a56af9b0421
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-08-06 06:51:06 -0700

    vioinput/build: Add SDV target
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- vioinput/buildAll.bat
+++ vioinput/buildAll.bat
@@ -1,1 +1,4 @@
-@call ..\tools\build.bat vioinput.sln "Win7 Win8 Win10" %*
+@echo off
+call ..\tools\build.bat vioinput.sln "Win7 Win8 Win10" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat sys\vioinput.vcxproj "Win10_SDV" %*
