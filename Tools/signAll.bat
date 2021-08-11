8a8a809760d8b5118f7d77d256c8a19f6073bb41
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-08-11 03:36:30 -0700

    singAll: Update command line for new version of signtool
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- Tools/signAll.bat
+++ Tools/signAll.bat
@@ -1,4 +1,4 @@
 @echo off
 call "%~dp0\SetVsEnv.bat" x86
-for /r "%~dp0\..\" %%i in (*.sys) do "signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
-for /r "%~dp0\..\" %%i in (*.cat) do "signtool.exe" sign /f "%~dp0\VirtIOTestCert.pfx" "%%i"
+for /r "%~dp0\..\" %%i in (*.sys) do "signtool.exe" sign /fd SHA256 /f "%~dp0\VirtIOTestCert.pfx" "%%i"
+for /r "%~dp0\..\" %%i in (*.cat) do "signtool.exe" sign /fd SHA256 /f "%~dp0\VirtIOTestCert.pfx" "%%i"
