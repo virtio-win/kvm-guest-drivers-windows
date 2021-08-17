0eaae8f103a51fa1fb2d39873faf85c9426138e3
Vadim Rozenfeld <vrozenfe@redhat.com>
2021-08-17 03:28:56 -0700

    CodeQL fix blitting functions
    Signed-off-by: Vadim Rozenfeld <vrozenfe@redhat.com>

--- viogpu/common/bitops.h
+++ viogpu/common/bitops.h
@@ -22,22 +22,22 @@ BYTE* GetRowStart(
     _In_ CONST RECT* pRect);
 
 VOID CopyBitsGeneric(
-    _Out_ BLT_INFO* pDst,
+    _In_ BLT_INFO* pDst,
     _In_ CONST BLT_INFO* pSrc,
     _In_ UINT  NumRects,
     _In_reads_(NumRects) CONST RECT *pRects);
 
 VOID CopyBits32_32(
-    _Out_ BLT_INFO* pDst,
+    _In_ BLT_INFO* pDst,
     _In_ CONST BLT_INFO* pSrc,
     _In_ UINT  NumRects,
     _In_reads_(NumRects) CONST RECT *pRects);
 
 VOID BltBits(
-    _Out_ BLT_INFO* pDst,
+    _In_ BLT_INFO* pDst,
     _In_ CONST BLT_INFO* pSrc,
     _In_ UINT  NumRects,
     _In_reads_(NumRects) CONST RECT *pRects);
 
 
-UINT BPPFromPixelFormat(D3DDDIFORMAT Format);
+UINT BPPFromPixelFormat(_In_ D3DDDIFORMAT Format);
