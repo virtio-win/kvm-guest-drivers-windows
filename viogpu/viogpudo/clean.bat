59bd27e909d7b66b79605e07ed94db7353ed786c
Vadim Rozenfeld <vrozenfe@redhat.com>
2021-08-17 03:45:04 -0700

    CodeQL clean up codeql_db directory
    Signed-off-by: Vadim Rozenfeld <vrozenfe@redhat.com>

--- viogpu/viogpudo/clean.bat
+++ viogpu/viogpudo/clean.bat
@@ -6,6 +6,7 @@ for /d %%x in (objfre_*) do rmdir /S /Q %%x
 for /d %%x in (objchk_*) do rmdir /S /Q %%x
 rmdir /S /Q .\sdv
 rmdir /S /Q .\sdv.temp
+rmdir /S /Q .\codeql_db
 
 del /F *.log *.wrn *.err *.sdf *.sdv *.xml
 del viogpudo.dvl.xml
