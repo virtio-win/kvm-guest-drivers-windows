virtio-win/kvm-guest-drivers-windows

commit c62a8a2c7bf73541fcfd6bc43bcf85147abf2620

vioscsi: Increasing max phys breaks to 512
The maximum block size has been increased to 512 pages and is now the
same as Viostor's maximum block size. This can give io performance gains
of up to 30% on fast storage. Dynamic memory allocation for sglist has
been removed because the SRB extension now has enough pre-allocated
space to hold the complete sglist. In addition, there was a bug in this
code that led to unnecessary allocation and copying for requests of
MAX_PHYS_SEGMENTS size, which also reduced performance.

Signed-off-by: Ilya Rudakov <irudakov@virtuozzo.com>

vioscsi/vioscsi.c
@@ -331,7 +331,7 @@ BOOLEAN VioScsiReadRegistry(
            sizeof(ULONG));
     adaptExt->max_physical_breaks = min(
                                         max(SCSI_MINIMUM_PHYSICAL_BREAKS, adaptExt->max_physical_breaks),
-                                        SCSI_MAXIMUM_PHYSICAL_BREAKS);
+                                        MAX_PHYS_SEGMENTS);
 
     StorPortFreeRegistryBuffer(DeviceExtension, pBuf );
 
@@ -1348,46 +1348,7 @@ ENTER_FN_SRB();
     sgList = StorPortGetScatterGatherList(DeviceExtension, Srb);
     if (sgList)
     {
-        sgMaxElements = sgList->NumberOfElements;
-
-#if (NTDDI_VERSION > NTDDI_WIN7)
-        if (sgMaxElements > MAX_PHYS_SEGMENTS && adaptExt->indirect)
-        {
-            PHYSICAL_ADDRESS Low;
-            PHYSICAL_ADDRESS High;
-            PHYSICAL_ADDRESS Alighn;
-            ULONG Status =   STATUS_SUCCESS;
-            srbExt->allocated = sgMaxElements + 3;
-
-            Status = StorPortAllocatePool(DeviceExtension,
-                                    sizeof(VIO_SG) * (srbExt->allocated),
-                                    VIOSCSI_POOL_TAG,
-                                    (PVOID*)&srbExt->psgl);
-            if (!NT_SUCCESS(Status)) {
-                RhelDbgPrint(TRACE_LEVEL_FATAL, " FAILED to allocate pool with status 0x%x\n", Status);
-                return FALSE;
-            }
-
-            memcpy(srbExt->psgl, srbExt->vio_sg, sizeof(VIO_SG));
-            Low.QuadPart = 0;
-            High.QuadPart = (-1);
-            Alighn.QuadPart = 0;
-            Status = StorPortAllocateContiguousMemorySpecifyCacheNode(
-                                    DeviceExtension,
-                                    sizeof(VRING_DESC_ALIAS) * (srbExt->allocated),
-                                    Low, High, Alighn,
-                                    MmCached,
-                                    MM_ANY_NODE_OK,
-                                    (PVOID*)&srbExt->pdesc);
-            if (!NT_SUCCESS(Status)) {
-                RhelDbgPrint(TRACE_LEVEL_FATAL, " FAILED to allocate contiguous memory with status 0x%x\n", Status);
-                StorPortFreePool(DeviceExtension, srbExt->psgl);
-                srbExt->allocated = 0;
-                srbExt->psgl = srbExt->vio_sg;
-                return FALSE;
-            }
-        }
-#endif
+        sgMaxElements = min((MAX_PHYS_SEGMENTS + 1), sgList->NumberOfElements);
 
         if((SRB_FLAGS(Srb) & SRB_FLAGS_DATA_OUT) == SRB_FLAGS_DATA_OUT) {
             for (i = 0; i < sgMaxElements; i++, sgElement++) {
@@ -1397,13 +1358,14 @@ ENTER_FN_SRB();
             }
         }
     }
+
     srbExt->out = sgElement;
     srbExt->psgl[sgElement].physAddr = StorPortGetPhysicalAddress(DeviceExtension, NULL, &cmd->resp.cmd, &fragLen);
     srbExt->psgl[sgElement].length = sizeof(cmd->resp.cmd);
     sgElement++;
     if (sgList)
     {
-        sgMaxElements = sgList->NumberOfElements;
+        sgMaxElements = min((MAX_PHYS_SEGMENTS + 1), sgList->NumberOfElements);
 
         if((SRB_FLAGS(Srb) & SRB_FLAGS_DATA_OUT) != SRB_FLAGS_DATA_OUT) {
             for (i = 0; i < sgMaxElements; i++, sgElement++) {

vioscsi/vioscsi.h
@@ -45,10 +45,13 @@ typedef struct VirtIOBufferDescriptor VIO_SG, *PVIO_SG;
 #define VIRTIO_SCSI_CDB_SIZE   32
 #define VIRTIO_SCSI_SENSE_SIZE 96
 
+#if (NTDDI_VERSION > NTDDI_WIN7)
+#define MAX_PHYS_SEGMENTS       512
+#else
 #define MAX_PHYS_SEGMENTS       64
-#define MAX_PHYS_INDIRECT_SEGMENTS 256
+#endif
 #define VIOSCSI_POOL_TAG        'SoiV'
-#define VIRTIO_MAX_SG            (3+MAX_PHYS_SEGMENTS)
+#define VIRTIO_MAX_SG            (1+1+MAX_PHYS_SEGMENTS+1) //cmd + resp + (MAX_PHYS_SEGMENTS + extra_page)
 
 #define SECTOR_SIZE             512
 #define IO_PORT_LENGTH          0x40

commit 4e20987cd7c53957d1d710952b3e293a291b8e44

[vioscsi] bugfix for BZ#1998473 windows guest can not boot with indirect_desc=off
regression introduced by commit c62a8a2c7bf73541fcfd6bc43bcf85147abf2620

Signed-off-by: Vadim Rozenfeld <vrozenfe@redhat.com>

vioscsi/vioscsi.c
@@ -500,9 +500,10 @@ ENTER_FN();
     if(adaptExt->dump_mode) {
         ConfigInfo->NumberOfPhysicalBreaks  = SCSI_MINIMUM_PHYSICAL_BREAKS;
     } else {
-        adaptExt->max_physical_breaks = MAX_PHYS_SEGMENTS;
+        adaptExt->max_physical_breaks = PHYS_SEGMENTS;
 #if (NTDDI_VERSION > NTDDI_WIN7)
         if (adaptExt->indirect) {
+            adaptExt->max_physical_breaks = MAX_PHYS_SEGMENTS;
             VioScsiReadRegistry(DeviceExtension);
         }
 #endif
@@ -1351,7 +1352,7 @@ ENTER_FN_SRB();
     sgList = StorPortGetScatterGatherList(DeviceExtension, Srb);
     if (sgList)
     {
-        sgMaxElements = min((MAX_PHYS_SEGMENTS + 1), sgList->NumberOfElements);
+        sgMaxElements = min((adaptExt->max_physical_breaks + 1), sgList->NumberOfElements);
 
         if((SRB_FLAGS(Srb) & SRB_FLAGS_DATA_OUT) == SRB_FLAGS_DATA_OUT) {
             for (i = 0; i < sgMaxElements; i++, sgElement++) {
@@ -1368,7 +1369,7 @@ ENTER_FN_SRB();
     sgElement++;
     if (sgList)
     {
-        sgMaxElements = min((MAX_PHYS_SEGMENTS + 1), sgList->NumberOfElements);
+        sgMaxElements = min((adaptExt->max_physical_breaks + 1), sgList->NumberOfElements);
 
         if((SRB_FLAGS(Srb) & SRB_FLAGS_DATA_OUT) != SRB_FLAGS_DATA_OUT) {
             for (i = 0; i < sgMaxElements; i++, sgElement++) {

vioscsi/vioscsi.h
@@ -49,6 +49,7 @@ typedef struct VirtIOBufferDescriptor VIO_SG, *PVIO_SG;
 #define NTDDI_WINTHRESHOLD                  0x0A000000  /* ABRACADABRA_THRESHOLD */
 #endif
 
+#define PHYS_SEGMENTS           32
 #if (NTDDI_VERSION > NTDDI_WIN7)
 #define MAX_PHYS_SEGMENTS       512
 #else