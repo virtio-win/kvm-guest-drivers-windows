90bc6b5c3ab5f5a0bd862ca3c5dd8fe535eab3fd
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2021-09-01 11:12:25 +0300

    NetKVM: add status check (CA)
    Signed-off-by: Viktor Prutyanov <viktor.prutyanov@phystech.edu>

--- NetKVM/Common/ParaNdis_Protocol.h
+++ NetKVM/Common/ParaNdis_Protocol.h
@@ -2,7 +2,7 @@
 
 #include "ndis56common.h"
 
-void ParaNdis_ProtocolInitialize(NDIS_HANDLE DriverHandle);
+NDIS_STATUS ParaNdis_ProtocolInitialize(NDIS_HANDLE DriverHandle);
 void ParaNdis_ProtocolRegisterAdapter(PARANDIS_ADAPTER *pContext);
 void ParaNdis_ProtocolUnregisterAdapter(PARANDIS_ADAPTER *pContext = (PARANDIS_ADAPTER *)(ULONG_PTR)1, bool UnregisterOnLast = true);
 bool ParaNdis_ProtocolSend(PARANDIS_ADAPTER *pContext, PNET_BUFFER_LIST pNBL);
