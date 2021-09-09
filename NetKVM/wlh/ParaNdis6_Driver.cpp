7ddd2c1af3d93d7f3dbe7d4173ac1eb7c61694f4
Yuri Benditovich <yuri.benditovich@daynix.com>
2021-09-09 17:09:52 +0300

    netkvm: build win10 driver with support of NDIS 6.85
    https://bugzilla.redhat.com/show_bug.cgi?id=2002603
    Required for latest WHQL tests.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/wlh/ParaNdis6_Driver.cpp
+++ NetKVM/wlh/ParaNdis6_Driver.cpp
@@ -1078,11 +1078,15 @@ static void SetRuntimeNdisVersion()
         major = NDIS_MINIPORT_MAJOR_VERSION;
         minor = NDIS_MINIPORT_MINOR_VERSION;
     }
-    else if (minor > NDIS_MINIPORT_MINOR_VERSION)
+    else if (minor >= NDIS_MINIPORT_MINOR_VERSION)
     {
         minor = NDIS_MINIPORT_MINOR_VERSION;
     }
-    else if (minor < NDIS_MINIPORT_MINOR_VERSION && minor >= 80)
+    else if (minor >= 83)
+    {
+        minor = 83;
+    }
+    else if (minor >= 80)
     {
         minor = 80;
     }
