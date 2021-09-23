b670d6b95680f7248cb8a1a06fa7624912e0576e
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-09-23 12:40:02 -0700

    Build DLV for vioinput\hidpassthrough
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- buildAll.bat
+++ buildAll.bat
@@ -44,6 +44,8 @@ call tools\build.bat viofs\pci\viofs.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
 call tools\build.bat vioinput\sys\vioinput.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
+call tools\build.bat vioinput\hidpassthrough\hidpassthrough.vcxproj "Win10_SDV" %*
+if errorlevel 1 goto :fail
 
 :nosdv2022
 
--- vioinput/buildAll.bat
+++ vioinput/buildAll.bat
@@ -2,3 +2,5 @@
 call ..\tools\build.bat vioinput.sln "Win7 Win8 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat sys\vioinput.vcxproj "Win10_SDV" %*
+if errorlevel 1 goto :eof
+call ..\tools\build.bat hidpassthrough\hidpassthrough.vcxproj "Win10_SDV" %*
