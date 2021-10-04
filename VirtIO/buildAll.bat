c31e40f0759568c0a3c87f470729db7ad22fca96
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-10-04 09:45:09 -0700

    Disable legacy configurations in build scripts
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- VirtIO/buildAll.bat
+++ VirtIO/buildAll.bat
@@ -1,4 +1,5 @@
 @echo off
-call ..\tools\build.bat VirtioLib.sln "Wxp Wnet Wlh Win7" %*
+::call ..\tools\build.bat VirtioLib.sln "Wxp Wnet Wlh" %*
+call ..\tools\build.bat VirtioLib.sln "Win7" %*
 if errorlevel 1 goto :eof
 call build_NoLegacy.bat
