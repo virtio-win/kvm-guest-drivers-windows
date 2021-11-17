edeff0bfd81cc69fed371de834621b73c66d1f5f
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-17 14:28:42 -0800

    viostor: Remove Windows 7 target
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- viostor/buildAll.bat
+++ viostor/buildAll.bat
@@ -1,4 +1,4 @@
 @echo off
-call ..\tools\build.bat viostor.sln "Wnet Wlh Win7" %*
+call ..\tools\build.bat viostor.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
-call build_NoLegacy.bat
+call ..\tools\build.bat viostor.vcxproj "Win8_SDV Win10_SDV" %*
