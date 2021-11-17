5a358b8a6895ee0f068ea210bd29060e6d4af295
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-17 13:01:00 -0800

    vioscsi: Remove Windows 7 target
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- vioscsi/buildAll.bat
+++ vioscsi/buildAll.bat
@@ -1,4 +1,4 @@
 @echo off
-call ..\tools\build.bat vioscsi.sln "Wlh Win7" %*
+call ..\tools\build.bat vioscsi.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
-call build_NoLegacy.bat
+call ..\tools\build.bat vioscsi.vcxproj "Win8_SDV Win10_SDV" %*
