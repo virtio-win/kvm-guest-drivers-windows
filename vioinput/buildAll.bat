fff19f5933f56f8949fd92ecacd5ef3b4f8bc72b
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-17 11:32:32 -0800

    vioinput: Remove Windows 7 target
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- vioinput/buildAll.bat
+++ vioinput/buildAll.bat
@@ -1,5 +1,5 @@
 @echo off
-call ..\tools\build.bat vioinput.sln "Win7 Win8 Win10" %*
+call ..\tools\build.bat vioinput.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat sys\vioinput.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :eof
