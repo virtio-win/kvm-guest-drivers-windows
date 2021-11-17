dd9c956d4e2d1cea817671adb16deb1521c1a48c
Kostiantyn Kostiuk <konstantin@daynix.com>
2021-11-17 10:20:15 -0800

    NetKVM: Remove Windows 7 target
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- NetKVM/buildAll.bat
+++ NetKVM/buildAll.bat
@@ -1,6 +1,4 @@
 @echo off
-call ..\tools\build.bat netkvm-vs2015.sln "Win7" %*
+call ..\tools\build.bat netkvm-vs2015.sln "Win8 Win8.1 Win10" %*
 if errorlevel 1 goto :eof
-call build_NoLegacy.bat
-if errorlevel 1 goto :eof
-::call ..\tools\build.bat NDIS5\NetKVM-NDIS5.sln "WXp Wnet Wlh" %*
+call ..\tools\build.bat NetKVM-VS2015.vcxproj "Win10_SDV" %*
