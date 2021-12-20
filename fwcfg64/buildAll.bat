f1ba15c6cc09a191807342e250a00fb0f3f40564
Viktor Prutyanov <viktor.prutyanov@phystech.edu>
2021-12-19 16:09:54 -0800

    FWCfg64: add 32-bit Windows support
    Make FWCfg64 driver produce VMCoreInfo record with Complete Memory Dump
    header for 32-bit Windows guest.
    
    Signed-off-by: Viktor Prutyanov <viktor.prutyanov@phystech.edu>

--- fwcfg64/buildAll.bat
+++ fwcfg64/buildAll.bat
@@ -1,4 +1,4 @@
 @echo off
-call ..\tools\build.bat fwcfg.sln "Win8 Win8.1 Win10" x64 %*
+call ..\tools\build.bat fwcfg.sln "Win8 Win8.1 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat fwcfg.vcxproj "Win10_SDV" %*
