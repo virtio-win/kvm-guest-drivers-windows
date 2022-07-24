2b8c9370edbfa5a5d6b4c132d5d10b79174447f9
Martin Drab <martin.drab@email.cz>
2022-07-24 17:50:21 +0200

    WSK: Address translation routines
    Implements WskGetAddressInfo, WskFreeAddressInfo and WskGetNameInfo which can be used to translate AF_VSOCK addresses between their string and binary forms. Also, some necessary logic for IRP processing is being added as well. This is necessary because the WSK interface uses IRPs to signal the client driver that certain WSK operation was completed. Apart from that (and from providing operation results in their `IoStatus` field), IRPs are not used by the interface (they carry neither operation information, nor data associated with it).
    
    It is worth to note that the whole concept of address translation is not that useful for AF_VSOCK since there are no such things as DNS servers and domain names.
    
    ref: [WskGetAddressInfo](https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wsk/nc-wsk-pfn_wsk_get_address_info)
    ref: [WskFreeAddressInfo](https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wsk/nc-wsk-pfn_wsk_free_address_info)
    ref: [WskGetNameInfo](https://learn.microsoft.com/en-us/windows-hardware/drivers/ddi/wsk/nc-wsk-pfn_wsk_get_name_info)
    Signed-off-by: Martin Drab <martin.drab@virtuozzo.com>

---
+++ viosock/wsk/wsk-utils.h
@@ -0,0 +1,78 @@
+/*
+ * Provider NPI functions
+ *
+ * Copyright (c) 2021 Virtuozzo International GmbH
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met :
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and / or other materials provided with the distribution.
+ * 3. Neither the names of the copyright holders nor the names of their contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+#ifndef _WSK_UTILS_H
+#define _WSK_UTILS_H
+
+#ifdef _MSC_VER
+#pragma once
+#endif //_MSC_VER
+
+
+#include "..\inc\vio_wsk.h"
+#include "viowsk.h"
+
+
+_Must_inspect_result_
+NTSTATUS
+VioWskIrpAcquire(
+    _In_opt_ PVIOWSK_SOCKET Socket,
+    _Inout_                 PIRP Irp
+);
+
+void
+VioWskIrpRelease(
+    _In_opt_ PVIOWSK_SOCKET Socket,
+    _In_                    PIRP Irp
+);
+
+NTSTATUS
+VioWskIrpComplete(
+    _Inout_opt_ PVIOWSK_SOCKET Socket,
+    _In_ PIRP                  Irp,
+    _In_ NTSTATUS              Status,
+    _In_ ULONG_PTR             Information
+);
+
+_Must_inspect_result_
+NTSTATUS
+VioWskAddressPartToString(
+    _In_ ULONG            Value,
+    _Out_ PUNICODE_STRING String
+);
+
+_Must_inspect_result_
+NTSTATUS
+VioWskStringToAddressPart(
+    _In_ PUNICODE_STRING String,
+    _Out_ PULONG         Value
+);
+
+
+#endif
