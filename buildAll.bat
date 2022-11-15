857fa22bc40f420a9313bfe5c49383c5cfa34b16
Kostiantyn Kostiuk <konstantin@daynix.com>
2022-11-15 02:20:11 -0800

    vioinput: Fix catalog file generation
    - resolves:
        BZ#2012756 - [virtio-win][vioinput]Signtool verify viohidkmdf.sys
                     file failed with virtio-win-prewhql-211 on win10+ guests
        BZ#2141603 - HCK-CI failed to install virtio-input driver
    
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- buildAll.bat
+++ buildAll.bat
@@ -30,10 +30,11 @@ call tools\build.bat viosock\sys\viosock.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
 call tools\build.bat viofs\pci\viofs.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
-call tools\build.bat vioinput\sys\vioinput.vcxproj "Win10_SDV" %*
-if errorlevel 1 goto :fail
 call tools\build.bat vioinput\hidpassthrough\hidpassthrough.vcxproj "Win10_SDV" %*
 if errorlevel 1 goto :fail
+call tools\build.bat vioinput\sys\vioinput.vcxproj "Win10_SDV" %*
+if errorlevel 1 goto :fail
+
 
 :nosdv2022
 
