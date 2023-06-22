72f0942d0471411cf671f01fd68a5ac4c327c5ea
Yuri Benditovich <yuri.benditovich@daynix.com>
2023-06-23 00:21:45 +0300

    netkvm-prot: append EOL to log lines
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/ProtocolService/Log.h
+++ NetKVM/ProtocolService/Log.h
@@ -1,5 +1,5 @@
 #pragma once
 
 #define ELEMENTS_IN(a) sizeof(a)/sizeof(a[0])
-#define Log(fmt, ...) { CStringA _s_; _s_.Format(fmt, __VA_ARGS__); OutputDebugStringA(_s_); }
+#define Log(fmt, ...) { CStringA _s_; _s_.Format(fmt "\n", __VA_ARGS__); OutputDebugStringA(_s_); }
 
