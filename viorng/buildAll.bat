214242e7775bbc04238baad99ff94e3a91413d52
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-08-16 11:31:17 -0700

    Add ARM64 build in drivers build.bat
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- viorng/buildAll.bat
+++ viorng/buildAll.bat
@@ -1,4 +1,6 @@
 @echo off
+if "%VIRTIO_WIN_NO_ARM%"=="" call ..\tools\build.bat viorng.sln Win10 ARM64
+if errorlevel 1 goto :eof
 call ..\tools\build.bat viorng.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat viorng\viorng.vcxproj "Win10_SDV" %*
