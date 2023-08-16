58aceb74f6a94c2a762db9113b06c4257b2ed3b7
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-08-16 11:31:17 -0700

    Add ARM64 build in drivers build.bat
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- vioscsi/buildAll.bat
+++ vioscsi/buildAll.bat
@@ -1,4 +1,6 @@
 @echo off
+if "%VIRTIO_WIN_NO_ARM%"=="" call ..\tools\build.bat vioscsi.sln Win10 ARM64
+if errorlevel 1 goto :eof
 call ..\tools\build.bat vioscsi.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat vioscsi.vcxproj "Win8_SDV Win10_SDV" %*
--- viosock/buildAll.bat
+++ viosock/buildAll.bat
@@ -1,4 +1,6 @@
 @echo off
+if "%VIRTIO_WIN_NO_ARM%"=="" call ..\tools\build.bat viosock.sln Win10 ARM64
+if errorlevel 1 goto :eof
 call ..\tools\build.bat viosock.sln "Win8 Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat sys\viosock.vcxproj "Win10_SDV" %*
