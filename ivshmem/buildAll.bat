c165bd54987eb352084f7c634bd8ac377206b9d7
Kostiantyn Kostiuk <konstantin@daynix.com>
2023-10-26 07:49:27 -0700

    Remove Win8.1 targets and build scripts
    Signed-off-by: Kostiantyn Kostiuk <konstantin@daynix.com>

--- ivshmem/buildAll.bat
+++ ivshmem/buildAll.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%VIRTIO_WIN_NO_ARM%"=="" call ..\tools\build.bat ivshmem.sln Win10 ARM64
 if errorlevel 1 goto :eof
-call ..\tools\build.bat ivshmem.sln "Win8.1 Win10" %*
+call ..\tools\build.bat ivshmem.sln "Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat ivshmem.vcxproj "Win10_SDV" %*
--- viocrypt/buildAll.bat
+++ viocrypt/buildAll.bat
@@ -1,6 +1,6 @@
 @echo off
 if "%VIRTIO_WIN_NO_ARM%"=="" call ..\tools\build.bat viocrypt.sln Win10 ARM64
 if errorlevel 1 goto :eof
-call ..\tools\build.bat viocrypt.sln "Win8.1 Win10" %*
+call ..\tools\build.bat viocrypt.sln "Win10" %*
 if errorlevel 1 goto :eof
 call ..\tools\build.bat sys\viocrypt.vcxproj "Win10_SDV" %*
