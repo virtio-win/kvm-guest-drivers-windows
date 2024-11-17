0bb71c89979136c7571671ff74f869719a55f85a
Kostiantyn Kostiuk <50669799+kostyanf14@users.noreply.github.com>
2024-11-17 15:35:27 +0200

    Update Driver.c
    Signed-off-by: Kostiantyn Kostiuk <50669799+kostyanf14@users.noreply.github.com>

--- viomem/sys/Driver.c
+++ viomem/sys/Driver.c
@@ -3,6 +3,7 @@
  *
  * Copyright (c) 2009-2022  Red Hat, Inc.
  *
+ * 
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
