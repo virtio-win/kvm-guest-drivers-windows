13de4f055b8dd6dd745b2324a725139214be2850
Vitalii Chulak <vitalii@daynix.com>
2024-12-01 14:39:17 +0200

    RHEL-69069: Apply clang-format CI for Balloon driver
    Signed-off-by: Vitalii Chulak <vitalii@daynix.com>

--- Balloon/sys/memstat.c
+++ Balloon/sys/memstat.c
@@ -41,19 +41,15 @@
 #include "ntddkex.h"
 
 #ifdef ALLOC_PRAGMA
-#pragma alloc_text (PAGE, StatInitializeWorkItem)
+#pragma alloc_text(PAGE, StatInitializeWorkItem)
 #endif
 
-static __inline
-UINT64
-U32_2_S64(ULONG x)
+static __inline UINT64 U32_2_S64(ULONG x)
 {
     return ((UINT64)x);
 }
 
-static __inline
-VOID
-UpdateStat(PBALLOON_STAT stat, USHORT tag, UINT64 val)
+static __inline VOID UpdateStat(PBALLOON_STAT stat, USHORT tag, UINT64 val)
 {
     stat->tag = tag;
     stat->val = val;
@@ -64,18 +60,18 @@ UpdateStat(PBALLOON_STAT stat, USHORT tag, UINT64 val)
  * In case of multiple calls with the same value
  * there will be no false overflow detection
  */
-static __inline
-UINT64
-UpdateOverflowFreeCounter(PULARGE_INTEGER ofc, ULONG value)
+static __inline UINT64 UpdateOverflowFreeCounter(PULARGE_INTEGER ofc, ULONG value)
 {
-    if (value < ofc->LowPart) {
+    if (value < ofc->LowPart)
+    {
         ofc->HighPart++;
     }
     ofc->LowPart = value;
     return ofc->QuadPart;
 }
 
-typedef enum _ULONG_COUNTER {
+typedef enum _ULONG_COUNTER
+{
     _PageReadCount = 0,
     _DirtyPagesWriteCount,
     _MappedPagesWriteCount,
@@ -113,65 +109,84 @@ NTSTATUS GatherKernelStats(BALLOON_STAT stats[VIRTIO_BALLOON_S_NR])
     ULONG idx = 0;
     UINT64 SoftFaults, AvailBytes;
 
-    RtlZeroMemory(&basicInfo,sizeof(basicInfo));
-    RtlZeroMemory(&perfInfo,sizeof(perfInfo));
+    RtlZeroMemory(&basicInfo, sizeof(basicInfo));
+    RtlZeroMemory(&perfInfo, sizeof(perfInfo));
 
     ntStatus = ZwQuerySystemInformation(SystemBasicInformation, &basicInfo, sizeof(basicInfo), &outLen);
-    if(!NT_SUCCESS(ntStatus))
+    if (!NT_SUCCESS(ntStatus))
     {
-        TraceEvents(TRACE_LEVEL_ERROR, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemBasicInformation) failed 0x%08x (outLen=0x%x)\n", ntStatus, outLen);
+        TraceEvents(TRACE_LEVEL_ERROR,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemBasicInformation) failed 0x%08x (outLen=0x%x)\n",
+                    ntStatus,
+                    outLen);
         return ntStatus;
     }
 
-    if ((!bBasicInfoWarning)&&(outLen != sizeof(basicInfo))) {
+    if ((!bBasicInfoWarning) && (outLen != sizeof(basicInfo)))
+    {
         bBasicInfoWarning = TRUE;
-        TraceEvents(TRACE_LEVEL_WARNING, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemBasicInformation) expected outLen=0x%08x returned with 0x%0x",
-            sizeof(basicInfo), outLen);
+        TraceEvents(TRACE_LEVEL_WARNING,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemBasicInformation) expected outLen=0x%08x returned with 0x%0x",
+                    sizeof(basicInfo),
+                    outLen);
     }
 
     ntStatus = ZwQuerySystemInformation(SystemPerformanceInformation, &perfInfo, sizeof(perfInfo), &outLen);
-    if(!NT_SUCCESS(ntStatus))
+    if (!NT_SUCCESS(ntStatus))
     {
-        TraceEvents(TRACE_LEVEL_ERROR, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemPerformanceInformation) failed 0x%08x (outLen=0x%x)\n", ntStatus, outLen);
+        TraceEvents(TRACE_LEVEL_ERROR,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemPerformanceInformation) failed 0x%08x (outLen=0x%x)\n",
+                    ntStatus,
+                    outLen);
         return ntStatus;
     }
 
-    if ((!bPerfInfoWarning)&&(outLen != sizeof(perfInfo))) {
+    if ((!bPerfInfoWarning) && (outLen != sizeof(perfInfo)))
+    {
         bPerfInfoWarning = TRUE;
-        TraceEvents(TRACE_LEVEL_WARNING, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemPerformanceInformation) expected outLen=0x%08x returned with 0x%0x",
-            sizeof(perfInfo), outLen);
+        TraceEvents(TRACE_LEVEL_WARNING,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemPerformanceInformation) expected outLen=0x%08x returned with 0x%0x",
+                    sizeof(perfInfo),
+                    outLen);
     }
 
     ntStatus = ZwQuerySystemInformation(SystemFileCacheInformationEx, &cacheInfo, sizeof(cacheInfo), &outLen);
-    if(!NT_SUCCESS(ntStatus))
+    if (!NT_SUCCESS(ntStatus))
     {
-        TraceEvents(TRACE_LEVEL_ERROR, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemFileCacheInformationEx) failed 0x%08x (outLen=0x%x)\n", ntStatus, outLen);
+        TraceEvents(TRACE_LEVEL_ERROR,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemFileCacheInformationEx) failed 0x%08x (outLen=0x%x)\n",
+                    ntStatus,
+                    outLen);
         return ntStatus;
     }
 
-    if ((!bCacheInfoWarning)&&(outLen != sizeof(cacheInfo))) {
+    if ((!bCacheInfoWarning) && (outLen != sizeof(cacheInfo)))
+    {
         bCacheInfoWarning = TRUE;
-        TraceEvents(TRACE_LEVEL_WARNING, DBG_HW_ACCESS,
-            "GatherKernelStats (SystemFileCacheInformationEx) expected outLen=0x%08x returned with 0x%0x",
-            sizeof(cacheInfo), outLen);
+        TraceEvents(TRACE_LEVEL_WARNING,
+                    DBG_HW_ACCESS,
+                    "GatherKernelStats (SystemFileCacheInformationEx) expected outLen=0x%08x returned with 0x%0x",
+                    sizeof(cacheInfo),
+                    outLen);
     }
 
-    #define UpdateNoOverflow(x) UpdateOverflowFreeCounter(&Counters[_##x],perfInfo.##x)
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_SWAP_IN,  UpdateNoOverflow(PageReadCount) << PAGE_SHIFT);
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_SWAP_OUT,
-        (UpdateNoOverflow(DirtyPagesWriteCount) + UpdateNoOverflow(MappedPagesWriteCount)) << PAGE_SHIFT);
+#define UpdateNoOverflow(x) UpdateOverflowFreeCounter(&Counters[_##x], perfInfo.##x)
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_SWAP_IN, UpdateNoOverflow(PageReadCount) << PAGE_SHIFT);
+    UpdateStat(&stats[idx++],
+               VIRTIO_BALLOON_S_SWAP_OUT,
+               (UpdateNoOverflow(DirtyPagesWriteCount) + UpdateNoOverflow(MappedPagesWriteCount)) << PAGE_SHIFT);
     SoftFaults = UpdateNoOverflow(CopyOnWriteCount) + UpdateNoOverflow(TransitionCount) +
                  UpdateNoOverflow(CacheTransitionCount) + UpdateNoOverflow(DemandZeroCount);
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MAJFLT,   UpdateNoOverflow(PageReadCount));
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MINFLT,   SoftFaults);
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MAJFLT, UpdateNoOverflow(PageReadCount));
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MINFLT, SoftFaults);
     AvailBytes = U32_2_S64(perfInfo.AvailablePages) << PAGE_SHIFT;
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MEMFREE,  AvailBytes);
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MEMTOT,   U32_2_S64(basicInfo.NumberOfPhysicalPages) << PAGE_SHIFT);
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MEMFREE, AvailBytes);
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_MEMTOT, U32_2_S64(basicInfo.NumberOfPhysicalPages) << PAGE_SHIFT);
 
     if (cacheInfo.Flags & QUOTA_LIMITS_HARDWS_MIN_ENABLE &&
         cacheInfo.CurrentSize > (cacheInfo.MinimumWorkingSet << PAGE_SHIFT))
@@ -179,21 +194,19 @@ NTSTATUS GatherKernelStats(BALLOON_STAT stats[VIRTIO_BALLOON_S_NR])
         cacheInfo.CurrentSize -= cacheInfo.MinimumWorkingSet << PAGE_SHIFT;
     }
 
-    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_AVAIL, AvailBytes + (UINT64)cacheInfo.CurrentSize/2);
+    UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_AVAIL, AvailBytes + (UINT64)cacheInfo.CurrentSize / 2);
     UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_CACHES, (UINT64)cacheInfo.CurrentSize);
     UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_HTLB_PGALLOC, 0);
     UpdateStat(&stats[idx++], VIRTIO_BALLOON_S_HTLB_PGFAIL, 0);
-    #undef UpdateNoOverflow
+#undef UpdateNoOverflow
 
     return ntStatus;
 }
 
-NTSTATUS StatInitializeWorkItem(
-    IN WDFDEVICE  Device
-    )
+NTSTATUS StatInitializeWorkItem(IN WDFDEVICE Device)
 {
-    WDF_OBJECT_ATTRIBUTES   attributes;
-    WDF_WORKITEM_CONFIG     workitemConfig;
+    WDF_OBJECT_ATTRIBUTES attributes;
+    WDF_WORKITEM_CONFIG workitemConfig;
     PDEVICE_CONTEXT devCtx = GetDeviceContext(Device);
 
     PAGED_CODE();
@@ -210,19 +223,15 @@ NTSTATUS StatInitializeWorkItem(
  * Still use devCtx->MemStats cause it points to non-paged pool,
  * for virtio/host that access stats via physical memory.
  */
-VOID
-StatWorkItemWorker(
-    IN WDFWORKITEM  WorkItem
-    )
+VOID StatWorkItemWorker(IN WDFWORKITEM WorkItem)
 {
-    WDFDEVICE       Device = WdfWorkItemGetParentObject(WorkItem);
+    WDFDEVICE Device = WdfWorkItemGetParentObject(WorkItem);
     PDEVICE_CONTEXT devCtx = GetDeviceContext(Device);
-    NTSTATUS        status = STATUS_SUCCESS;
+    NTSTATUS status = STATUS_SUCCESS;
 
     do
     {
-        TraceEvents(TRACE_LEVEL_INFORMATION, DBG_HW_ACCESS,
-            "StatWorkItemWorker Called! \n");
+        TraceEvents(TRACE_LEVEL_INFORMATION, DBG_HW_ACCESS, "StatWorkItemWorker Called! \n");
         status = GatherKernelStats(devCtx->MemStats);
         if (NT_SUCCESS(status))
         {
@@ -235,11 +244,13 @@ StatWorkItemWorker(
                     devCtx->MemStats[i].tag, devCtx->MemStats[i].val);
             }
 #endif
-        } else {
-            RtlFillMemory (devCtx->MemStats, sizeof (BALLOON_STAT) * VIRTIO_BALLOON_S_NR, -1);
+        }
+        else
+        {
+            RtlFillMemory(devCtx->MemStats, sizeof(BALLOON_STAT) * VIRTIO_BALLOON_S_NR, -1);
         }
         BalloonMemStats(Device);
-    } while(InterlockedDecrement(&devCtx->WorkCount));
+    } while (InterlockedDecrement(&devCtx->WorkCount));
     return;
 }
 
