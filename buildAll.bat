74f5391c0eddc34d9cc241dab2f0978afe9a175b
benyamin-codez <115509179+benyamin-codez@users.noreply.github.com>
2024-12-15 04:16:49 +1100

    [build] Fix and extend build scripting
    Addendum to da96c89.
    
    1. Further changes to buildAll.bat
       a) Added @ prefix to FOR do commands
       b) Extended logging around creation of COMPAT (RS1/1607) DVL files
       c) Added SDV and Legacy DVL (DVL files created with legacy EWDKs)
          reporting. The latter queries the <PROJECT>.legacy_dvl_result.txt
          log file created by build\makeLegacyDVLs.bat and reports accordingly.
          Please see PR #1210 for more information.
       d) Consolidates exiting function (whether success or fail) to :leave
       e) Creates a scheduled task to call build\clean_build_log.bat before leaving.
    
    2. Created build\clean_build_log.bat to remove colour pallette artefacts
       from build_log.txt created during HCK/CI using Jenkins config.
       This script also deletes the scheduled task created in 1(d) above.
    
    Signed-off-by: benyamin-codez <115509179+benyamin-codez@users.noreply.github.com>

--- buildAll.bat
+++ buildAll.bat
@@ -39,13 +39,12 @@ if errorlevel 1 goto :fail
 call build\build.bat viomem\sys\viomem.vcxproj "Win10_SDV Win11_SDV" %*
 if errorlevel 1 goto :fail
 
-
 :nosdv2022
 
 call :prepare_palette
 
 path %path%;C:\Program Files (x86)\Windows Kits\10\bin\x86\
-for %%D in (pciserial fwcfg Q35 packaging) do (
+for %%D in (pciserial fwcfg Q35 packaging) do @(
   echo.
   call :_color_echo %_c_Cyn% "Building : %%D"
   pushd %%D
@@ -56,31 +55,110 @@ for %%D in (pciserial fwcfg Q35 packaging) do (
 )
 
 echo.
-call :_color_echo %_c_Cyn% "Processing DVL xml files..."
-for /R %%f in (*.dvl.xml) do call :process_xml %%f
-call :_color_echo %_c_Cyn% "All processing completed."
+call :_color_echo %_c_Cyn% "Processing DVL files to create COMPAT (Redstone 1 / 1607) version..."
+for /R %%f in (*.dvl.xml) do @call :process_xml %%f
+if "%found_dvl_xml%"=="" (
+  call :_color_echo %_c_Yel% "WARNING : No DVL files were found."
+) else (
+  call :_color_echo %_c_Cyn% "Processing of DVL files is complete."
+)
+
+rem Report SDV and DVL creation status...
+for %%D in (NetKVM vioscsi viostor) do @call :check_sdv_dvl %%D
+
+:bld_success
 echo.
 call :_color_echo %_c_Grn% "BUILD COMPLETED SUCCESSFULLY."
-exit /B 0
+call :leave 0
+goto :eof
 
 :fail
 call :_color_echo %_c_Red% "BUILD FAILED."
 set BUILD_FAILED=
-exit /B 1
+call :leave 1
+goto :eof
 
 :process_xml
+set found_dvl_xml=Yes
 if not exist "%~dpn1-compat%~x1" (
   call :fudge_xml %1
 ) else (
-  rem NOTE: Here we retain the COMPAT version created by C:\DVL1903\dvl.exe
+  rem NOTE: Here we retain the Windows 10 version 1607 (build 14393) COMPAT DVL.
   call :_color_echo %_c_Grn% "The file already exists : %~dpn1-compat%~x1"
 )
 goto :eof
 
 :fudge_xml
 call :_color_echo %_c_Yel% "Auto-magically creating : %~dpn1-compat%~x1"
-rem NOTE: Here we also have to remove the ..General.Checksum because we modded the file and changed it.
+rem NOTE: Here we create a Windows 10 version 1607 (build 14393) COMPAT DVL.
 findstr /v /c:"General.Checksum" "%~1" | findstr /v /c:".Semmle." > "%~dpn1-compat%~x1"
+goto :eof
+
+:check_sdv_dvl
+echo.
+set drvr=%~1
+set results_file="%~dpn1\%~1.legacy_dvl_result.txt"
+set sdv_dir="%~dpn1\sdv"
+call :_color_echo %_c_Cyn% "%drvr% : Checking if the SDV directory exists..."
+if exist %sdv_dir% (
+  call :_color_echo %_c_Grn% "%drvr% : SUCCESS : The SDV directory exists."
+) else (
+  call :_color_echo %_c_Yel% "%drvr% : WARNING : The SDV directory does NOT exist."
+)
+echo.
+call :_color_echo %_c_Cyn% "%drvr% : Checking if the DVL build logged results..."
+if exist "%results_file%" (
+  call :_color_echo %_c_Cyn% "%drvr% : Reporting Driver Verification Log build results..."
+  if exist "%~dpn1\%~1.DVL-win10-latest.XML" (
+    call :_color_echo %_c_Grn% "%drvr% : SUCCESS : Latest Windows 10 DVL was created with the Cobalt EWDK."
+  ) else (
+    call :_color_echo %_c_Yel% "%drvr% : WARNING : Latest Windows 10 DVL was NOT created."
+  )
+  if exist "%~dpn1\%~1.DVL-win11-latest.XML" (
+    call :_color_echo %_c_Grn% "%drvr% : SUCCESS : Latest Windows 11 DVL was created."
+  ) else (
+    call :_color_echo %_c_Yel% "%drvr% : WARNING : Latest Windows 11 DVL was NOT created."
+  )
+  for /f "tokens=1,2 usebackq delims=," %%i in (`type %%results_file%%`) do @call :check_dvl_result %%i %%j
+  del /f "%results_file%"
+) else (
+  call :_color_echo %_c_Yel% "%drvr% : WARNING : No DVL build results were logged."
+)
+goto :eof
+
+:check_dvl_result
+set dvl_ver=%~1
+set dvl_outcome=%~2
+if "%dvl_ver%"=="%dvl_outcome%" (
+  if "%dvl_ver%"=="1607" (
+    call :_color_echo %_c_Grn% "%drvr% : SUCCESS : Windows 10 version %dvl_ver% DVL was created with the Redstone 1 EWDK."
+  ) else (
+    if "%dvl_ver%"=="1903" (
+      call :_color_echo %_c_Grn% "%drvr% : SUCCESS : Windows 10 version %dvl_ver% DVL was created with the Titanium EWDK."
+    ) else (
+      call :_color_echo %_c_Grn% "%drvr% : SUCCESS : Windows 10 version %dvl_ver% DVL was created with an EWDK of the same version."
+    )
+  )
+) else (
+  if "%dvl_ver%"=="1607" (
+    if "%dvl_outcome%"=="1903" (
+      call :_color_echo %_c_Yel% "%drvr% : WARNING : Windows 10 version %dvl_ver% DVL was derived from an alternate DVL created with the Titanium EWDK."
+    ) else (
+      if "%dvl_outcome%"=="latest" (
+        call :_color_echo %_c_Yel% "%drvr% : WARNING : Windows 10 version %dvl_ver% DVL was derived from an alternate DVL created with the Cobalt EWDK."
+      )
+    )
+  )
+  if "%dvl_outcome%"=="fail" (
+    if "%dvl_ver%"=="1607" (
+      call :_color_echo %_c_Red% "%drvr% : FAILURE : Windows 10 version %dvl_ver% DVL was NOT created. The %drvr%.DVL-compat.XML file will be missing."
+    ) else (
+      if "%dvl_ver%"=="1903" (
+        call :_color_echo %_c_Red% "%drvr% : FAILURE : Windows 10 version %dvl_ver% DVL was NOT created. The %drvr%.DVL-win10.XML file will be missing."
+      )
+    )
+  )
+)
 goto :eof
 
 :_color_echo
@@ -98,3 +176,11 @@ set "_c_Yel="40;93m""
 set "_c_Cyn="40;96m""
 set "_c_Wht="40;37m""
 goto :eof
+
+:leave
+if exist ".\build_log.txt" (
+  call schtasks /create /tn build_log_cleanup /tr "%comspec% /c %~dp0build\clean_build_log.bat" /sc ONCE /sd 01/01/1910 /st 00:00 1> nul 2>&1
+  call schtasks /run /tn build_log_cleanup 1> nul 2>&1
+)
+exit /B %1
+goto :eof :: never hit
