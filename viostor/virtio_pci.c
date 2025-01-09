19cc1e496831d59b20ad02a22ea83a2634c86bbd
Vitalii Chulak <vitalii@daynix.com>
2025-01-09 19:02:13 +0200

    RHEL-69080: broken style fix for viostor driver
    Signed-off-by: Vitalii Chulak <vitalii@daynix.com>

--- viostor/virtio_pci.c
+++ viostor/virtio_pci.c
@@ -261,6 +261,7 @@ static void vdev_sleep(void *context, unsigned int msecs)
     StorPortStallExecution(1000 * msecs);
 }
 
+// clang-format off
 VirtIOSystemOps VioStorSystemOps = {
     .vdev_read_byte = ReadVirtIODeviceByte,
     .vdev_read_word = ReadVirtIODeviceWord,
@@ -281,3 +282,4 @@ VirtIOSystemOps VioStorSystemOps = {
     .vdev_get_msix_vector = vdev_get_msix_vector,
     .vdev_sleep = vdev_sleep,
 };
+// clang-format on
