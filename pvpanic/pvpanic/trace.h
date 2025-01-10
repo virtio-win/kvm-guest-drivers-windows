aa71405c82d54820e33e6f2461efa8f1a36d5b05
Vitalii Chulak <vitalii@daynix.com>
2025-01-10 18:01:42 +0200

    Exclude trace.h from clang-format for pvpanic
    Signed-off-by: Vitalii Chulak <vitalii@daynix.com>

--- pvpanic/pvpanic/trace.h
+++ pvpanic/pvpanic/trace.h
@@ -33,18 +33,25 @@
 // Tracing GUID - 5eeabb8c-be9a-40d0-99fd-86f2a0b21378
 //
 
-#define WPP_CONTROL_GUIDS                                                                                              \
-    WPP_DEFINE_CONTROL_GUID(PVPanicTraceGuid,                                                                          \
-                            (5eeabb8c, be9a, 40d0, 99fd, 86f2a0b21378),                                                \
-                            WPP_DEFINE_BIT(DBG_ALL) WPP_DEFINE_BIT(DBG_INIT) WPP_DEFINE_BIT(DBG_POWER))
+#define WPP_CONTROL_GUIDS \
+    WPP_DEFINE_CONTROL_GUID( \
+        PVPanicTraceGuid, (5eeabb8c,be9a,40d0,99fd,86f2a0b21378), \
+        WPP_DEFINE_BIT(DBG_ALL)         \
+        WPP_DEFINE_BIT(DBG_INIT)        \
+        WPP_DEFINE_BIT(DBG_POWER)       \
+    )
 
-#define WPP_FLAG_LEVEL_LOGGER(flag, level)  WPP_LEVEL_LOGGER(flag)
+#define WPP_FLAG_LEVEL_LOGGER(flag, level) \
+    WPP_LEVEL_LOGGER(flag)
 
-#define WPP_FLAG_LEVEL_ENABLED(flag, level) (WPP_LEVEL_ENABLED(flag) && WPP_CONTROL(WPP_BIT_##flag).Level >= level)
+#define WPP_FLAG_LEVEL_ENABLED(flag, level) \
+    (WPP_LEVEL_ENABLED(flag) && WPP_CONTROL(WPP_BIT_ ## flag).Level >= level)
 
-#define WPP_LEVEL_FLAGS_LOGGER(lvl, flags)  WPP_LEVEL_LOGGER(flags)
+#define WPP_LEVEL_FLAGS_LOGGER(lvl,flags) \
+    WPP_LEVEL_LOGGER(flags)
 
-#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_##flags).Level >= lvl)
+#define WPP_LEVEL_FLAGS_ENABLED(lvl, flags) \
+    (WPP_LEVEL_ENABLED(flags) && WPP_CONTROL(WPP_BIT_ ## flags).Level >= lvl)
 
 //
 // This comment block is scanned by the trace preprocessor to define our
