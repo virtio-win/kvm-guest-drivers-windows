5d6070dc2786a5464c2ff94caf7c937cc9278b7b
Vitalii Chulak <vitalii@daynix.com>
2025-01-13 16:07:07 +0200

    RHEL-69071: clang-format for СoInstaller folder in NetKVM
    Signed-off-by: Vitalii Chulak <vitalii@daynix.com>

--- NetKVM/CoInstaller/RegAccess.h
+++ NetKVM/CoInstaller/RegAccess.h
@@ -4,42 +4,32 @@
 
 #pragma once
 
-const DWORD DEFAULT_REG_ENTRY_DATA_LEN  = 0x00000100;
+const DWORD DEFAULT_REG_ENTRY_DATA_LEN = 0x00000100;
 
 class neTKVMRegAccessConstructorFailedException : public neTKVMException
 {
-public:
-    neTKVMRegAccessConstructorFailedException() :
-      neTKVMException(TEXT("Can't construct neTKVMRegAccess object"))
-    { }
+  public:
+    neTKVMRegAccessConstructorFailedException() : neTKVMException(TEXT("Can't construct neTKVMRegAccess object"))
+    {
+    }
 };
 
 class neTKVMRegAccess
 {
-public:
+  public:
     neTKVMRegAccess();
     neTKVMRegAccess(HKEY hNewPrKey, LPCTSTR lpzNewRegPath);
     virtual ~neTKVMRegAccess();
 
-    VOID  SetPrimaryKey(HKEY hNewPrKey);
-    BOOL  SetRegPath(LPCTSTR lpzNewRegPath);
-    HKEY  GetPrimaryKey(VOID);
-    BOOL  GetRegPath(LPTSTR lpsBuffer,
-                     DWORD  dwNumberOfElements);
+    VOID SetPrimaryKey(HKEY hNewPrKey);
+    BOOL SetRegPath(LPCTSTR lpzNewRegPath);
+    HKEY GetPrimaryKey(VOID);
+    BOOL GetRegPath(LPTSTR lpsBuffer, DWORD dwNumberOfElements);
 
-    BOOL  ReadValueName(LPTSTR lpsValueName,
-                        DWORD  dwNumberOfElements,
-                        DWORD  dwIndex=0,
-                        LPCTSTR lpzSubKey=NULL);
-    BOOL  ReadKeyName(LPTSTR  lpsKeyName,
-                      DWORD   dwNumberOfElements,
-                      DWORD   dwIndex,
-                      LPCTSTR lpzSubKey = NULL);
+    BOOL ReadValueName(LPTSTR lpsValueName, DWORD dwNumberOfElements, DWORD dwIndex = 0, LPCTSTR lpzSubKey = NULL);
+    BOOL ReadKeyName(LPTSTR lpsKeyName, DWORD dwNumberOfElements, DWORD dwIndex, LPCTSTR lpzSubKey = NULL);
 
-    BOOL GetValueInfo(LPCTSTR lpzValueName,
-                      DWORD*  lpDataType,
-                      DWORD*  lpDataSize,
-                      LPCTSTR lpzSubKey = NULL);
+    BOOL GetValueInfo(LPCTSTR lpzValueName, DWORD *lpDataType, DWORD *lpDataSize, LPCTSTR lpzSubKey = NULL);
     BOOL GetKeyInfo(LPDWORD lpdwNofSubKeys,
                     LPDWORD lpdwMaxSubKeyLen,
                     LPDWORD lpdwNofValues,
@@ -47,42 +37,21 @@ public:
                     LPDWORD lpdwMaxValueLen,
                     LPCTSTR lpzSubKey = NULL);
 
-    DWORD ReadDWord(LPCTSTR lpzValueName,
-                    DWORD   dwDefault = 0,
-                    LPCTSTR lpzSubKey = NULL);
-    BOOL  ReadDWord(LPCTSTR lpzValueName,
-                    LPDWORD lpdwValue,
-                    LPCTSTR lpzSubKey = NULL);
-    DWORD ReadString(LPCTSTR lpzValueName,
-                     LPTSTR  lpzData,
-                     DWORD   dwNumberOfElements,
-                     LPCTSTR lpzSubKey=NULL);
-    DWORD ReadBinary(LPCTSTR lpzValueName,
-                     LPBYTE  lpzData,
-                     DWORD   dwSize,
-                     LPCTSTR lpzSubKey=NULL);
-    BOOL  WriteValue(LPCTSTR lpzValueName,
-                     DWORD  dwValue,
-                     LPCTSTR lpzSubKey = NULL);
-    BOOL  WriteString(LPCTSTR lpzValueName,
-                      LPCTSTR lpzValue,
-                      LPCTSTR lpzSubKey=NULL);
-    BOOL  WriteBinary(LPCTSTR lpzValueName,
-                      LPCBYTE lpData,
-                      DWORD   dwDataSize,
-                      LPCTSTR lpzSubKey = NULL);
-    BOOL  DeleteKey(LPCTSTR lpzKeyName,
-                    LPCTSTR lpzSubKey = NULL);
-    BOOL  DeleteValue(LPCTSTR lpzValueName,
-                      LPCTSTR lpzSubKey = NULL);
+    DWORD ReadDWord(LPCTSTR lpzValueName, DWORD dwDefault = 0, LPCTSTR lpzSubKey = NULL);
+    BOOL ReadDWord(LPCTSTR lpzValueName, LPDWORD lpdwValue, LPCTSTR lpzSubKey = NULL);
+    DWORD ReadString(LPCTSTR lpzValueName, LPTSTR lpzData, DWORD dwNumberOfElements, LPCTSTR lpzSubKey = NULL);
+    DWORD ReadBinary(LPCTSTR lpzValueName, LPBYTE lpzData, DWORD dwSize, LPCTSTR lpzSubKey = NULL);
+    BOOL WriteValue(LPCTSTR lpzValueName, DWORD dwValue, LPCTSTR lpzSubKey = NULL);
+    BOOL WriteString(LPCTSTR lpzValueName, LPCTSTR lpzValue, LPCTSTR lpzSubKey = NULL);
+    BOOL WriteBinary(LPCTSTR lpzValueName, LPCBYTE lpData, DWORD dwDataSize, LPCTSTR lpzSubKey = NULL);
+    BOOL DeleteKey(LPCTSTR lpzKeyName, LPCTSTR lpzSubKey = NULL);
+    BOOL DeleteValue(LPCTSTR lpzValueName, LPCTSTR lpzSubKey = NULL);
 
-    BOOL  AddKey(LPCTSTR lpzKeyName);
+    BOOL AddKey(LPCTSTR lpzKeyName);
 
-protected:
-    VOID FormatFullRegPath(LPTSTR    lpzFullPathBuff,
-                           DWORD_PTR dwNumberOfElements,
-                           LPCTSTR   lpzSubKey);
+  protected:
+    VOID FormatFullRegPath(LPTSTR lpzFullPathBuff, DWORD_PTR dwNumberOfElements, LPCTSTR lpzSubKey);
 
     LPTSTR m_lpsRegPath;
-    HKEY   m_hkPrimaryHKey;
+    HKEY m_hkPrimaryHKey;
 };
--- NetKVM/CoInstaller/resource.h
+++ NetKVM/CoInstaller/resource.h
@@ -2,57 +2,57 @@
 // Microsoft Visual C++ generated include file.
 // Used by netco.rc
 //
-#define IDS_ROOTGROUPDESC               101
-#define IDS_LOGICEXCEPTION              102
-#define IDS_NODEVICESFOUND              103
-#define IDS_CANNOTOPENKEY               104
-#define IDS_SHOWCMDHELP                 105
-#define IDS_SHOWDEVICESSHORT            106
-#define IDS_SHOWDEVICESLONG             107
-#define IDS_DEVICEIDX                   108
-#define IDS_DEVICEID                    109
-#define IDS_DEVICEDESC                  110
-#define IDS_DEVICEFRNAME                111
-#define IDS_DEVICELOCINFO               112
-#define IDS_DEVICENUM                   113
-#define IDS_DEVICEREGKEY                114
-#define IDS_RESTARTDEVICE               115
-#define IDS_RESTARTDEVICELONG           116
-#define IDS_RESTARTINGDEVICE            117
-#define IDS_DONE                        118
-#define IDS_INDEXOUTOFRANGE             119
-#define IDS_FAIL                        120
-#define IDS_REBOOTREQUIRED              121
-#define IDS_SHOWPARAMSSHORT             122
-#define IDS_SHOWPARAMSLONG              123
-#define IDS_SHOWPARAMINFOSHORT          124
-#define IDS_SHOWPARAMINFOLONG           125
-#define IDS_PARAMENUMVALUE              126
-#define IDS_PARAMNUMMIN                 127
-#define IDS_PARAMNUMMAX                 128
-#define IDS_PARAMNUMSTEP                129
-#define IDS_PARAMTEXTLIMIT              130
-#define IDS_PARAMUPPERCASE              131
-#define IDS_PARAMUNKNOWNPROP            132
-#define IDS_PARAMTYPE                   133
-#define IDS_PARAMENUM                   134
-#define IDS_PARAMINT                    135
-#define IDS_PARAMLONG                   136
-#define IDS_PARAMTEXT                   137
-#define IDS_PARAMUNKNOWN                138
-#define IDS_GETPARAM                    139
-#define IDS_GETPARAMLONG                140
-#define IDS_SETPARAMLONG                141
-#define IDS_SETPARAM                    142
-#define IDS_LOCALONLY                   143
+#define IDS_ROOTGROUPDESC      101
+#define IDS_LOGICEXCEPTION     102
+#define IDS_NODEVICESFOUND     103
+#define IDS_CANNOTOPENKEY      104
+#define IDS_SHOWCMDHELP        105
+#define IDS_SHOWDEVICESSHORT   106
+#define IDS_SHOWDEVICESLONG    107
+#define IDS_DEVICEIDX          108
+#define IDS_DEVICEID           109
+#define IDS_DEVICEDESC         110
+#define IDS_DEVICEFRNAME       111
+#define IDS_DEVICELOCINFO      112
+#define IDS_DEVICENUM          113
+#define IDS_DEVICEREGKEY       114
+#define IDS_RESTARTDEVICE      115
+#define IDS_RESTARTDEVICELONG  116
+#define IDS_RESTARTINGDEVICE   117
+#define IDS_DONE               118
+#define IDS_INDEXOUTOFRANGE    119
+#define IDS_FAIL               120
+#define IDS_REBOOTREQUIRED     121
+#define IDS_SHOWPARAMSSHORT    122
+#define IDS_SHOWPARAMSLONG     123
+#define IDS_SHOWPARAMINFOSHORT 124
+#define IDS_SHOWPARAMINFOLONG  125
+#define IDS_PARAMENUMVALUE     126
+#define IDS_PARAMNUMMIN        127
+#define IDS_PARAMNUMMAX        128
+#define IDS_PARAMNUMSTEP       129
+#define IDS_PARAMTEXTLIMIT     130
+#define IDS_PARAMUPPERCASE     131
+#define IDS_PARAMUNKNOWNPROP   132
+#define IDS_PARAMTYPE          133
+#define IDS_PARAMENUM          134
+#define IDS_PARAMINT           135
+#define IDS_PARAMLONG          136
+#define IDS_PARAMTEXT          137
+#define IDS_PARAMUNKNOWN       138
+#define IDS_GETPARAM           139
+#define IDS_GETPARAMLONG       140
+#define IDS_SETPARAMLONG       141
+#define IDS_SETPARAM           142
+#define IDS_LOCALONLY          143
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        142
-#define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
-#define _APS_NEXT_SYMED_VALUE           101
+#define _APS_NEXT_RESOURCE_VALUE 142
+#define _APS_NEXT_COMMAND_VALUE  40001
+#define _APS_NEXT_CONTROL_VALUE  1001
+#define _APS_NEXT_SYMED_VALUE    101
 #endif
 #endif
