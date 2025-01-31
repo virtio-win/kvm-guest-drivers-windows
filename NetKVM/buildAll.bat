virtio-win/kvm-guest-drivers-windows

commit e9c95e522a6aa9ad20df6a325fb7642602e1d64a

[viostor] Refactored legacy DVL creation
1. Refactors the CustomBuildStep Command to manage DVL file creation.
   We now create DVL files in the Project (driver) folder with version
   numbers in the file name for DVL files created with legacy EWDKs,
   and with the "latest" label for current EWDKs. These files are
   then copied to the relevant .\Install folders and renamed for
   extant WHCP submission operations. Makes use of the new batch file
   build\makeLegacyDVLs.bat script (from PR #1210) and which now creates
   all legacy DVL files. Logging is also enabled to provide feedback
   when read at the tail of root\buildAll.bat. Please see PR #1212 for
   details on those changes.

2. Enables Win10_SDV for local (viostor) buildAll.bat script.

Signed-off-by: benyamin-codez <115509179+benyamin-codez@users.noreply.github.com>

viostor/buildAll.bat
@@ -3,4 +3,4 @@ if "%VIRTIO_WIN_NO_ARM%"=="" call ..\build\build.bat viostor.sln "Win10 Win11" A
 if errorlevel 1 goto :eof
 call ..\build\build.bat viostor.sln "Win10 Win11" %*
 if errorlevel 1 goto :eof
-call ..\build\build.bat viostor.vcxproj "Win11_SDV" %*
+call ..\build\build.bat viostor.vcxproj "Win10_SDV" "Win11_SDV" %*

viostor/viostor.vcxproj
@@ -152,13 +152,39 @@
   <ItemDefinitionGroup>
     <CustomBuildStep>
       <Command>
-        copy /Y $(ProjectDir)viostor.DVL.XML $(ProjectDir)Install\$(TargetOS)\$(TargetArch)
+        echo Copying $(TargetName).DVL.XML to Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL.XML
+        copy /Y $(ProjectDir)$(TargetName).DVL.XML^
+         $(ProjectDir)Install\$(TargetOS)\$(TargetArch)
+        echo Copying $(IntDir)vc.nativecodeanalysis.all.xml to $(ProjectDir)
         copy /Y $(IntDir)vc.nativecodeanalysis.all.xml $(ProjectDir)
-        call "..\build\dvl1903.bat" "$(ProjectDir)\$(IntDir)" "$(TargetName)" "$(Configuration)" "$(Platform)"
-        copy /Y $(ProjectDir)viostor.DVL.XML $(ProjectDir)Install\$(TargetOS)\$(TargetArch)\viostor.DVL-win10.XML
+        if "$(TargetOS)"=="Win10" (
+          echo Copying $(TargetName).DVL.XML to $(ProjectDir)$(TargetName).DVL-win10-latest.XML
+          copy /Y $(ProjectDir)$(TargetName).DVL.XML^
+           $(ProjectDir)$(TargetName).DVL-win10-latest.XML
+        )
+        if "$(TargetOS)"=="Win11" (
+          echo Copying $(TargetName).DVL.XML to $(ProjectDir)$(TargetName).DVL-win11-latest.XML
+          copy /Y $(ProjectDir)$(TargetName).DVL.XML^
+           $(ProjectDir)$(TargetName).DVL-win11-latest.XML
+        )
+        call "..\build\makeLegacyDVLs.bat" "$(ProjectDir)" "$(IntDir)" "$(TargetName)" "$(Configuration)" "$(Platform)"
+        if exist "$(ProjectDir)$(TargetName).DVL-win10-1903.XML" (
+          echo Copying $(TargetName).DVL-win10-1903.XML to Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL-win10.XML
+          copy /Y $(ProjectDir)$(TargetName).DVL-win10-1903.XML^
+           $(ProjectDir)Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL-win10.XML
+        ) else (
+          echo Unable to copy Win10 DVL $(ProjectDir)$(TargetName).DVL-win10-1903.XML - it does not exist.
+        )
+        if exist "$(ProjectDir)$(TargetName).DVL-win10-1607.XML" (
+          echo Copying $(TargetName).DVL-win10-1607.XML to Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL-compat.XML
+          copy /Y $(ProjectDir)$(TargetName).DVL-win10-1607.XML^
+           $(ProjectDir)Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL-compat.XML
+        ) else (
+          echo Unable to copy COMPAT DVL $(ProjectDir)$(TargetName).DVL-win10-1607.XML - it does not exist.
+        )
       </Command>
-      <Inputs>$(IntDir)vc.nativecodeanalysis.all.xml;$(ProjectDir)viostor.DVL.XML</Inputs>
-      <Outputs>$(ProjectDir)Install\$(TargetOS)\$(TargetArch)\viostor.DVL.XML;$(ProjectDir)Install\$(TargetOS)\$(TargetArch)\viostor.DVL-win10.XML</Outputs>
+      <Inputs>$(IntDir)vc.nativecodeanalysis.all.xml;$(ProjectDir)$(TargetName).DVL.XML</Inputs>
+      <Outputs>$(ProjectDir)Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL.XML;$(ProjectDir)Install\$(TargetOS)\$(TargetArch)\$(TargetName).DVL-win10.XML</Outputs>
     </CustomBuildStep>
     <ClCompile>
       <WppEnabled Condition="'$(Configuration)|$(Platform)'=='Win10 Release|Win32'">true</WppEnabled>

commit 13465e8047bb363b3a6c1d99f9e0e80a3b85f6f0

[build] Fix for improper SDV Target parameter
1. Fixes regressions introduced in PR #1240 (4502ac3) and PR #1241 (510167a).

My bad... 8^d

Signed-off-by: benyamin-codez <115509179+benyamin-codez@users.noreply.github.com>

NetKVM/buildAll.bat
@@ -3,4 +3,4 @@ if "%VIRTIO_WIN_NO_ARM%"=="" call ..\build\build.bat netkvm-vs2015.sln "Win10 Wi
 if errorlevel 1 goto :eof
 call ..\build\build.bat netkvm-vs2015.sln "Win10 Win11" %*
 if errorlevel 1 goto :eof
-call ..\build\build.bat NetKVM-VS2015.vcxproj "Win10_SDV" "Win11_SDV" %*
+call ..\build\build.bat NetKVM-VS2015.vcxproj "Win10_SDV Win11_SDV" %*

viostor/buildAll.bat
@@ -3,4 +3,4 @@ if "%VIRTIO_WIN_NO_ARM%"=="" call ..\build\build.bat viostor.sln "Win10 Win11" A
 if errorlevel 1 goto :eof
 call ..\build\build.bat viostor.sln "Win10 Win11" %*
 if errorlevel 1 goto :eof
-call ..\build\build.bat viostor.vcxproj "Win10_SDV" "Win11_SDV" %*
+call ..\build\build.bat viostor.vcxproj "Win10_SDV Win11_SDV" %*