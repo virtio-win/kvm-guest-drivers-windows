6dd6d4122088c158f1680bbafe63848d5c8e47a8
Yuri Benditovich <yuri.benditovich@daynix.com>
2025-04-05 16:31:13 +0300

    netkvm: add CNBL getter for m_NBL
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/Common/ParaNdis-TX.h
+++ NetKVM/Common/ParaNdis-TX.h
@@ -319,6 +319,10 @@ class CNBL : public CNdisAllocatableViaHelper<CNBL>, public CRefCountingObject, 
             m_TransferSize += ChunkSize;
         }
     }
+    bool HasNBL() const
+    {
+        return m_NBL != nullptr;
+    }
     ULONG NumberOfBuffers() const
     {
         return m_BuffersNumber;
