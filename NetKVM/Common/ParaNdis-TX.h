ab8149cd88a82e2940494de108eb0b738c9cb118
Yuri Benditovich <yuri.benditovich@daynix.com>
2025-04-12 15:43:17 +0300

    [RHEL-22354] netkvm: enable NBL chains completion
    Jira link: https://issues.redhat.com/browse/RHEL-22354
    See also: https://issues.redhat.com/browse/RHEL-85605
    
    Enable NBL chain processing.
    See previous commit for technical details.
    
    If in future for any purpose there is a need to
    disable this functionality completely, it is enough
    to define NBL_CHAINS as 0, then each CNBL is
    independent and the way code works is backward-compatible.
    
    Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

--- NetKVM/Common/ParaNdis-TX.h
+++ NetKVM/Common/ParaNdis-TX.h
@@ -8,7 +8,7 @@
 /* Must be a power of 2 */
 #define PARANDIS_TX_LOCK_FREE_QUEUE_DEFAULT_SIZE 2048
 #define NBL_MAINTAIN_HISTORY                     0
-#define NBL_CHAINS                               0
+#define NBL_CHAINS                               1
 #define REPAIR_CHAIN                             (NBL_CHAINS && 1)
 
 /* the maximum number of pages that a single network packet can span.
