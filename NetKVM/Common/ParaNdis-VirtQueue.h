virtio-win/kvm-guest-drivers-windows

commit 5e279d37f82638609b2d92851ac273c54adbaa43

netkvm: allocate 8K block instead of 2x4K in TX path
Jira link: https://issues.redhat.com/browse/RHEL-40693
Currently we allocate 2x4K blocks for each TX descriptor.
Chnaging it to one allocation of 8K, the allocated block
is used exactly as before - 4K for headers area, 4K for
indirect area.

Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

NetKVM/Common/ParaNdis-VirtQueue.h
@@ -31,13 +31,7 @@ public:
     CTXHeaders()
     {}
 
-    void Initialize(NDIS_HANDLE DrvHandle, ULONG VirtioHdrSize)
-    {
-        m_VirtioHdrSize = VirtioHdrSize;
-        m_HeadersBuffer.Initialize(DrvHandle);
-    }
-
-    bool Allocate();
+    void Initialize(ULONG VirtioHdrSize, const tCompletePhysicalAddress& Buffer);
 
     virtio_net_hdr *VirtioHeader() const
     { return static_cast<virtio_net_hdr*>(m_VirtioHeaderVA); }
@@ -67,7 +61,7 @@ public:
     { return m_IPHeadersPA; }
 
 private:
-    CNdisSharedMemory m_HeadersBuffer;
+    tCompletePhysicalAddress m_HeadersBuffer;
     ULONG m_VirtioHdrSize = 0;
 
     PVOID m_VlanHeaderVA = nullptr;
@@ -99,15 +93,30 @@ public:
                   bool Indirect,
                   bool AnyLayout)
     {
-        m_Headers.Initialize(DrvHandle, VirtioHeaderSize);
-        m_IndirectArea.Initialize(DrvHandle);
+        m_MemoryBuffer.Initialize(DrvHandle);
+        // allocate 8K buffer
+        if (!m_MemoryBuffer.Allocate(PAGE_SIZE * 2))
+        {
+            return false;
+        }
         m_VirtioSGL = VirtioSGL;
         m_VirtioSGLSize = VirtioSGLSize;
         m_Indirect = Indirect;
         m_AnyLayout = AnyLayout;
 
+        // first 4K is for headers area
+        tCompletePhysicalAddress headers;
+        headers.Physical = m_MemoryBuffer.GetPA();
+        headers.Virtual = m_MemoryBuffer.GetVA();
+        headers.size = PAGE_SIZE;
+        m_Headers.Initialize(VirtioHeaderSize, headers);
 
-        return m_Headers.Allocate() && (!m_Indirect || m_IndirectArea.Allocate(PAGE_SIZE));
+        // second 4K is for indirect area
+        m_IndirectArea.Physical = m_MemoryBuffer.GetPA();
+        m_IndirectArea.Physical.QuadPart += PAGE_SIZE;
+        m_IndirectArea.Virtual = RtlOffsetToPointer(m_MemoryBuffer.GetVA(), PAGE_SIZE);
+        m_IndirectArea.size = PAGE_SIZE;
+        return true;
     }
 
     SubmitTxPacketResult Enqueue(CTXVirtQueue *Queue, ULONG TotalDescriptors, ULONG FreeDescriptors);
@@ -131,7 +140,8 @@ public:
 
 private:
     CTXHeaders m_Headers;
-    CNdisSharedMemory m_IndirectArea;
+    CNdisSharedMemory m_MemoryBuffer;
+    tCompletePhysicalAddress m_IndirectArea;
     bool m_Indirect = false;
     bool m_AnyLayout = false;
 

NetKVM/Common/ParaNdis_VirtQueue.cpp
@@ -387,8 +387,8 @@ SubmitTxPacketResult CTXDescriptor::Enqueue(CTXVirtQueue *Queue, ULONG TotalDesc
                            m_CurrVirtioSGLEntry,
                            0,
                            this,
-                           m_IndirectArea.GetVA(),
-                           m_IndirectArea.GetPA().QuadPart))
+                           m_IndirectArea.Virtual,
+                           m_IndirectArea.Physical.QuadPart))
     {
         return SubmitTxPacketResult::SUBMIT_SUCCESS;
     }
@@ -448,33 +448,29 @@ bool CTXDescriptor::SetupHeaders(ULONG ParsedHeadersLength)
     }
 }
 
-bool CTXHeaders::Allocate()
+void CTXHeaders::Initialize(ULONG VirtioHdrSize, const tCompletePhysicalAddress& Buffer)
 {
-    if (m_HeadersBuffer.Allocate(PAGE_SIZE))
-    {
-        auto VA = m_HeadersBuffer.GetVA();
-        auto PA = m_HeadersBuffer.GetPA();
-        ULONG prioSize = ALIGN_UP(ETH_PRIORITY_HEADER_SIZE, ULONGLONG);
+    m_VirtioHdrSize = VirtioHdrSize;
+    m_HeadersBuffer = Buffer;
 
-        //Headers buffer layout:
-        //    Priority header
-        //    Virtio header
-        //    Ethernet headers
+    auto VA = m_HeadersBuffer.Virtual;
+    auto PA = m_HeadersBuffer.Physical;
+    ULONG prioSize = ALIGN_UP(ETH_PRIORITY_HEADER_SIZE, ULONGLONG);
 
-        m_VlanHeaderVA   = VA;
-        m_VirtioHeaderVA = RtlOffsetToPointer(m_VlanHeaderVA, prioSize);
-        m_EthHeaderVA    = RtlOffsetToPointer(m_VirtioHeaderVA, m_VirtioHdrSize);
-        m_IPHeadersVA    = RtlOffsetToPointer(m_EthHeaderVA, ETH_HEADER_SIZE);
+    //Headers buffer layout:
+    //    Priority header
+    //    Virtio header
+    //    Ethernet headers
 
-        m_VirtioHeaderPA.QuadPart = PA.QuadPart + RtlPointerToOffset(VA, m_VirtioHeaderVA);
-        m_VlanHeaderPA.QuadPart   = PA.QuadPart + RtlPointerToOffset(VA, m_VlanHeaderVA);
-        m_EthHeaderPA.QuadPart    = PA.QuadPart + RtlPointerToOffset(VA, m_EthHeaderVA);
-        m_IPHeadersPA.QuadPart    = PA.QuadPart + RtlPointerToOffset(VA, m_IPHeadersVA);
+    m_VlanHeaderVA   = VA;
+    m_VirtioHeaderVA = RtlOffsetToPointer(m_VlanHeaderVA, prioSize);
+    m_EthHeaderVA    = RtlOffsetToPointer(m_VirtioHeaderVA, m_VirtioHdrSize);
+    m_IPHeadersVA    = RtlOffsetToPointer(m_EthHeaderVA, ETH_HEADER_SIZE);
 
-        m_MaxEthHeadersSize = m_HeadersBuffer.GetSize() - prioSize - m_VirtioHdrSize;
+    m_VirtioHeaderPA.QuadPart = PA.QuadPart + RtlPointerToOffset(VA, m_VirtioHeaderVA);
+    m_VlanHeaderPA.QuadPart   = PA.QuadPart + RtlPointerToOffset(VA, m_VlanHeaderVA);
+    m_EthHeaderPA.QuadPart    = PA.QuadPart + RtlPointerToOffset(VA, m_EthHeaderVA);
+    m_IPHeadersPA.QuadPart    = PA.QuadPart + RtlPointerToOffset(VA, m_IPHeadersVA);
 
-        return true;
-    }
-
-    return false;
+    m_MaxEthHeadersSize = m_HeadersBuffer.size - prioSize - m_VirtioHdrSize;
 }

commit 10982d90b0f6c96c9ec47e28ca9493ba6939ced3

netkvm: fix unconditional usage by indirect in TX path
Fixes: 5e279d37f8 ("netkvm: allocate 8K block instead of 2x4K in TX path")

We can't use indirect descriptors when the device does not
suggest respective feature. Unfortunately QEMU supports
indirect transfer even if 'indirect_desc' feature is
suppressed in command line.

Signed-off-by: Yuri Benditovich <yuri.benditovich@daynix.com>

NetKVM/Common/ParaNdis-VirtQueue.h
@@ -120,8 +120,8 @@ class CTXDescriptor : public CNdisAllocatable<CTXDescriptor, 'DTHR'>
                 bool AnyLayout)
     {
         m_MemoryBuffer.Initialize(DrvHandle);
-        // allocate 8K buffer
-        if (!m_MemoryBuffer.Allocate(PAGE_SIZE * 2))
+        // allocate 8K buffer (or 4K if indirect is not used)
+        if (!m_MemoryBuffer.Allocate(PAGE_SIZE * (Indirect ? 2 : 1)))
         {
             return false;
         }
@@ -138,10 +138,13 @@ class CTXDescriptor : public CNdisAllocatable<CTXDescriptor, 'DTHR'>
         m_Headers.Initialize(VirtioHeaderSize, headers);
 
         // second 4K is for indirect area
-        m_IndirectArea.Physical = m_MemoryBuffer.GetPA();
-        m_IndirectArea.Physical.QuadPart += PAGE_SIZE;
-        m_IndirectArea.Virtual = RtlOffsetToPointer(m_MemoryBuffer.GetVA(), PAGE_SIZE);
-        m_IndirectArea.size = PAGE_SIZE;
+        if (m_Indirect)
+        {
+            m_IndirectArea.Physical = m_MemoryBuffer.GetPA();
+            m_IndirectArea.Physical.QuadPart += PAGE_SIZE;
+            m_IndirectArea.Virtual = RtlOffsetToPointer(m_MemoryBuffer.GetVA(), PAGE_SIZE);
+            m_IndirectArea.size = PAGE_SIZE;
+        }
         return true;
     }
 
@@ -175,7 +178,7 @@ class CTXDescriptor : public CNdisAllocatable<CTXDescriptor, 'DTHR'>
   private:
     CTXHeaders m_Headers;
     CNdisSharedMemory m_MemoryBuffer;
-    tCompletePhysicalAddress m_IndirectArea;
+    tCompletePhysicalAddress m_IndirectArea = {};
     bool m_Indirect = false;
     bool m_AnyLayout = false;
 