941d3c81d19f1e67ea54f9fd2ffa65adaad4a87a
Konstantin Kostiuk <kkostiuk@redhat.com>
2025-06-02 10:59:54 -0700

    viosock: Add tcp-bridge service skeleton
    Service installs VSock provider and does nothing.
    
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>

---
+++ viosock/tcp-bridge/utils.h
@@ -0,0 +1,19 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+BOOL InstallService();
+BOOL UninstallService();
+BOOL GetConfiguration();
+BOOL ChangeConfig();
+BOOL ServiceRun();
+BOOL ServiceControl(int ctrl);
+
+void ShowUsage();
+__declspec(noreturn) void ErrorExit(const char *s, int err);
+void PrintMessage(const char *s);
+
+#endif
