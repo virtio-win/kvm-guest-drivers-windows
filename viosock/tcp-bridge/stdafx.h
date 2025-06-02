74120eeba48c375c4360a194af4d0bfec50b6e7e
Konstantin Kostiuk <kkostiuk@redhat.com>
2025-06-02 10:59:54 -0700

    viosock: Add tcp-bridge service skeleton
    Service installs VSock provider and does nothing.
    
    Signed-off-by: Konstantin Kostiuk <kkostiuk@redhat.com>

--- viosock/tcp-bridge/stdafx.h
+++ viosock/tcp-bridge/stdafx.h
@@ -5,3 +5,18 @@
 
 #pragma once
 #include <windows.h>
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <tchar.h>
+#include <dbt.h>
+#include <setupapi.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <strsafe.h>
+#include <cfgmgr32.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <tchar.h>
+#include "..\inc\vio_sockets.h"
+#include "service.h"
+#include "utils.h"
