cbe840e1daa259ad0ab115fed2316cb345b5c63b
Elizabeth Ashurov <eashurov@redhat.com>
2025-08-05 12:56:31 +0300

    RHEL-20221: remove unneeded parts of the NetKVM INF file
    Merged RSS and RSC parameters from separate template files into
    netkvm-base.txt since Windows 10 is now the minimum supported version.
    
    Removed the netkvm_no_RSS and netkvm_no_RSC build variants from
    GenerateInf.bat. These features are now standard for all builds.
    
    Signed-off-by: Elizabeth Ashurov <eashurov@redhat.com>

--- NetKVM/GenerateInf.bat
+++ NetKVM/GenerateInf.bat
@@ -1,30 +1,20 @@
 @echo off
 pushd "%~dp0"
-if /i "%~n1"=="netkvm_no_RSS" call :netkvm_no_RSS
-if /i "%~n1"=="netkvm_no_RSC" call :netkvm_no_RSC
 if /i "%~n1"=="netkvm_no_USO" call :netkvm_no_USO
 if /i "%~n1"=="netkvm" call :netkvm
 if /i "%~n1"=="netkvmpoll" call :netkvmpoll
 popd
 goto :eof
-:netkvm_no_RSS
-copy /y netkvm-base.txt netkvm_no_RSS.inx.tmp > nul
-call :update netkvm_no_RSS.inx
-goto :eof
-:netkvm_no_RSC
-copy /y netkvm-base.txt + netkvm-add-rss.txt netkvm_no_RSC.inx.tmp > nul
-call :update netkvm_no_RSC.inx
-goto :eof
 :netkvm_no_USO
-copy /y netkvm-base.txt + netkvm-add-rss.txt + netkvm-add-rsc.txt netkvm_no_USO.inx.tmp > nul
+copy /y netkvm-base.txt netkvm_no_USO.inx.tmp > nul
 call :update netkvm_no_USO.inx
 goto :eof
 :netkvmpoll
-copy /y netkvm-base.txt + netkvm-add-rss.txt + netkvm-add-rsc.txt + netkvm-add-uso.txt + netkvm-add-poll.txt netkvmpoll.inx.tmp > nul
+copy /y netkvm-base.txt + netkvm-add-uso.txt + netkvm-add-poll.txt netkvmpoll.inx.tmp > nul
 call :update netkvmpoll.inx
 goto :eof
 :netkvm
-copy /y netkvm-base.txt + netkvm-add-rss.txt + netkvm-add-rsc.txt + netkvm-add-uso.txt netkvm.inx.tmp > nul
+copy /y netkvm-base.txt + netkvm-add-uso.txt netkvm.inx.tmp > nul
 call :update netkvm.inx
 goto :eof
 :update
