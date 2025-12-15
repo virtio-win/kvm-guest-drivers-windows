c19930e50cb872f21befa9d13f1eca2b404bf3bb
zhangjianming <zhangjianming.zjm@alibaba-inc.com>
2025-12-15 11:13:58 +0800

    netkvm: add data structures for mergeable RX buffers
    Signed-off-by: Zhang JianMing <zhangjianming.zjm@alibaba-inc.com>

--- NetKVM/Common/ParaNdis-RX.h
+++ NetKVM/Common/ParaNdis-RX.h
@@ -66,11 +66,40 @@ class CParaNdisRX : public CParaNdisTemplatePath<CVirtQueue>, public CNdisAlloca
 
     PARANDIS_RECEIVE_QUEUE m_UnclassifiedPacketsQueue;
 
+// Maximum mergeable packet size per VirtIO spec: 65562 bytes (including 12-byte header)
+// Required buffers: ceil(65562 / 4096) = 17 PAGE-sized buffers maximum
+#define VIRTIO_NET_MAX_MRG_BUFS   17
+
+// Merge buffer context structure - pre-allocated to avoid hot-path allocation
+// Maximum PhysicalPages needed: First buffer (2 pages) + additional buffers (16 * 1 page) = 18 pages
+// Note: First buffer has 2 logical pages (PhysicalPages[0] and [1] alias to same physical page)
+#define MAX_MERGED_PHYSICAL_PAGES 18
+    struct _MergeBufferContext
+    {
+        pRxNetDescriptor BufferSequence[VIRTIO_NET_MAX_MRG_BUFS];
+        UINT32 BufferActualLengths[VIRTIO_NET_MAX_MRG_BUFS];
+        UINT16 ExpectedBuffers;
+        UINT16 CollectedBuffers;
+        UINT32 TotalPacketLength;
+
+        // Pre-allocated array for merged packet assembly (eliminates allocate/copy/free in hot path)
+        tCompletePhysicalAddress PhysicalPages[MAX_MERGED_PHYSICAL_PAGES];
+    } m_MergeContext;
+
     void ReuseReceiveBufferNoLock(pRxNetDescriptor pBuffersDescriptor);
+    pRxNetDescriptor ProcessMergedBuffers(pRxNetDescriptor pFirstBuffer, UINT nFullLength);
+    BOOLEAN CollectRemainingMergeBuffers();
+    pRxNetDescriptor AssembleMergedPacket();
+    void ReuseCollectedBuffers();
+    void ProcessReceivedPacket(pRxNetDescriptor pBufferDescriptor, CCHAR nCurrCpuReceiveQueue);
+
+    // Helper function for mergeable buffer state management
+    void DisassembleMergedPacket(pRxNetDescriptor pBuffer);
 
   private:
     int PrepareReceiveBuffers();
     pRxNetDescriptor CreateRxDescriptorOnInit();
+    pRxNetDescriptor CreateMergeableRxDescriptorOnInit(); // Simplified descriptor for mergeable buffers
     void RecalculateLimits();
 };
 
