d1933121e85d00666e11b3b392ee6ce80c9f2053
Yuri Benditovich <ybendito@redhat.com>
2026-03-03 11:06:26 +0200

    [RHEL-152850]virtio-wdf: extend DMA API to allow MDL chain
    Jira link: https://issues.redhat.com/browse/RHEL-152850
    
    virtio-fs will use this feature, no changes required
    in other drivers as they use or buffer or WDF request
    (if any). Configuring WDF DMA for MDL chain requires
    to set both buffer (to PMDL) and request (to WDF_INVALID_HANDLE).
    
    Signed-off-by: Yuri Benditovich <ybendito@redhat.com>

--- VirtIO/WDF/VirtIOWdf.h
+++ VirtIO/WDF/VirtIOWdf.h
@@ -173,6 +173,8 @@ typedef BOOLEAN (*VirtIOWdfDmaTransactionCallback)(PVIRTIO_DMA_TRANSACTION_PARAM
  *    The request should be non-cancellable all the way
  * 2. req = NULL, buffer and size provided, the buffer will be reallocated and the callback
  *    will receive SG of copied data (originally provided buffer is not used for DMA)
+ * 3. req = (WDFREQUEST)WDF_INVALID_HANDLE, then the buffer is PMDL describing MDL chain
+ *    provided by the caller, the size describes used MDL area, offset in the MDL is 0
  * If the callback wants to return FALSE (too many elements in SG or whatever), call
  *    VirtIOWdfDeviceDmaTxComplete, then complete the request (if req != NULL), then return FALSE
  * If the callback returns TRUE, call VirtIOWdfDeviceDmaTxComplete later from InterruptDpc
